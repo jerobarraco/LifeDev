123a0339927a908942817be2aa5568e53b370e6c
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-05-22 18:35:16 +1200

    * Stub subsystem

---
+++ Plugins/Inventory/Source/Inventory/Inventory.cpp
@@ -0,0 +1,7 @@
+// Copyright (C) 2023 Jeronimo Barraco-Marmol
+
+
+#include "Inventory.h"
+
+#pragma optimize("", off)
+#pragma optimize("", on)
---
+++ Plugins/Inventory/Source/Inventory/Inventory.h
@@ -0,0 +1,20 @@
+// Copyright (C) 2023 Jeronimo Barraco-Marmol
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/WorldSubsystem.h"
+
+#include "Inventory.generated.h"
+
+
+// World subsystem to deal with Inventory
+UCLASS(Blueprintable, Category="Inventory")
+class INVENTORY_API UInventory : public UWorldSubsystem
+{
+	GENERATED_BODY()
+
+public:
+protected:
+
+};
