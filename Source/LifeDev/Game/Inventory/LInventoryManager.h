f25a05361ab814fd9c55131cad4f1407e4f82150
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-05-26 19:26:16 +1200

    * Added stub LInventoryManager.cpp

---
+++ Source/LifeDev/Game/Inventory/LInventoryManager.h
@@ -0,0 +1,13 @@
+// Copyright (C) 2023 - Jeronimo Barraco-Marmol. All rights reserved.
+#pragma once
+#include "CoreMinimal.h"
+
+#include "Inventory/InventoryManager.h"
+#include "LInventoryManager.generated.h"
+
+UCLASS(Blueprintable)
+class LIFEDEV_API ALInventoryManager : public AInventoryManager {
+	GENERATED_BODY()
+
+public:
+};
