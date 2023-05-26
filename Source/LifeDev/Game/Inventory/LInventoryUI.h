4e5af931fdd6612f0dbf1e1836abbc8cf7b43f0f
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-05-26 19:21:39 +1200

    * added stub cpp classes

---
+++ Source/LifeDev/Game/Inventory/LInventoryUI.h
@@ -0,0 +1,14 @@
+// Copyright (C) 2023 - Jeronimo Barraco-Marmol. All rights reserved.
+
+#pragma once
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+
+#include "LInventoryUI.generated.h"
+
+UCLASS(Blueprintable)
+class LIFEDEV_API ULInventoryUI : public UUserWidget {
+	GENERATED_BODY()
+
+public:
+};
