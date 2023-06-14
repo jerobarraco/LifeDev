6c33f63bd5a665dd8f88fdb79d60eab2d93f6c22
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-06-14 17:41:41 +1200

    * Added bras and had an interact

---
+++ Source/LifeDev/Game/Chaps/01/Inters/Clout03.h
@@ -0,0 +1,17 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#pragma once
+#include "LifeDev/Game/Interact/LInteract.h"
+
+#include "Clout03.generated.h"
+
+// Base class for Clout00 actors
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API AClout03: public ALInteract {
+public:
+	GENERATED_BODY()
+
+	AClout03();
+
+protected:
+};
