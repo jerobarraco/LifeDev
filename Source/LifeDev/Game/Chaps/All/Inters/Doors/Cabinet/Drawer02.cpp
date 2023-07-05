967814fc9d3598bd4e5bb241413f776da8e7201c
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-07-05 18:00:27 +1200

    * Added cabinet Drawer02.cpp * removed bp

--- Source/LifeDev/Game/Chaps/All/Inters/Doors/Cabinet/Drawer02.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Doors/Cabinet/Drawer02.cpp
@@ -6,11 +6,11 @@
 #include "Interact/CInteract.h"
 
 ADrawer02::ADrawer02():Super() {
-	SFX->SetRelativeLocation(FVector(60,0,5));
+	SFX->SetRelativeLocation(FVector(60.000000,0.000000,20.000000));
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CMesh(TEXT("/Game/LifeDev/Game/Chaps/All/Inters/Cabinet00/Cabinet00-drawer1.Cabinet00-drawer1"));
 	Mesh->SetStaticMesh(CMesh.Object);
-	Mesh->SetRelativeLocation(FVector(80.000000,-63.000000,0.000000));
-	Interact->SetRelativeLocation(FVector(-20.000000,62.538369,5.000000));
-	Interact->SetBoxExtent(FVector(5.000000,18.265225,5.580736));
+	Mesh->SetRelativeLocation(FVector(80.000000,-62.500000,0.000000));
+	Interact->SetRelativeLocation(FVector(-20.000000,62.5,20.000000));
+	Interact->SetBoxExtent(FVector(5.000000,19.000000,5.000000));
 }
