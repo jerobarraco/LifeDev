1787c8a7967d129dc64eb05747f1275adb53c061
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-07-06 18:40:12 +1200

    * a few fixes on the dialogs * fixed the wardrobe door rotations

--- Source/LifeDev/Game/Chaps/All/Inters/Doors/Wardrobe/WDoor00.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Doors/Wardrobe/WDoor00.cpp
@@ -4,6 +4,7 @@
 
 #include "Components/AudioComponent.h"
 #include "Interact/CInteract.h"
+#include "Interact/Animator/CAnimatorMix.h"
 
 AWDoor00::AWDoor00():Super() {
 	SFX->SetRelativeLocation(FVector(37.5,2.5,110));
@@ -13,4 +14,7 @@ AWDoor00::AWDoor00():Super() {
 	Mesh->SetRelativeLocation(FVector(-10.000000,60.000000,0));
 	Interact->SetRelativeLocation(FVector(32.5,-55,107.5));
 	Interact->SetBoxExtent(FVector(22,5,97.5));
+	Anim->IsAdditive = true;
+	Anim->TEnd.SetLocation(FVector::ZeroVector);
+	Anim->TEnd.SetRotation(FRotator(0.0,90.0,0.0).Quaternion());
 }
--- Source/LifeDev/Game/Chaps/All/Inters/Doors/Wardrobe/WDoor01.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Doors/Wardrobe/WDoor01.cpp
@@ -4,6 +4,7 @@
 
 #include "Components/AudioComponent.h"
 #include "Interact/CInteract.h"
+#include "Interact/Animator/CAnimatorMix.h"
 
 AWDoor01::AWDoor01():Super() {
 	SFX->SetRelativeLocation(FVector(-30.753022,5.302253,110));
@@ -13,4 +14,7 @@ AWDoor01::AWDoor01():Super() {
 	Mesh->SetRelativeLocation(FVector(-95,60,0));
 	Interact->SetRelativeLocation(FVector(72.513521,-55,107.5));
 	Interact->SetBoxExtent(FVector(22,5,97.5));
+	Anim->IsAdditive = true;
+	Anim->TEnd.SetLocation(FVector::ZeroVector);
+	Anim->TEnd.SetRotation(FRotator(0.0,-90.0,0.0).Quaternion());
 }
