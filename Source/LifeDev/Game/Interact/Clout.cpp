b3c3a88f3dac8dd85b8241a2d6010ce272548797
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-07-10 19:19:37 +1200

    * Cleanup

--- Source/LifeDev/Game/Interact/Clout.cpp
+++ Source/LifeDev/Game/Interact/Clout.cpp
@@ -17,7 +17,4 @@ AClout::AClout():Super() {
 	static ConstructorHelpers::FObjectFinder<USoundBase>
 		CSnd (TEXT("/Game/LifeDev/Game/Chaps/All/Inters/Clouts/Clouts.Clouts"));
 	SFX_Trigger = CSnd.Object;
-	// if (CSnd.Succeeded()) {
-		// SFX->SetSound(CSnd.Object);
-	// }
 }
