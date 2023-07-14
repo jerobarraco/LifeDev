e98d981d33f2f571895745ac160438ab27149cba
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-07-14 19:37:06 +1200

    * docs

--- Source/LifeDev/Core/LGameInstance.cpp
+++ Source/LifeDev/Core/LGameInstance.cpp
@@ -13,8 +13,12 @@ void ULGameInstance::SetTrs(bool Enabled) {
 	GetWorld()->Exec(GetWorld(), TEXT("r.test.SecondaryScaleOverride 4"));
 	GetWorld()->Exec(GetWorld(), TEXT("t.MaxFPS 60"));
 	GetWorld()->Exec(GetWorld(), TEXT("r.Upscale.Quality 1")); // Simple bilinear https://docs.unrealengine.com/4.27/en-US/RenderingAndGraphics/ScreenPercentage/
-	GetWorld()->Exec(GetWorld(), TEXT("r.SecondaryScreenPercentage.GameViewport 80"));
-	GetWorld()->Exec(GetWorld(), TEXT("sg.ResolutionQuality 35"));
+	GetWorld()->Exec(GetWorld(), TEXT("r.SecondaryScreenPercentage.GameViewport 50"));
+	GetWorld()->Exec(GetWorld(), TEXT("sg.ResolutionQuality 40"));
+	GetWorld()->Exec(GetWorld(), TEXT("r.TemporalAA.Upsampling 0"));
+	GetWorld()->Exec(GetWorld(), TEXT("r.ScreenPercentage 50"));
+	// TODO disable temporal upsampling
+	// https://forums.unrealengine.com/t/ue5p2-r-screenpercentage-not-working-for-me/509965/11?u=nande
 }
 
 void ULGameInstance::ResetFeats() {
