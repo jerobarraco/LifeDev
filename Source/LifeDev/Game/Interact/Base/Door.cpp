01049397b264b340271833716bd40f439c16a8b8
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-07-14 17:12:54 +1200

    * turn a all possible meshes to nanite

--- Source/LifeDev/Game/Interact/Base/Door.cpp
+++ Source/LifeDev/Game/Interact/Base/Door.cpp
@@ -5,7 +5,6 @@
 #include "Interact/CInteract.h"
 
 ADoor::ADoor():Super() {
-
 	Interact->SetRelativeLocation(FVector(-60.039127,-7.825052,100.782019));
 	Interact->SetBoxExtent(FVector(60.000000,10.000000,103.743262));
 	
@@ -16,15 +15,11 @@ ADoor::ADoor():Super() {
 	Frame = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Frame"));
 	Frame->SetupAttachment(RootComponent);
 	Frame->SetComponentTickEnabled(false);
-
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CFrameMesh(TEXT("/Game/LifeDev/Game/Chaps/All/Inters/Door00/Door00-Frame.Door00-Frame"));
 	Frame->SetStaticMesh(CFrameMesh.Object);
 	Frame->SetRelativeLocation(FVector(0,-10,0));
 	Frame->SetRelativeRotation(FRotator(0,180,0).Quaternion());
-	
-	// TODO
-	// static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		// CFrame(TEXT("/Game/LifeDev/Game/Chaps/All/Inters/Door00/Door00.Door00"));
-	// Frame->SetStaticMesh(CFrame.Object);
+	// Frame->SetMobility(EComponentMobility::Static);
+	// can't make static component that is child of movable
 }
