800e48150fe089e1c8a1d2784b80f21c1b2510da
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-07-28 18:51:42 +1200

    * added window00 base class

--- Source/LifeDev/Game/Chaps/All/Inters/Doors/Windows/Window00.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Doors/Windows/Window00.cpp
@@ -9,13 +9,36 @@
 AWindow00::AWindow00():Super() {
 	// TODO set up
 	SFX->SetRelativeLocation(FVector(37.5,2.5,110));
+	
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh(TEXT("/Game/LifeDev/Game/Chaps/All/Inters/Wardrobe00/Wardrobe00-Door0.Wardrobe00-Door0"));
+		CMesh(TEXT("/Game/LifeDev/Game/Chaps/All/Inters/Window00/Window00-SideA.Window00-SideA"));
 	Mesh->SetStaticMesh(CMesh.Object);
 	Mesh->SetRelativeLocation(FVector(-10.000000,60.000000,0));
 	Interact->SetRelativeLocation(FVector(32.5,-55,107.5));
 	Interact->SetBoxExtent(FVector(22,5,97.5));
+	
 	Anim->IsAdditive = true;
 	Anim->TEnd.SetLocation(FVector::ZeroVector);
 	Anim->TEnd.SetRotation(FRotator(0.0,90.0,0.0).Quaternion());
+
+
+	// TODO set positions
+	// TODO set use parent bounds (what was the name? )
+	// TODO make a child of ustaticmeshcompont that is more optimized
+	GlassA = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("GlassA"));
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		CGlass(TEXT("/Game/LifeDev/Game/Chaps/All/Inters/Window00/Window00-Glass.Window00-Glass"));
+	GlassA->SetupAttachment(Mesh);
+	GlassA->SetStaticMesh(CGlass.Object);
+	GlassA->bUseAttachParentBound = true;
+
+	GlassB = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("GlassA"));
+	GlassB->SetupAttachment(Mesh);
+	GlassB->SetStaticMesh(CGlass.Object);
+	GlassB->bUseAttachParentBound = true;
+	
+	GlassC = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("GlassA"));
+	GlassC->SetupAttachment(Mesh);
+	GlassC->SetStaticMesh(CGlass.Object);
+	GlassC->bUseAttachParentBound = true;
 }
--- Source/LifeDev/Game/Chaps/All/Inters/Doors/Windows/Window00.h
+++ Source/LifeDev/Game/Chaps/All/Inters/Doors/Windows/Window00.h
@@ -5,11 +5,19 @@
 
 #include "Window00.generated.h"
 
-// Window 00
+// Window 00 Window type 0 side 0
 UCLASS(Blueprintable, BlueprintType)
 class LIFEDEV_API AWindow00: public ADrawer {
 public:
 	GENERATED_BODY()
 
 	AWindow00();
+
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	UStaticMeshComponent* GlassA;
+	UPROPERTY(BlueprintReadOnly)
+	UStaticMeshComponent* GlassB;
+	UPROPERTY(BlueprintReadOnly)
+	UStaticMeshComponent* GlassC;
 };
