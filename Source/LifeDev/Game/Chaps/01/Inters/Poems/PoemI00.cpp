85acb38290b1f65d19cf853a5b0cc8ccc05d4d24
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-08-14 18:45:49 +1200

    * trigger dialog on poem pick

--- Source/LifeDev/Game/Chaps/01/Inters/Poems/PoemI00.cpp
+++ Source/LifeDev/Game/Chaps/01/Inters/Poems/PoemI00.cpp
@@ -4,7 +4,7 @@
 
 APoemI00::APoemI00():Super() {
 	ItemReward = "P00";
-	TriggerDlg = "P00_PICK"; // TODO add dialogue for this
+	TriggerDlg = "P00_T";
 	// a lazy dog is a sheet of paper
 	// a sheet of paper is an ink lined plane
 	// an inclined plane is a slope up
