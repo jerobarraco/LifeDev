dc5792fa70d1eff325b498b8e6b6ca79d286d2ff
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-08-14 19:48:22 +1200

    * Added HeaterI01.cpp interaction

---
+++ Source/LifeDev/Game/Chaps/01/Inters/Var/HeaterI01.cpp
@@ -0,0 +1,16 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#include "HeaterI01.h"
+#include "Interact/CInteract.h"
+
+AHeaterI01::AHeaterI01():Super() {
+	AnimEnabled = false;
+	Locked = true;
+	Interact->SetEnabled(true);
+	
+	LockedDlg = "HT01_L";
+	Texts = {
+		FText::FromString(TEXT("Turn on.")),
+		FText::FromString(TEXT("Maybe not..."))
+	};
+}
