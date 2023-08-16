e3a75b781c686a5076090bdd2c426d2453887400
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-08-16 20:53:43 +1200

    cleanup canimatormix

--- Plugins/Interact/Source/Interact/Animator/CAnimatorMix.cpp
+++ Plugins/Interact/Source/Interact/Animator/CAnimatorMix.cpp
@@ -4,25 +4,12 @@
 #include "CAnimatorMix.h"
 
 void UCAnimatorMix::DeInit() {
-	TRoot = nullptr;
 	Mat = nullptr;
 	Super::DeInit();
 }
 
 void UCAnimatorMix::Update_Implementation(float Alpha) {
 	Super::Update_Implementation(Alpha);
-
-	if (IsValid(TRoot)) {
-		FTransform TNew = TStart;
-		if (IsAdditive) {
-			TStart.BlendFromIdentityAndAccumulate(TNew, TEnd, (const ScalarRegister) Alpha);
-			// TNew.Accumulate(TEnd, (const ScalarRegister) Alpha); // not what i want, does something different with the scale.
-		}else {
-			// Thanks Tim! this actually works very well!
-			TNew.BlendWith(TEnd, Alpha);
-		}
-		TRoot->SetRelativeTransform(TNew);
-	}
 
 	if (IsValid(Mat)) {
 		if (!MatFName.IsNone()) {
@@ -41,11 +28,4 @@ void UCAnimatorMix::Update_Implementation(float Alpha) {
 
 void UCAnimatorMix::BeginPlay() {
 	Super::BeginPlay();
-	// PrimaryComponentTick.bCanEverTick = true;
-	// PrimaryComponentTick.Target = this;
-	// PrimaryComponentTick.SetTickFunctionEnable(true);
-	// RegisterComponentTickFunctions(true); will crash
-	if (IsValid(TRoot)) {
-		TStart = TRoot->GetRelativeTransform();
-	}
 }
