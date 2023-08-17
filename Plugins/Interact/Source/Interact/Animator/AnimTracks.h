773a2e205c43d347a605cfe8bede36860287e298
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-08-17 20:17:12 +1200

    * having uobject tracks working

--- Plugins/Interact/Source/Interact/Animator/AnimTracks.h
+++ Plugins/Interact/Source/Interact/Animator/AnimTracks.h
@@ -7,35 +7,38 @@
 #include "AnimTracks.generated.h"
 
 // This is just experimental and can be removed at any point
-//
-// UCLASS(Blueprintable, BlueprintType)
-// class UAnimTrackBase : public UObject {
-// 	GENERATED_BODY()
-//
-// public:
-// 	virtual void Update(float Progress, float Alpha) {};
-// };
 
-//
-// UCLASS(Blueprintable, BlueprintType)
-// class UOAnimTrackF : public UAnimTrackBase {
-// 	GENERATED_BODY()
-//
-// public:
-// 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="SetUp")
-// 	FName Name;
-//
-// 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="SetUp")
-// 	float Start = 0.0;
-// 	
-// 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="SetUp")
-// 	float End = 1;
-//
-// 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category="SetUp")
-// 	UMaterialInstanceDynamic* Mat = nullptr;
-//
-// 	virtual void Update(float Progress, float Alpha) override {};
-// };
+// thanks to https://slowburn.dev/blog/polymorphic-serialization-in-unreal-engine/
+
+
+UCLASS(Blueprintable, BlueprintType, EditInlineNew, DefaultToInstanced)
+class UAnimTrackBase : public UObject {
+	GENERATED_BODY()
+
+public:
+	virtual void Update(float Progress, float Alpha) {};
+};
+
+
+UCLASS(Blueprintable, BlueprintType, EditInlineNew, DefaultToInstanced)
+class UOAnimTrackF : public UAnimTrackBase {
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	FName Name;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float Start = 0.0;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	float End = 1;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	UMaterialInstanceDynamic* Mat = nullptr;
+
+	virtual void Update(float Progress, float Alpha) override {};
+};
 
 // USTRUCT(Blueprintable, BlueprintType)
 // struct INTERACT_API FFAnimTrackBase {
@@ -125,6 +128,10 @@ public:
 	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category="SetUp|Tracks")
 	TArray<FAnimTrackTrans> Transfs;
 
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Instanced, Category="SetUp|Tracks")
+	TArray<UAnimTrackBase*> Track;
+
+	
 protected:
 	virtual void BeginPlay() override;
 	virtual void Update_Implementation(float Alpha) override;
