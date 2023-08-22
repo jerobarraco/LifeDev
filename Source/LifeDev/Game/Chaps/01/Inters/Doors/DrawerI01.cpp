b022141049d839a5fdc53b1e523a716661c7e64a
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-08-22 19:22:02 +1200

    * added DrawerI01.cpp with dialogs * tweaked other dialogs and item description

---
+++ Source/LifeDev/Game/Chaps/01/Inters/Doors/DrawerI01.cpp
@@ -0,0 +1,8 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#include "DrawerI01.h"
+
+void ADrawerI01::Trigger_Implementation() {
+	TriggerDlg = IsOpen ? "" : "DR01_T";
+	Super::Trigger_Implementation();
+}
---
+++ Source/LifeDev/Game/Chaps/01/Inters/Doors/DrawerI01.h
@@ -0,0 +1,16 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#pragma once
+#include "LifeDev/Game/Chaps/All/Inters/Doors/Cabinet/Drawer02.h"
+
+#include "DrawerI01.generated.h"
+
+// Drawer 02 instance 00
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API ADrawerI01: public ADrawer02 {
+	GENERATED_BODY()
+public:
+
+protected:
+	void Trigger_Implementation();
+};
