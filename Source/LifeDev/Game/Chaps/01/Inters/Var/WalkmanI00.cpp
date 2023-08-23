a9540ac3b678e88507c808355ba241287531c075
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-08-23 17:45:56 +1200

    * diag rnd changed to * postfix * walkman++

--- Source/LifeDev/Game/Chaps/01/Inters/Var/WalkmanI00.cpp
+++ Source/LifeDev/Game/Chaps/01/Inters/Var/WalkmanI00.cpp
@@ -9,13 +9,20 @@ AWalkmanI00::AWalkmanI00():Super() {
 	
 	TriggerFlashInc = .1f;
 	TriggerDlg = "WM00_T"; // TODO
-	LockedDlg = "WM00_L"; // TODO
+	LockedDlg = "WM00_L"; // TODO WM_WrongItem
 	ItemReward = "WM00";// TODO
+	
 	Locked = true;
-	ULockItem = "Batts";
+	ULockItemReq = "Batts"; // TODO Batteries
 	Texts = {
-		FText::FromString(TEXT("A walkman with no batteries")),
+		FText::FromString(TEXT("My walkman, with no batteries")),
 	};
+	
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		ObjMesh(TEXT("/Game/LifeDev/Game/Chaps/All/Inters/Walkman00/Walkman00.Walkman00"));
+
+	Interact->SetRelativeLocation(FVector(7,-5,1.725000));
+	Interact->SetBoxExtent(FVector(7.5,5,2));
 	// TODO set up meshes
 }
 
