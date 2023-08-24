70f7dfb39a0f3fbc310ec3dab9027001ff88cbc3
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-08-24 19:59:57 +1200

    * adjusted noiser times

--- Source/LifeDev/Game/Char/LChar.cpp
+++ Source/LifeDev/Game/Char/LChar.cpp
@@ -60,9 +60,9 @@ ALChar::ALChar(): Super()
 	Interactor->SetupAttachment(Camera);
 
 	Noiser = CreateDefaultSubobject<UCNoiser>(TEXT("Noiser"));
-	Noiser->TimeMin = 45;
-	Noiser->TimeMax = 150;
-	Noiser->HalfAngleWidth = (360.0-90.0)/2.0;
+	Noiser->TimeMin = 30;
+	Noiser->TimeMax = 75;
+	Noiser->HalfAngleWidth = (360.0-90.0)/2.0; // the back
 	Noiser->HalfAngleHeight = 40.0;
 	Noiser->DistMin = 50;
 	Noiser->DistMax = 600;
