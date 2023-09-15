4ed134dae021c067ba2badca3beaa698ee4ad997
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-09-15 19:06:05 +1200

    * reparent QualitySwitch_W.uasset to GroupBox.cpp

---
+++ Plugins/JUtils/Source/JUtils/UI/GroupBox.cpp
@@ -0,0 +1,29 @@
+// Copyright (C) 2023 - Jeronimo Barraco-Marmol. All rights reserved.
+
+#include "GroupBox.h"
+
+#include "Components/CheckBox.h"
+#include "Components/TextBlock.h"
+
+UGroupBox::UGroupBox(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer) {
+}
+
+void UGroupBox::SetSelected_Implementation(int32 NewSelected, bool Broadcast) {
+	const int32 N = CheckBoxes.Num();
+	// if past upper bound, then unselect
+	for (uint8 i = 0; i<N; ++i) {
+		CheckBoxes[i]->SetIsChecked(i==NewSelected);
+	}
+
+	Selected = NewSelected;
+	if (Broadcast) {
+		OnChange.Broadcast(NewSelected);
+	}
+}
+
+void UGroupBox::SetLabel_Implementation(const FText& Text) {
+	// https://benui.ca/unreal/ui-bindwidget/
+	if (!Label_T) return;
+	Label_T->SetText(Text);
+}
---
+++ Plugins/JUtils/Source/JUtils/UI/GroupBox.h
@@ -0,0 +1,43 @@
+// Copyright (C) 2023 - Jeronimo Barraco-Marmol. All rights reserved.
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#pragma once
+#include "CoreMinimal.h"
+#include "JUtils/UI/BaseUI.h"
+
+#include "GroupBox.generated.h"
+class UCheckBox;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnGroupBoxChanged, int32, Q);
+
+UCLASS(Blueprintable, BlueprintType)
+class JUTILS_API UGroupBox : public UUserWidget {
+	GENERATED_BODY()
+	
+public:
+	UGroupBox(const FObjectInitializer& ObjectInitializer);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void SetSelected(int32 NewSelected = -1, bool Broadcast = false);
+	virtual void SetSelected_Implementation(int32 NewSelected = -1, bool Broadcast = false);
+	
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void SetLabel(const FText& Text);
+	virtual void SetLabel_Implementation(const FText& Text);
+
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE int32 GetSelected() { return Selected; };
+	
+	UPROPERTY(BlueprintAssignable, Category=SetUp)
+	FOnGroupBoxChanged OnChange;
+
+protected:
+	UPROPERTY(BlueprintReadWrite, Category=SetUp)
+	TArray<UCheckBox*> CheckBoxes;
+	
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	class UTextBlock* Label_T = nullptr;
+	
+	UPROPERTY(BlueprintReadOnly)
+	int32 Selected = -1;
+};
