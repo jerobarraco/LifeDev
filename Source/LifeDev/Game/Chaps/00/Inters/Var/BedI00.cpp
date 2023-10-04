af8b579a2bd507dc6a8e06df4fcc5081bb3f4331
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-10-04 19:21:44 +1300

    * added BedI00.cpp * tweaks to dialogs

---
+++ Source/LifeDev/Game/Chaps/00/Inters/Var/BedI00.cpp
@@ -0,0 +1,25 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#include "BedI00.h"
+#include "Interact/CInteract.h"
+#include "JUtils/Actors/CQuickMesh.h"
+
+ABedI00::ABedI00():Super() {
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		CMesh(TEXT("/Game/LifeDev/Game/Chaps/All/Inters/Bed00/Bed00.Bed00"));
+	Mesh->SetStaticMesh(CMesh.Object);
+	
+	AnimEnabled = false;
+	// static since we won't animate it
+	Super::SetMobility(EComponentMobility::Static);
+	Locked = true;
+	Interact->SetEnabled(true);
+	Interact->SetRelativeLocation(FVector(102.5,-55,32.5));
+	Interact->SetBoxExtent(FVector(102.5,55,32.5));
+	
+	LockedDlg = "BED00_L";
+	Texts = {
+		FText::FromString(TEXT("I really need to sleep...")),
+	};
+	TriggerFlashInc = .1;
+}
---
+++ Source/LifeDev/Game/Chaps/00/Inters/Var/BedI00.h
@@ -0,0 +1,15 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#pragma once
+#include "LifeDev/Game/Interact/LInteract.h"
+
+#include "BedI00.generated.h"
+
+// Heater00 Instance 02
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API ABedI00: public ALInteract {
+	GENERATED_BODY()
+
+public:
+	ABedI00();
+};
