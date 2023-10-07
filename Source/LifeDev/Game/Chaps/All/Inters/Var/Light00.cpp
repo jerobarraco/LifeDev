6f423542d42774a038d159f3a54a0f72266d4eb6
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-10-07 18:07:14 +1300

    * light improvements

--- Source/LifeDev/Game/Chaps/All/Inters/Var/Light00.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Var/Light00.cpp
@@ -7,8 +7,11 @@
 #include "JUtils/Actors/CQuickMesh.h"
 
 ALight00::ALight00():Super() {
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		CMesh (TEXT("/Game/LifeDev/Game/Chaps/All/Inters/Fluorescent/Support.Support"));
 	Mesh->SetRelativeLocation(FVector(-2.5,2.5,0));
 	Mesh->SetRelativeScale3D(FVector(0.05,0.05,0.05));
+	Mesh->SetStaticMesh(CMesh.Object);
 
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CTube (TEXT("/Game/LifeDev/Game/Chaps/All/Inters/Fluorescent/Fluorescent.Fluorescent"));
@@ -23,8 +26,15 @@ ALight00::ALight00():Super() {
 	RectLight = CreateDefaultSubobject<URectLightComponent>(TEXT("Light"));
 	RectLight->SetupAttachment(Mesh);
 	RectLight->SetRelativeLocation(FVector(0.5,7.5,100));
-	RectLight->SetIntensity(300);
-
+	RectLight->SetRelativeRotation(FRotator(90,90,0));
+	RectLight->SetIntensityUnits(ELightUnits::Lumens);
+	RectLight->SetIntensity(1.5);
+	RectLight->SetAttenuationRadius(700);
+	RectLight->SetSourceWidth(75);
+	RectLight->SetSourceHeight(5);
+	RectLight->SetBarnDoorAngle(90.000000);
+	RectLight->SetBarnDoorLength(7);
+	// RectLight->SetBarnDoorLength()
 	// TODO light config
 
 	Interact->SetRelativeLocation(FVector(40,-50,103.734790));
