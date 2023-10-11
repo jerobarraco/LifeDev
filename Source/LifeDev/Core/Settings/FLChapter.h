377d533d630fdbb87c2ddc580cfcff0802bb5399
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-10-11 19:08:27 +1300

    * cleanup the FLChapter.h

--- Source/LifeDev/Core/Settings/FLChapter.h
+++ Source/LifeDev/Core/Settings/FLChapter.h
@@ -11,17 +11,10 @@ struct LIFEDEV_API FLChapter: public FTableRowBase {
 	GENERATED_BODY()
 
 public:
-	// TODO remove
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(DeprecatedProperty))
-	FText Title;
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(RowType="Dialog"))
 	TSoftObjectPtr<UDataTable> Dialogs;
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(RowType="DialogSequence"))
 	TSoftObjectPtr<UDataTable> Sequences;
 	UPROPERTY(BlueprintReadWrite, EditAnywhere)
 	TArray<FName> StorySeq;
-
-	// TODO remove
-	UPROPERTY(BlueprintReadWrite, EditAnywhere, meta=(DeprecatedProperty))
-	TSoftObjectPtr<USoundBase> Music;
 };
