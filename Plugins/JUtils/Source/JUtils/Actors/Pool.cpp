a68d95c4d1958e603ad9494d20cbcce2503cb1ec
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-11-02 17:55:38 +1300

    * Basic actor pooler clas

---
+++ Plugins/JUtils/Source/JUtils/Actors/Pool.cpp
@@ -0,0 +1,127 @@
+﻿#include "Pool.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogJPool, Log, Log);
+
+#pragma optimize("", off)
+void UPool::Set(int32 Max, bool bCanIncrease, TSubclassOf<AActor> Class) {
+    Empty();
+    
+    static FActorSpawnParameters P;
+    P.bNoFail = true;
+    P.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+    Ready.Reserve(Max);
+    if (Ready.Num()>Max) {
+        // TODO destroy
+    }
+    // TODO CanIncrease
+    // TODO store class to increase
+    if (Ready.Num()<Max) {
+        for (int32 i = Ready.Num(); i<Max; ++i) {
+            AActor* const Actor = GetWorld()->SpawnActor(Class, 0, 0, P);
+            if (!IsValid(Actor)) {
+                UE_LOG(LogJPool, Log, TEXT("Could not spawn the actor"));
+                continue;
+            }
+            Ready.Add(Actor);
+            Actor->SetActorHiddenInGame(true);
+        }
+    }
+}
+
+AActor* UPool::Get() {
+    if (Ready.Num()<=0) {
+        UE_LOG(LogJPool, Log, TEXT("Pool is exhausted so can't return an actor."));
+        return nullptr;
+    }
+    
+    AActor* const A = Ready[0];
+    Ready.RemoveAt(0, 1, false);
+    A->SetActorHiddenInGame(false);
+    A->Reset();
+    UE_LOG(LogJPool, Log, TEXT("Pool gave an actor."));
+    return A;
+}
+
+void UPool::Return(AActor* A) {
+    UE_LOG(LogJPool, Log, TEXT("Actor returned to pool."));
+    A->SetActorHiddenInGame(true);
+    Ready.AddUnique(A);
+}
+
+void UPool::Empty() {
+    UE_LOG(LogJPool, Log, TEXT("Clearing pool"));
+    for(AActor* const A: Ready) {
+        A->Destroy();
+    }
+    Ready.Empty();
+}
+
+void UPool::BeginDestroy() {
+    Empty();
+    UObject::BeginDestroy();
+}
+
+void UPooler::AddPool(int32 Max, bool CanIncrease, TSubclassOf<AActor> Class) {
+    const FName Key = Class->GetFName();
+    UPool** pPool = Pools.Find(Key);
+    UPool* Pool = nullptr;
+    if (pPool) {
+        Pool = *pPool;
+    }else{
+        Pool = NewObject<UPool>(this);
+        Pools.Add(Key, Pool);
+    }
+
+    Pool->Set(Max, CanIncrease, Class);
+}
+
+void UPooler::RemPool(TSubclassOf<AActor> Class) {
+    const FName Key = Class->GetFName();
+    
+    UPool** const PPool = Pools.Find(Key);
+    if (!PPool) {
+        UE_LOG(LogJPool, Log, TEXT("DelPool. Could not find the pool"));
+        return;
+    }
+
+    UPool* const Pool = *PPool; 
+    Pool->Empty();
+    
+    Pools.Remove(Key);
+}
+
+UPool* UPooler::GetPool(TSubclassOf<AActor> Class) {
+    const FName Key = Class->GetFName();
+    UPool** const pPool = Pools.Find(Key);
+    if (!pPool) {
+        UE_LOG(LogJPool, Log, TEXT("Pooler.GetPool. Could not find the pool for class=%s"), *Key.ToString());
+        return nullptr;
+    }
+    return *pPool;
+}
+
+AActor* UPooler::Get(TSubclassOf<AActor> Class) {
+    UPool* const Pool = GetPool(Class); 
+    if (!Pool) {
+        return nullptr;
+    }
+    return Pool->Get();
+}
+
+void UPooler::Return(AActor* A) {
+    if (!IsValid(A)) {
+        UE_LOG(LogJPool, Log, TEXT("Pooler.Return. Actor was invalid, ignoring."));
+        return;
+    }
+
+    UPool* const Pool = GetPool(A->GetClass());
+    if (!Pool) {
+        UE_LOG(LogJPool, Log, TEXT("Pooler.Return. Could not find the pool for class=%s. Destroying the actor."), *Key.ToString());
+        A->Destroy();
+        return;
+    }
+
+    Pool->Return(A);
+}
+
+#pragma optimize("", on)
---
+++ Plugins/JUtils/Source/JUtils/Actors/Pool.h
@@ -0,0 +1,52 @@
+﻿#pragma once
+
+#include "Pool.generated.h"
+
+// basic actor pooling. manages one local pool.
+UCLASS(Blueprintable, BlueprintType)
+class JUTILS_API UPool: public UObject {
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable)
+    void Set(int32 Max, bool bCanIncrease, TSubclassOf<AActor> Class);
+    UFUNCTION(BlueprintCallable)
+    AActor* Get();
+    UFUNCTION(BlueprintCallable)
+    void Return(AActor* A);
+    UFUNCTION(BlueprintCallable)
+    void Empty();
+
+    virtual void BeginDestroy() override;
+protected:
+    
+    UPROPERTY()
+    TArray<AActor*> Ready;
+    friend class UPooler;
+};
+
+// basic actor pooling. handles multiple pools
+UCLASS(Blueprintable)
+class JUTILS_API UPooler: public UWorldSubsystem {
+    GENERATED_BODY()
+public:
+
+    UFUNCTION(BlueprintCallable)
+    void AddPool(int32 Max, bool CanIncrease, TSubclassOf<AActor> Class);
+
+    UFUNCTION(BlueprintCallable)
+    void RemPool(TSubclassOf<AActor> Class);
+
+    UFUNCTION(BlueprintCallable)
+    UPool* GetPool(TSubclassOf<AActor> Class);
+
+    UFUNCTION(BlueprintCallable)
+    AActor* Get(TSubclassOf<AActor> Class);
+
+    UFUNCTION(BlueprintCallable)
+    void Return(AActor* A);
+    
+protected:
+    UPROPERTY() // gcd
+    TMap<FName, UPool*> Pools;
+};
