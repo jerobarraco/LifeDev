70606cfe41a7925368a9f76d9d0dff6bf52dfb16
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-11-10 18:53:20 +1300

    * Added stub RadioI00.cpp * added missing room 02?

---
+++ Source/LifeDev/Game/Chaps/00/Inters/Var/RadioI00.cpp
@@ -0,0 +1,7 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#include "RadioI00.h"
+
+ARadioI00::ARadioI00():Super() {
+	
+}
---
+++ Source/LifeDev/Game/Chaps/00/Inters/Var/RadioI00.h
@@ -0,0 +1,15 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#pragma once
+#include "LifeDev/Game/Interact/LInteract.h"
+
+#include "RadioI00.generated.h"
+
+// Heater00 Instance 02
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API ARadioI00: public ALInteract {
+	GENERATED_BODY()
+
+public:
+	ARadioI00();
+};
