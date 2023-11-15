68805b88f383f41d83bc0c88191ca0930d580f99
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-11-16 11:50:24 +1300

    * updated the RadioI00.cpp dialogs

--- Source/LifeDev/Game/Chaps/00/Inters/Var/RadioI00.cpp
+++ Source/LifeDev/Game/Chaps/00/Inters/Var/RadioI00.cpp
@@ -8,9 +8,13 @@ ARadioI00::ARadioI00():Super() {
 	ULockDlg = "RD00_UL";
 	TriggerDlg = "RD00_T";
 	ULockItem = "T01"; // Tape
+	DisableWhileAnim = false; // to allow to disable once activated
+	Texts = {
+		FText::FromString(TEXT("Play"))
+	};
 }
 
 void ARadioI00::Trigger_Implementation() {
 	Super::Trigger_Implementation();
-	SetEnabled(false);// disable once activated
+	SetEnabled(false);// disable once activated. one use only.
 }
