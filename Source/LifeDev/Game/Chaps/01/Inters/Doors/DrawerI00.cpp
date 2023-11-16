87073edb37f4bbf5cc0627d4a5520429b4e36431
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-11-16 18:52:44 +1300

    * fix drawer dialog

--- Source/LifeDev/Game/Chaps/01/Inters/Doors/DrawerI00.cpp
+++ Source/LifeDev/Game/Chaps/01/Inters/Doors/DrawerI00.cpp
@@ -4,5 +4,5 @@
 
 void ADrawerI00::SetState_Implementation(int32 NewState) {
 	Super::SetState_Implementation(NewState);
-	TriggerDlg = IsOpen() ? "" : "DR00_T";
+	TriggerDlg = IsOpen() ? "DR00_T" : "";
 }
