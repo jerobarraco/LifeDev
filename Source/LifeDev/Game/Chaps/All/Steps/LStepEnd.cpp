5b3e089e3090cb68c99ba4378bb4fce6fe043023
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-12-07 18:43:13 +1300

    * fixing the end step

--- Source/LifeDev/Game/Chaps/All/Steps/LStepEnd.cpp
+++ Source/LifeDev/Game/Chaps/All/Steps/LStepEnd.cpp
@@ -5,18 +5,28 @@
 
 ALStepEnd::ALStepEnd():Super() {
 	Name = FName("End");
+	static FText ST = FText::FromString("~ To be continued ... ~");
+	Title = ST;
 	InputEnabled = false;
-	// don't wait for fades
-	UseFadeTime = false;
-	// don't finish, we will change levels. and better not to mess with this.
-	FinishPostWait = false;
+	UseFadeTime = true;
+	FinishPostWait = false; // avoid crash
+	UseFade = true;
 	// don't change the camera
-	UsePawnCam = false;
+	UsePawnCam = true;
 	CamTarget = nullptr;
 }
 
 void ALStepEnd::Start_Implementation() {
 	Super::Start_Implementation();
-	
 	UGameplayStatics::OpenLevel(GetWorld(), FName(*NextLevel), true);
+	
+}
+
+void ALStepEnd::Stop_Implementation() {
+	Super::Stop_Implementation();
+	
+}
+
+void ALStepEnd::PostWait_Implementation() {
+	Super::PostWait_Implementation();
 }
--- Source/LifeDev/Game/Chaps/All/Steps/LStepEnd.h
+++ Source/LifeDev/Game/Chaps/All/Steps/LStepEnd.h
@@ -20,4 +20,6 @@ public:
 
 protected:
 	virtual void Start_Implementation() override;
+	virtual void Stop_Implementation() override;
+	virtual void PostWait_Implementation() override;
 };
