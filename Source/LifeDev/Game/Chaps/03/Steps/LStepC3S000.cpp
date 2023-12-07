a89ac8bb94a700458d66223a6b4659010553d218
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-12-07 18:17:00 +1300

    * Added Chater03 stub * reorganized assets

--- Source/LifeDev/Game/Chaps/03/Steps/LStepC3S000.cpp
+++ Source/LifeDev/Game/Chaps/03/Steps/LStepC3S000.cpp
@@ -1,27 +1,27 @@
 // Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
-#include "LStepC2S000.h"
+#include "LStepC3S000.h"
 
 #include "WorldPartition/DataLayer/DataLayerAsset.h"
 
-ALStepC2S000::ALStepC2S000():Super() {
-	Name = FName("C2S0");
-	static FText ST = FText::FromString("~ Yin|Remove ~");
+ALStepC3S000::ALStepC3S000():Super() {
+	Name = FName("C3S0");
+	static FText ST = FText::FromString("~ To be continued ... ~");
 	Title = ST;
 	UseFade = true;
-	// DlgId = FName("C2S0"); // this chapter finishes after the dialog
+	// DlgId = FName("C3S0"); // this chapter finishes after the dialog
 	InputEnabled = true;
 	UsePawnCam = true;
 	UseFadeTime = true;
 	FinishPostWait = false;
 
 	static ConstructorHelpers::FObjectFinder<UDataLayerAsset>
-		CDL1 (TEXT("/Game/LifeDev/Game/Chaps/All/DataLayers/Chap02_DL.Chap02_DL"));
+		CDL1 (TEXT("/Game/LifeDev/Game/Chaps/All/DataLayers/Chap03_DL.Chap03_DL"));
 	if (CDL1.Succeeded()) {
 		DL_Load.Add(CDL1.Object);
 	}
 	
 	static ConstructorHelpers::FObjectFinder<UDataLayerAsset>
-		CDL2 (TEXT("/Game/LifeDev/Game/Chaps/All/DataLayers/Chap01_DL.Chap01_DL"));
+		CDL2 (TEXT("/Game/LifeDev/Game/Chaps/All/DataLayers/Chap02_DL.Chap02_DL"));
 	if (CDL2.Succeeded()) {
 		DL_Unload.Add(CDL2.Object);
 	}
---
+++ Source/LifeDev/Game/Chaps/03/Steps/LStepC3S000.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LifeDev/Core/Story/LStep.h"
+
+#include "LStepC3S000.generated.h"
+
+UCLASS(Blueprintable, BlueprintType)
+class ALStepC3S000 : public ALStep {
+	GENERATED_BODY()
+
+public:
+	ALStepC3S000();
+};
