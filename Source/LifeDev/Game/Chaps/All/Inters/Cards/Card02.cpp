abf6b43cd9369618af078f680959e95209edfbaf
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-12-18 19:22:35 +1300

    * fixes to the card02 and inventory

--- Source/LifeDev/Game/Chaps/All/Inters/Cards/Card02.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Cards/Card02.cpp
@@ -8,5 +8,5 @@ ACard02::ACard02():Super() {
 	UseAnim = false;
 	RewardItem = LDConsts::Items::Card2;
 	Texts = { FText::FromString(TEXT("Pick Up")), FText::FromString(TEXT("Pick Up")) } ;
-	TriggerDlg = "C02_Look*";
+	TriggerDlg = "C2_Look*";
 }
