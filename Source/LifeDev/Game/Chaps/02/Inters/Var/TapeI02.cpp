f6bda25c7efa731045b57e13ef479a6d65927e1e
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-12-18 18:58:43 +1300

    * dlg: t02_l and t02_look

--- Source/LifeDev/Game/Chaps/02/Inters/Var/TapeI02.cpp
+++ Source/LifeDev/Game/Chaps/02/Inters/Var/TapeI02.cpp
@@ -5,15 +5,18 @@
 #include "LifeDev/Game/Sys/Consts/ConstItems.h"
 
 ATapeI02::ATapeI02():Super() {
-	Texts = { FText::FromString(TEXT("Pick up tape")) };
+	Texts = { FText::FromString(TEXT("Another tape of mine")) };
 	StateNum = 1;
 	UseAnim = false;
 	RewardFlash = .1;
 	RewardItem = "T02";
+	UseRewardFade = true;
 	Locked = true;
-	LockedDlg = FName("T02_L");
-	TriggerDlg = FName("T02_Look"); // will say what it is. triggered on pick up
 	ULockItem = LDConsts::Items::Batts;
+	LockedDlg = FName("T02_L");
+	TriggerDlg = FName("T02_Look*"); // will say what it is. triggered on pick up
 	// needed to be able to attach to the drawer
 	Super::SetMobility(EComponentMobility::Movable);
+	// At the moment it says the whole song on look or pick.
+	// TODO leave only the 1st dialog as look or pick. then show the rest on item usage.
 }
