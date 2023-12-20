183964c81aeaba13a86ed509be2d27a9fef540dd
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-12-20 18:30:49 +1300

    * locked door to outside and hall0-room01

--- Source/LifeDev/Game/Chaps/01/Inters/Doors/DoorI02.cpp
+++ Source/LifeDev/Game/Chaps/01/Inters/Doors/DoorI02.cpp
@@ -4,7 +4,7 @@
 
 ADoorI02::ADoorI02():Super() {
 	UseAnim = false;
-	LockedDlg = "D02_LCK";
+	LockedDlg = "D02_L";
 	Locked = true;
 	RootComponent->SetWorldLocation(FVector(-290,960,0));
 	RootComponent->SetWorldRotation(FRotator(0,180,0));
