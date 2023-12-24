e11c2924b3d32a86ce97378fb9f9b8c7cbf9f313
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2023-12-24 19:25:37 +1300

    * added cabinet01 to scene and reorg

--- Source/LifeDev/Game/Chaps/All/Inters/Doors/Cabinet/Drawer01.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Doors/Cabinet/Drawer01.cpp
@@ -9,7 +9,7 @@
 ADrawer01::ADrawer01():Super() {
 	SFX->SetRelativeLocation(FVector(60,0,5));
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh(TEXT("/Game/LifeDev/Game/Chaps/All/Inters/Cabinet00/Cabinet00-drawer0.Cabinet00-drawer0"));
+		CMesh(TEXT("/Game/LifeDev/Game/Chaps/All/Inters/Cabinets/Cabinet00-drawer0.Cabinet00-drawer0"));
 	Mesh->SetStaticMesh(CMesh.Object);
 	Mesh->SetRelativeLocation(FVector(80.000000,-62.500000,0.000000));
 	Interact->SetRelativeLocation(FVector(-20.000000,62.5,5.000000));
--- Source/LifeDev/Game/Chaps/All/Inters/Doors/Cabinet/Drawer02.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Doors/Cabinet/Drawer02.cpp
@@ -10,7 +10,7 @@
 ADrawer02::ADrawer02():Super() {
 	SFX->SetRelativeLocation(FVector(60.000000,0.000000,20.000000));
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh(TEXT("/Game/LifeDev/Game/Chaps/All/Inters/Cabinet00/Cabinet00-drawer1.Cabinet00-drawer1"));
+		CMesh(TEXT("/Game/LifeDev/Game/Chaps/All/Inters/Cabinets/Cabinet00-drawer1.Cabinet00-drawer1"));
 	Mesh->SetStaticMesh(CMesh.Object);
 	Mesh->SetRelativeLocation(FVector(80.000000,-62.500000,0.000000));
 	Interact->SetRelativeLocation(FVector(-20.000000,62.5,20.000000));
