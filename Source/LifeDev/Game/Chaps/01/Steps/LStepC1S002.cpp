d220122e5c6941d313b5e5dd26ee09abf62d25c8
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-01-01 18:32:53 +1300

    * fixed wrong camshake ref

--- Source/LifeDev/Game/Chaps/01/Steps/LStepC1S002.cpp
+++ Source/LifeDev/Game/Chaps/01/Steps/LStepC1S002.cpp
@@ -25,7 +25,7 @@ ALStepC1S002::ALStepC1S002():Super() {
 	CharClass = ALNPC01::StaticClass();
 	
 	static ConstructorHelpers::FClassFinder<UCameraShakeBase>
-		CShake(TEXT("/Game/LifeDev/Game/Chaps/01/Steps/02/CamShake_B"));
+		CShake(TEXT("/Game/LifeDev/Game/Env/CamShake_B.CamShake_B"));
 	ShakeClass = CShake.Succeeded()? CShake.Class.Get() : UDefaultCameraShakeBase::StaticClass();
 
 	Cam->SetConstraintAspectRatio(true);
