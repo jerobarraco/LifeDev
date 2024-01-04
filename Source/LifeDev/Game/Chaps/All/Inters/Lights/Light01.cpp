c80719eb6ded250e56f7aabb51ad8ca779fb1c23
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-01-04 18:52:19 +1300

    * fixed Light01.cpp

--- Source/LifeDev/Game/Chaps/All/Inters/Lights/Light01.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Lights/Light01.cpp
@@ -7,12 +7,12 @@
 
 ALight01::ALight01():Super() {
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh (TEXT("/Game/LifeDev/Game/Chaps/All/Arch/Lights/Light00/Light00-Frame.Light00-Frame"));
+		CMesh (TEXT("/Game/LifeDev/Game/Inters/Lights/Light00/Light00-Frame.Light00-Frame"));
 	Mesh->SetRelativeLocation(FVector(-27.500000,27.500000,0.000000));
 	Mesh->SetStaticMesh(CMesh.Object);
 
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CTube (TEXT("/Game/LifeDev/Game/Chaps/All/Arch/Lights/Light00/Light00-GlassB.Light00-GlassB"));
+		CTube (TEXT("/Game/LifeDev/Game/Inters/Lights/Light00/Light00-GlassB.Light00-GlassB"));
 	Glass = CreateDefaultSubobject<UCQuickMesh>(TEXT("Tube"));
 	Glass->SetupAttachment(Mesh);
 	Glass->SetStaticMesh(CTube.Object);
