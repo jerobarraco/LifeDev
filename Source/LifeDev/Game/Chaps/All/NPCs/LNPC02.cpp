44fd9af85c26f231a06b3ba8874a05f80d2840d8
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-01-04 18:23:57 +1300

    * fixed char07 pivots * fixed ncp02

--- Source/LifeDev/Game/Chaps/All/NPCs/LNPC02.cpp
+++ Source/LifeDev/Game/Chaps/All/NPCs/LNPC02.cpp
@@ -9,81 +9,72 @@ ALNPC02::ALNPC02():Super() {
 	Locked = true;
 	Texts = { FText::FromString("...") };
 
-	// (X=-30.000000,Y=30.000000,Z=-40.000000)
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh1(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/Hip.Hip"));
+		CMesh1(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/Pelvis.Pelvis"));
 	Pelvis->SetStaticMesh(CMesh1.Object);
-	Pelvis->SetRelativeLocation(FVector(0,0,45));
+	Pelvis->SetRelativeLocation(FVector(0,0,35));
 
-	// (X=-30.000000,Y=30.000000,Z=-40.000000)
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CMesh2(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/Torso.Torso"));
 	Torso->SetStaticMesh(CMesh2.Object);
 
-	// (X=-30.000000,Y=30.000000,Z=-80.000000)
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh3(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/HeadHair.HeadHair"));
+		CMesh3(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/Head.Head"));
 	Head->SetStaticMesh(CMesh3.Object);
-	Head->SetRelativeLocation(FVector(0,0,35));
+	Head->SetRelativeLocation(FVector(0,0,25));
 
-	// (X=-30.000000,Y=10.000000,Z=-75.000000)
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh4(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/LeftArm_High.LeftArm_High"));
+		CMesh4(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/ArmL1.ArmL1"));
 	ArmL1->SetStaticMesh(CMesh4.Object);
-	ArmL1->SetRelativeLocation(FVector(0,15,30));
-	// ArmL1->SetRelativeRotation(FRotator(0, 180, 0));
+	ArmL1->SetRelativeLocation(FVector(-10,0,25));
 	
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh5(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/Right_Arm_High.Right_Arm_High"));
+		CMesh5(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/ArmR1.ArmR1"));
 	ArmR1->SetStaticMesh(CMesh5.Object);
-	ArmR1->SetRelativeLocation(FVector(0,-15,30));
+	ArmR1->SetRelativeLocation(FVector(10,0,25));
 
-	// (X=-30.000000,Y=12.500000,Z=-55.000000)
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh6(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/LeftArm_Low.LeftArm_Low"));
+		CMesh6(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/ArmL2.ArmL2"));
 	ArmL2->SetStaticMesh(CMesh6.Object);
-	ArmL2->SetRelativeLocation(FVector(0,2.5,-20));
+	ArmL2->SetRelativeLocation(FVector(-2.5,0,-15));
 
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh7(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/Right_Arm_Low.Right_Arm_Low"));
+		CMesh7(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/ArmR2.ArmR2"));
 	ArmR2->SetStaticMesh(CMesh7.Object);
-	ArmR2->SetRelativeLocation(FVector(0,-2.5,-20));
+	ArmR2->SetRelativeLocation(FVector(2.5,0,-15));
 
-	// (X=-30.000000,Y=20.000000,Z=-25.000000)
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh8(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/Left_leg_high.Left_leg_high"));
+		CMesh8(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/LegL1.LegL1"));
 	LegL1->SetStaticMesh(CMesh8.Object);
-	LegL1->SetRelativeLocation(FVector(0,10,-10.000000));
+	LegL1->SetRelativeLocation(FVector(-5,0,-10));
 	
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh9(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/right_leg_high.right_leg_high"));
+		CMesh9(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/LegR1.LegR1"));
 	LegR1->SetStaticMesh(CMesh9.Object);
-	LegR1->SetRelativeLocation(FVector(0,-10,-10));
+	LegR1->SetRelativeLocation(FVector(5,0,-10));
 
-	// (X=-30.000000,Y=20.000000,Z=-10.000000)
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh10(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/Left_leg_low.Left_leg_low"));
+		CMesh10(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/LegL2.LegL2"));
 	LegL2->SetStaticMesh(CMesh10.Object);
-	LegL2->SetRelativeLocation(FVector(0.000000,0,-15));
+	LegL2->SetRelativeLocation(FVector(0,0,-10));
 
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh11(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/right_leg_low.right_leg_low"));
+		CMesh11(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/LegR2.LegR2"));
 	LegR2->SetStaticMesh(CMesh11.Object);
-	LegR2->SetRelativeLocation(FVector(0,0,-15));
+	LegR2->SetRelativeLocation(FVector(0,0,-10));
 
-	// (X=-30.000000,Y=20.000000,Z=5.000000)
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh12(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/Left_foot.Left_foot"));
+		CMesh12(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/FootL.FootL"));
 	FootL->SetStaticMesh(CMesh12.Object);
-	FootL->SetRelativeLocation(FVector(0,0,-15));
+	FootL->SetRelativeLocation(FVector(0,0,-10));
 
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh13(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/Right_foot.Right_foot"));
+		CMesh13(TEXT("/Game/LifeDev/Game/Chars/Char07/Parts/FootR.FootR"));
 	FootR->SetStaticMesh(CMesh13.Object);
-	FootR->SetRelativeLocation(FVector(0,0,-15));
+	FootR->SetRelativeLocation(FVector(0,0,-10));
 
-	Interact->SetRelativeLocation(FVector(0,0,65));
-	Interact->SetBoxExtent(FVector(25,20,65));
+	Interact->SetRelativeLocation(FVector(0,-5,50));
+	Interact->SetBoxExtent(FVector(25,25,55));
 
 	AnimFade->SetNewMat();
 }
