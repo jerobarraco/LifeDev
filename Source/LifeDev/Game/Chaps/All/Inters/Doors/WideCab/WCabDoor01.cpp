f5ae1a6772b92a89a44b76b3cccc365760bb8537
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-01-04 20:04:44 +1300

    * fixed wcdoor01 sound pos

--- Source/LifeDev/Game/Chaps/All/Inters/Doors/WideCab/WCabDoor01.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Doors/WideCab/WCabDoor01.cpp
@@ -9,7 +9,7 @@
 #include "JUtils/Actors/CQuickMesh.h"
 
 AWCabDoor01::AWCabDoor01():Super() {
-	SFX->SetRelativeLocation(FVector(0,-50,32.5));
+	SFX->SetRelativeLocation(FVector(0,55,32));
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CMesh(TEXT("/Game/LifeDev/Game/Inters/Cabinets/Cabinet01-DoorL.Cabinet01-DoorL"));
 	Mesh->SetStaticMesh(CMesh.Object);
