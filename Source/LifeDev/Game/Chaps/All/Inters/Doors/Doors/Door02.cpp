e80238e5ffe9d81e5c1e93d647f2697f534d46c9
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-01-05 18:11:29 +1300

    * fixed sfx position for door02 and its children, again. * fixed sfx position for wcabdoors

--- Source/LifeDev/Game/Chaps/All/Inters/Doors/Doors/Door02.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Doors/Doors/Door02.cpp
@@ -30,7 +30,7 @@ ADoor02::ADoor02():Super() {
 	WinB->SetCastAllShadows(false); // opt
 	WinB->bUseAttachParentBound = true; // opt
 	
-	SFX->SetRelativeLocation(FVector(-90,5,85));
+	SFX->SetRelativeLocation(FVector(100,-10,82.5));
 
 	Interact->SetRelativeLocation(FVector(55,-10,100));
 	Interact->SetBoxExtent(FVector(50,10,100));
--- Source/LifeDev/Game/Chaps/All/Inters/Doors/WideCab/WCabDoor01.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Doors/WideCab/WCabDoor01.cpp
@@ -9,7 +9,7 @@
 #include "JUtils/Actors/CQuickMesh.h"
 
 AWCabDoor01::AWCabDoor01():Super() {
-	SFX->SetRelativeLocation(FVector(0,55,32));
+	SFX->SetRelativeLocation(FVector(0,-55,32.5));
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CMesh(TEXT("/Game/LifeDev/Game/Inters/Cabinets/Cabinet01-DoorL.Cabinet01-DoorL"));
 	Mesh->SetStaticMesh(CMesh.Object);
