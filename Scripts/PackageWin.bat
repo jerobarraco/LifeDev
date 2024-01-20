88d72d611044b873eab24e385eccbd0f0212e029
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-01-20 18:05:19 +1300

    * added multi process cook

--- Scripts/PackageWin.bat
+++ Scripts/PackageWin.bat
@@ -12,7 +12,7 @@ set CONFIG=Shipping
 
 :: Build client
 pushd %UNREAL_ENGINE_ROOT% || exit /b 1
-call ./Engine/Build/BatchFiles/RunUAT.bat BuildCookRun -project="%WORKSPACE%/%PROJECT_NAME%.uproject" -noP4 -platform=Win64 -clientconfig=%CONFIG% -serverconfig=%CONFIG% -clean -cook -allmaps -build -stage -pak -stage -stagingdirectory="%WORKSPACE%/Build/"
+call ./Engine/Build/BatchFiles/RunUAT.bat BuildCookRun -project="%WORKSPACE%/%PROJECT_NAME%.uproject" -noP4 -platform=Win64 -clientconfig=%CONFIG% -serverconfig=%CONFIG% -clean -cook -allmaps -build -stage -pak -stage -stagingdirectory="%WORKSPACE%/Build/" -AdditionalCookerOptions="-cookprocesscount=4"
 popd
 exit /b 0
 :: archive does nothing that staging does.
