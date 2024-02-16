085a2bb2b01cb2d77db642b56ba75708fb5c3006
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-02-16 19:46:16 +1300

    * cleanup

--- Source/LifeDev/Game/Chaps/All/Inters/Music/PianoKey.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Music/PianoKey.cpp
@@ -23,28 +23,7 @@ APianoKey::APianoKey():Super() {
 	// niagara curves are not loaded somehow
 	Anim->Curve = CCurve.Object;
 	Anim->Duration = 1;
-	// this will avoid anim being set to reverse.
-	// Trans.Add(FTransform(FRotator(0,0,10).Quaternion(), FVector::ZeroVector, FVector::Zero()));
 
-	// the animation needs to play in a different way than what i want, hence i need to define tstart instead
-	// Anim->IsAdditive = false;
-	// Anim->TEnd = FTransform (FQuat::Identity, FVector::ZeroVector, FVector::OneVector); 
-	// Anim->TStart = Anim->TEnd;
 	Anim->IsAdditive = true;
 	Anim->TEnd.SetRotation(FRotator(0,0,10).Quaternion());
 }
-
-// void APianoKey::SetState_Implementation(int32 NewState) {
-	// overriden by AnimPlay and SetState
-	// when using Trans, i need to reset since bouncing will leave it on true
-	// Anim->IsReversed = false;
-	// Anim->IsBouncing = true;
-	// Super::SetState_Implementation(NewState);
-// }
-
-// void APianoKey::PostInitializeComponents() {
-	// Super::PostInitializeComponents();
-
-	// UCodeCurveLib* const CurveLib = UCodeCurveLib::Instance();
-	// Anim->CodeCurve.BindDynamic(CurveLib, &UCodeCurveLib::OutBack);
-// }
