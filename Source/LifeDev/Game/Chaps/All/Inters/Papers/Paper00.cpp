b72aa5bd0403dc7782c96a489ebe3fdbfe914916
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-02-16 18:14:29 +1300

    * Paper00.cpp init. moved sfx

--- Source/LifeDev/Game/Chaps/All/Inters/Papers/Paper00.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Papers/Paper00.cpp
@@ -2,12 +2,19 @@
 
 #include "Paper00.h"
 
+#include "Components/AudioComponent.h"
 #include "Interact/CInteract.h"
 #include "Interact/Animator/CAnimatorFade.h"
 #include "JUtils/Actors/CQuickMesh.h"
 
 APaper00::APaper00():Super() {
-	Super::SetMobility(EComponentMobility::Static);
+	UseAnim = false;
+	StateNum = 1;
+	Texts = { FText::FromString(TEXT("A piece of paper")) };
+	// a lazy dog is a sheet of paper
+	// a sheet of paper is an ink lined plane
+	// an inclined plane is a slope up
+	// a slow pup is a lazy dog
 
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CMesh(TEXT("/Game/LifeDev/Game/Inters/Paper00/Paper00.Paper00"));
@@ -16,16 +23,12 @@ APaper00::APaper00():Super() {
 	
 	Interact->SetRelativeLocation(FVector(7.5,-10.5,0.25));
 	Interact->SetBoxExtent(FVector(7.5,10.5,1));
-	
-	Texts = { FText::FromString(TEXT("A piece of paper")) } ;
-	// a lazy dog is a sheet of paper
-	// a sheet of paper is an ink lined plane
-	// an inclined plane is a slope up
-	// a slow pup is a lazy dog
+	SFX->SetRelativeLocation(FVector(7.5,-10.5,0.25));
 
 	static ConstructorHelpers::FObjectFinder<USoundBase>
 		CSnd(TEXT("/Game/LifeDev/Game/Inters/Paper00/Paper_SC.Paper_SC"));
 	SFX_Trigger = CSnd.Object;
 
 	AnimFade->SetNewMat();
+	Super::SetMobility(EComponentMobility::Static);
 }
