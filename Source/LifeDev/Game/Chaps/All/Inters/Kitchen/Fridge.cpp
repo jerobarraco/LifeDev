9e5c1da46f83024f57896a8fd223bd9207aaa9aa
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-02-18 20:07:14 +1300

    * add fridge interact

---
+++ Source/LifeDev/Game/Chaps/All/Inters/Kitchen/Fridge.cpp
@@ -0,0 +1,34 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#include "Fridge.h"
+
+#include "Components/AudioComponent.h"
+#include "Interact/CInteract.h"
+#include "JUtils/Actors/CQuickMesh.h"
+
+AFridge::AFridge():Super() {
+	Texts = { FText::FromString(TEXT("Fridge")) } ;
+	RewardFlash = 0;
+	UseAnim = false;
+	UseRewardFade = false;
+	StateNum = 1;
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		CMesh(TEXT("/Game/LifeDev/Game/Inters/Kitchen/Fridge.Fridge"));
+	Mesh->SetStaticMesh(CMesh.Object);
+	Mesh->SetRelativeLocation(FVector(60.5,17,0));
+	
+	Interact->SetRelativeLocation(FVector(-90,-12.5,90));
+	Interact->SetBoxExtent(FVector(35,7.5,85));
+	SFX->SetRelativeLocation(FVector(-90,-15,95));
+	
+	// stolen from paper. maybe get a new one?
+	static ConstructorHelpers::FObjectFinder<USoundBase>
+		CSnd(TEXT("/Game/LifeDev/Game/Inters/Generic/Grab_C.Grab_C"));
+	SFX_Trigger = CSnd.Object;
+
+	// set static by default
+	SetEnabled(false);
+	AFridge::SetMobility(EComponentMobility::Static);
+}
+
---
+++ Source/LifeDev/Game/Chaps/All/Inters/Kitchen/Fridge.h
@@ -0,0 +1,15 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#pragma once
+#include "LifeDev/Game/Interact/LInteract.h"
+
+#include "Fridge.generated.h"
+
+// Base class for Fridge. extend for instances.
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API AFridge: public ALInteract {
+	GENERATED_BODY()
+
+public:
+	AFridge();
+};
