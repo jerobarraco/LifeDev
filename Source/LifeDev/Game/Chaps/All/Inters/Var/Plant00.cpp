862f38fbad3c61fb681ba3c0f3d2dce614545174
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-02-27 19:00:12 +1300

    * Added plant sound

--- Source/LifeDev/Game/Chaps/All/Inters/Var/Plant00.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Var/Plant00.cpp
@@ -11,7 +11,11 @@ APlant00::APlant00():Super() {
 	UseAnim = false;
 	UseRewardFade = false;
 	StateNum = 1;
-	
+
+	static ConstructorHelpers::FObjectFinder<USoundBase>
+		CSFX(TEXT("/Game/LifeDev/Game/Inters/Plants/Bluezone_BC0254_wood_crushing_branches_001_001.Bluezone_BC0254_wood_crushing_branches_001_001"));
+	SFX_Trigger = CSFX.Object;
+
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CMesh(TEXT("/Game/LifeDev/Game/Inters/Plants/Pot00.Pot00"));
 	Mesh->SetStaticMesh(CMesh.Object);
