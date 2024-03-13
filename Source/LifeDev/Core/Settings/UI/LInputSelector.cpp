464601f227e08f2190b73d92a7244846cf0a1f43
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-03-13 19:59:25 +1300

    * improvements to the remapping options

--- Source/LifeDev/Core/Settings/UI/LInputSelector.cpp
+++ Source/LifeDev/Core/Settings/UI/LInputSelector.cpp
@@ -2,22 +2,29 @@
 
 #include "LInputSelector.h"
 
+#include "LifeDev/Game/Sys/Consts/ConstColors.h"
+
 ULInputSelector::ULInputSelector():Super() {
 	// ATM the enhanced input will make the key unusable when i rebind it
 	// Super::SetIsEnabled(false);
 
 	FTextBlockStyle S = GetTextStyle();
-	// TODO set proper colors
+	// TODO set proper colors (maybe wait after common ui)
 	S.Font.Size = 20;
+	S.ColorAndOpacity = FSlateColor (FColor(LDConsts::Colors::Palette[8][3]));
 	SetTextStyle(S);
+	
 
 	FButtonStyle BS = GetButtonStyle();
-	BS.Hovered.TintColor = FSlateColor(FColor::Red); 
+	BS.Hovered.TintColor = FSlateColor(FColor(LDConsts::Colors::Palette[3][3]));
 	SetButtonStyle(BS);
 
 	SetNoKeySpecifiedText(FText::FromString(TEXT("[None]")));
+	SetKeySelectionText(FText::FromString(TEXT("...")));
 	SetAllowModifierKeys(false);
-
+	EscapeKeys.Add(EKeys::Escape);
+	EscapeKeys.Add(EKeys::Gamepad_Special_Right);
+	
 	// SetToolTipText(FText::FromString(TEXT("Sorry. Rebinding is not available at the moment.")));
 }
 
