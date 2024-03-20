04ac860f1621450a16f72672ec51bc15ffe6fb2f
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-03-20 20:02:41 +1300

    * Added c4s00

---
+++ Source/LifeDev/Game/Chaps/04/Steps/LStepC4S000.cpp
@@ -0,0 +1,50 @@
+// Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
+#include "LStepC4S000.h"
+
+#include "Inventory/Inventory.h"
+#include "LifeDev/Game/Flashback/Flashback.h"
+#include "LifeDev/Game/Sys/Consts/ConstItems.h"
+#include "WorldPartition/DataLayer/DataLayerAsset.h"
+
+ALStepC4S000::ALStepC4S000():Super() {
+	Name = FName("C4S0");
+	static FText ST = FText::FromString("~ Acceptance ~");
+	Title = ST;
+	UseFade = true;
+	// DlgId = FName("C4S0"); // this chapter finishes after the dialog
+	InputEnabled = true;
+	UsePawnCam = true;
+	UseFadeTime = true;
+	FinishPostWait = false;
+	TeleportChar = true;
+
+	// TODO temporary until i get a new music
+	Music = FSoftObjectPath("/Game/LifeDev/Game/Env/Music/Music01/Music01_MS.Music01_MS");
+
+	static ConstructorHelpers::FObjectFinder<UDataLayerAsset>
+		CDL1 (TEXT("/Game/LifeDev/Game/Sys/DataLayers/Chaps/Chap04_DL.Chap04_DL"));
+	if (CDL1.Succeeded()) DL_Load.Add(CDL1.Object);
+	
+	static ConstructorHelpers::FObjectFinder<UDataLayerAsset>
+		CDL2 (TEXT("/Game/LifeDev/Game/Sys/DataLayers/Chaps/Chap03_DL.Chap03_DL"));
+	if (CDL2.Succeeded()) DL_Unload.Add(CDL2.Object);
+
+	ItemsEnsure = {
+		LDConsts::Items::Card0,
+		LDConsts::Items::Card1,
+		LDConsts::Items::Card2,
+		LDConsts::Items::Card3,
+		LDConsts::Items::Walkman
+	};
+}
+
+void ALStepC4S000::Start_Implementation() {
+	Super::Start_Implementation();
+	FB->SetMax(.4);
+	FB->SetVal(.01);
+}
+
+void ALStepC4S000::DoDebug_Implementation() {
+	Super::DoDebug_Implementation();
+	// if (Inventory) Inventory->Ensure(LDConsts::Items::Card3);
+}
