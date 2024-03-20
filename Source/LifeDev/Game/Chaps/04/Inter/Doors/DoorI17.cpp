9ca93b07be2292a0ecff3bbedf0807c9e69f3fd8
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-03-20 19:12:32 +1300

    * added doori17

---
+++ Source/LifeDev/Game/Chaps/04/Inter/Doors/DoorI17.cpp
@@ -0,0 +1,9 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#include "DoorI17.h"
+
+ADoorI17::ADoorI17():Super() {
+	UseAnim = false;
+	LockedDlg = "D17_L";
+	Locked = true;
+}
--- Source/LifeDev/Game/Chaps/04/Inter/Doors/DoorI17.h
+++ Source/LifeDev/Game/Chaps/04/Inter/Doors/DoorI17.h
@@ -3,13 +3,13 @@
 #pragma once
 #include "LifeDev/Game/Chaps/All/Inters/Doors/Doors/Door00.h"
 
-#include "DoorI16.generated.h"
+#include "DoorI17.generated.h"
 
-// Door from living00 to kitchen chap03
+// Door from livin00 to kitchen on chap04
 UCLASS(Blueprintable, BlueprintType)
-class LIFEDEV_API ADoorI16: public ADoor00 {
+class LIFEDEV_API ADoorI17: public ADoor00 {
 	GENERATED_BODY()
 
 public:
-	ADoorI16();
+	ADoorI17();
 };
