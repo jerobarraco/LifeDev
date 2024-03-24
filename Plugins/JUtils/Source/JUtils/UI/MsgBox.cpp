7158a79d109f17a26f9805985722d1be0b177707
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-03-24 20:35:42 +1300

    * implemented basic msg box wip

--- Plugins/JUtils/Source/JUtils/UI/MsgBox.cpp
+++ Plugins/JUtils/Source/JUtils/UI/MsgBox.cpp
@@ -2,14 +2,43 @@
 
 #include "MsgBox.h"
 
+#include "DelegateWrappers.h"
 #include "Components/Button.h"
 #include "Components/TextBlock.h"
 
 void UMsgBox::NativeOnInitialized() {
 	Super::NativeOnInitialized();
+
+	const TArray<UButton*> UBtns = {Btn0, Btn1, Btn2};
+	const int32 Num = UBtns.Num();
+	for (int32 i=0; i<Num; ++i) {
+		UButton* const B = UBtns[i];
+		if (!B) continue;
+
+		// the outer hangs to keep us all awake~ (and not get gcd) (doesn't work actually)
+		UDelegateWrapper* const Wrapper = NewObject<UDelegateWrapper>(this, UDelegateWrapper::StaticClass());
+		if (!IsValid(Wrapper)) continue;
+
+		Wrappers.AddUnique(Wrapper); // avoid getting gcd, actually needed
+		Wrapper->Obj = B;
+		Wrapper->ID = i;
+		Wrapper->OnDispatch.AddUniqueDynamic(this, &UMsgBox::BtnClick);
+		B->OnClicked.AddUniqueDynamic(Wrapper, &UDelegateWrapper::Dispatch);
+	}	
 }
 
-void UMsgBox::ShowButtons(const TArray<FText> Texts) {
+void UMsgBox::NativeDestruct() {
+	for (UDelegateWrapper* const W: Wrappers) {
+		if (!IsValid(W)) continue;
+		W->OnDispatch.RemoveAll(this);
+	}
+	Wrappers.Empty();
+
+	Super::NativeDestruct();
+}
+void UMsgBox::Init(const FText& Message, const TArray<FText>& Texts) {
+	Msg->SetText(Message);
+
 	const TArray<UButton*> UBtns = {Btn0, Btn1, Btn2};
 	const TArray<UTextBlock*> UTexts = {Text0, Text1, Text2};
 	const int32 Num = Texts.Num();
@@ -18,7 +47,7 @@ void UMsgBox::ShowButtons(const TArray<FText> Texts) {
 	for (int32 i=0; i<Num2; ++i) {
 		UButton* const B = UBtns[i];
 		if (!B) continue;
-		const FText& T = Texts[i];
+		FText T = i < Num ? Texts[i] : FText();
 		
 		const bool Show = i<Num && !T.IsEmpty();
 		B->SetVisibility( Show ? ESlateVisibility::Visible : ESlateVisibility::Collapsed);
@@ -28,4 +57,9 @@ void UMsgBox::ShowButtons(const TArray<FText> Texts) {
 		if (!Text) continue;
 		Text->SetText(T);
 	}
+}
+
+void UMsgBox::BtnClick(UDelegateWrapper* Wrapper, int32 ID, UObject* Obj) {
+	UE_LOG(LogTemp, Log, TEXT("Btn click id=%i"), ID);
+	Done(ID);
 }
--- Plugins/JUtils/Source/JUtils/UI/MsgBox.h
+++ Plugins/JUtils/Source/JUtils/UI/MsgBox.h
@@ -6,6 +6,7 @@
 
 #include "MsgBox.generated.h"
 
+class UDelegateWrapper;
 class UTextBlock;
 class UButton;
 
@@ -14,15 +15,22 @@ class JUTILS_API UMsgBox: public UBaseUI {
 	GENERATED_BODY()
 
 public:
-	void NativeOnInitialized() override;
-	
 	UFUNCTION(BlueprintCallable)
-	void ShowButtons(const TArray<FText> Texts);
+	void Init(const FText& Message, const TArray<FText>& Texts);
 	
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category=SetUp)
 	float AnimDuration = .5;
 
 protected:
+	virtual void NativeOnInitialized() override;
+	virtual void NativeDestruct() override;
+	
+	UFUNCTION()
+	void BtnClick(UDelegateWrapper* Wrapper, int32 ID, UObject* Obj);
+
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidget))
+	UTextBlock* Msg = nullptr;
+
 	UPROPERTY(BlueprintReadWrite, meta=(BindWidgetOptional))
 	UButton* Btn0 = nullptr;
 	UPROPERTY(BlueprintReadWrite, meta=(BindWidgetOptional))
@@ -36,5 +44,8 @@ protected:
 	UTextBlock* Text1 = nullptr;
 	UPROPERTY(BlueprintReadWrite, meta=(BindWidgetOptional))
 	UTextBlock* Text2 = nullptr;
-	
+
+private:
+	UPROPERTY(Transient)
+	TArray<UDelegateWrapper*> Wrappers;
 };
