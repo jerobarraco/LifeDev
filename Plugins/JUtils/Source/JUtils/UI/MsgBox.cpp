8c6f788cc0c1d8efbc2efe3cbb0fc4a8c6306da3
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-03-24 20:10:51 +1300

    * improved msgbox class

--- Plugins/JUtils/Source/JUtils/UI/MsgBox.cpp
+++ Plugins/JUtils/Source/JUtils/UI/MsgBox.cpp
@@ -2,3 +2,30 @@
 
 #include "MsgBox.h"
 
+#include "Components/Button.h"
+#include "Components/TextBlock.h"
+
+void UMsgBox::NativeOnInitialized() {
+	Super::NativeOnInitialized();
+}
+
+void UMsgBox::ShowButtons(const TArray<FText> Texts) {
+	const TArray<UButton*> UBtns = {Btn0, Btn1, Btn2};
+	const TArray<UTextBlock*> UTexts = {Text0, Text1, Text2};
+	const int32 Num = Texts.Num();
+	const int32 Num2 = UBtns.Num();
+	
+	for (int32 i=0; i<Num2; ++i) {
+		UButton* const B = UBtns[i];
+		if (!B) continue;
+		const FText& T = Texts[i];
+		
+		const bool Show = i<Num && !T.IsEmpty();
+		B->SetVisibility( Show ? ESlateVisibility::Visible : ESlateVisibility::Collapsed);
+		if (!Show) continue;
+		
+		UTextBlock* const Text = UTexts[i];
+		if (!Text) continue;
+		Text->SetText(T);
+	}
+}
--- Plugins/JUtils/Source/JUtils/UI/MsgBox.h
+++ Plugins/JUtils/Source/JUtils/UI/MsgBox.h
@@ -6,10 +6,35 @@
 
 #include "MsgBox.generated.h"
 
+class UTextBlock;
+class UButton;
+
 UCLASS(Blueprintable, BlueprintType)
 class JUTILS_API UMsgBox: public UBaseUI {
 	GENERATED_BODY()
 
 public:
+	void NativeOnInitialized() override;
+	
+	UFUNCTION(BlueprintCallable)
+	void ShowButtons(const TArray<FText> Texts);
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category=SetUp)
+	float AnimDuration = .5;
 
+protected:
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidgetOptional))
+	UButton* Btn0 = nullptr;
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidgetOptional))
+	UButton* Btn1 = nullptr;
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidgetOptional))
+	UButton* Btn2 = nullptr;
+
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidgetOptional))
+	UTextBlock* Text0 = nullptr;
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidgetOptional))
+	UTextBlock* Text1 = nullptr;
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidgetOptional))
+	UTextBlock* Text2 = nullptr;
+	
 };
