abd11eab4e8cafdf9659a48a38bffe2b2f43d093
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-03-28 19:43:56 +1300

    * fixed books random

--- Source/LifeDev/Game/Chaps/All/Inters/Var/Books.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Var/Books.cpp
@@ -21,7 +21,7 @@ void ABooks::CreateBooks() {
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CMesh(TEXT("/Game/LifeDev/Game/Inters/Books/BookP.BookP"));
 	// check against 0 to be able to use negative values as well
-	FRandomStream RS(RndSeed == 0 ?  FMath::Rand()*MAX_int32 : RndSeed); // not static
+	FRandomStream RS(RndSeed == 0 ?  FMath::Rand() : RndSeed); // not static
 	
 	const int32 MatMax = Materials.Num() -1;
 	for (int32 i =0; i<BookCount; ++i) {
