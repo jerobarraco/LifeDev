a9096f54371ac73baa246e8a2ffe26ea05e02b2a
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-03-28 19:22:49 +1300

    * basic plate instance

---
+++ Source/LifeDev/Game/Chaps/04/Inters/Var/PlateI00.cpp
@@ -0,0 +1,7 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#include "PlateI00.h"
+
+APlateI00::APlateI00():Super() {
+}
+
---
+++ Source/LifeDev/Game/Chaps/04/Inters/Var/PlateI00.h
@@ -0,0 +1,14 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#pragma once
+#include "LifeDev/Game/Chaps/All/Inters/Kitchen/Plate.h"
+
+#include "PlateI00.generated.h"
+
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API APlateI00: public APlate {
+	GENERATED_BODY()
+
+public:
+	APlateI00();
+};
