e2e49552a7417acb416e3b47693ba2546d070ff7
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-04-01 20:04:48 +1300

    * adjust sponge

--- Source/LifeDev/Game/Chaps/04/Inters/Var/SpongeI00.cpp
+++ Source/LifeDev/Game/Chaps/04/Inters/Var/SpongeI00.cpp
@@ -3,5 +3,11 @@
 #include "SpongeI00.h"
 
 ASpongeI00::ASpongeI00():Super() {
+	
+	LockedDlg = "Sponge00_L";
+	RewardItem = "Plates";
+	Locked = true;
+	UseRewardFade = false;
+	IsOneShot = true;
+	// TODO if i have time, on trigger, fade out, then fade in and do reward
 }
-
