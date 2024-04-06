1120f5dd1946892738039e45200d85c9324ec0d0
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-04-06 20:06:18 +1300

    * cleanup

--- Source/LifeDev/Game/Chaps/04/Steps/LStepC4S001.h
+++ Source/LifeDev/Game/Chaps/04/Steps/LStepC4S001.h
@@ -20,6 +20,7 @@ protected:
 	virtual void BeginPlay() override;
 	virtual void DoDebug_Implementation() override;
 	virtual void DoStart_Implementation() override;
+
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category=SetUp)
 	TArray<ALInteract*> Chars;
 };
