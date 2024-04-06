f6be502c937884a8fed167a4e501e4bbbb96877e
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-04-06 18:20:03 +1300

    * added c4s1

---
+++ Source/LifeDev/Game/Chaps/04/Steps/LStepC4S001.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
+#include "LStepC4S001.h"
+
+#include "LifeDev/Game/Interact/LInteract.h"
+
+ALStepC4S001::ALStepC4S001():Super() {
+	Name = FName("C4S1");
+	UseFade = false;
+	// DlgId = FName("C4S0"); // this chapter finishes after the dialog
+	InputEnabled = true;
+	UsePawnCam = true;
+	FinishPostWait = false;
+	TeleportChar = false;
+}
+
+void ALStepC4S001::BeginPlay() {
+	Super::BeginPlay();
+
+	for (ALInteract* const I: Chars) {
+		I->Fade(false);
+	}
+}
+
+void ALStepC4S001::Start_Implementation() {
+	Super::Start_Implementation();
+}
+
+void ALStepC4S001::DoDebug_Implementation() {
+	Super::DoDebug_Implementation();
+}
---
+++ Source/LifeDev/Game/Chaps/04/Steps/LStepC4S001.h
@@ -0,0 +1,26 @@
+// Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LifeDev/Core/Story/LStep.h"
+
+#include "LStepC4S001.generated.h"
+
+class ALInteract;
+
+UCLASS(Blueprintable, BlueprintType)
+class ALStepC4S001 : public ALStep {
+	GENERATED_BODY()
+
+public:
+	ALStepC4S001();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Start_Implementation() override;
+	virtual void DoDebug_Implementation() override;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category=SetUp)
+	TArray<ALInteract*> Chars;
+};
