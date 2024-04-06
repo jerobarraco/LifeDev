8bbcb68c095afd8fab3c6eef683d69e4ad75e98e
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-04-06 17:57:12 +1300

    * adjusted booki01

--- Source/LifeDev/Game/Chaps/04/Inters/Var/BooksI01.cpp
+++ Source/LifeDev/Game/Chaps/04/Inters/Var/BooksI01.cpp
@@ -5,6 +5,8 @@
 ABooksI01::ABooksI01():Super(1, 0) {
 	SetEnabled(true);
 	Texts = { FText::FromString(TEXT("Book"))};
-	// TriggerDlg = "BK00_T";
+	TriggerDlg = "BK00_T";
 	UseRewardFade = false;
+	IsOneShot = true;
+	Locked = true;
 }
