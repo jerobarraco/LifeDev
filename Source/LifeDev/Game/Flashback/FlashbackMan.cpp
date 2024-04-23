045a73e7f71f117426ff6cf0f57a4e419573261a
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-04-23 18:50:37 +1200

    * fix possible crash on FlashbackMan.cpp

--- Source/LifeDev/Game/Flashback/FlashbackMan.cpp
+++ Source/LifeDev/Game/Flashback/FlashbackMan.cpp
@@ -15,26 +15,39 @@ AFlashbackMan::AFlashbackMan():Super() {
 
 void AFlashbackMan::ValChanged(float Value) {
 	// UE_LOG(LogTemp, Log, TEXT("Flashback value changed to %3.3f"), Value);
-	if (IsValid(MPCInst)) {
-		MPCInst->SetScalarParameterValue("Intensity", Value);
-	}
+	if (!IsValid(MPCInst)) return;
+	MPCInst->SetScalarParameterValue("Intensity", Value);
 }
 
 void AFlashbackMan::BeginPlay() {
 	Super::BeginPlay();
 
 	UWorld* const World = GetWorld();
+	if (!World) return;
+
 	UFlashback* const Flashback = World->GetSubsystem<UFlashback>();
 	if (IsValid(Flashback)) {
 		Flashback->OnChange.AddUniqueDynamic(this, &AFlashbackMan::ValChanged);
 	}
 	
-	if (IsValid(MPC)) {
-		MPCInst = World->GetParameterCollectionInstance(MPC);
-		const int Strobe = ULSettings::GetFeatS(World, EFeat::A_STROBE)?1:0;
-		MPCInst->SetScalarParameterValue("Strobe", Strobe);
-		// TODO set the flashback feat as well
+	if (!IsValid(MPC)) {
+		UE_LOG(LogTemp, Warning, TEXT("FlashbackMan::%hs Could not get the MPC. Stop"),
+			__func__);
+		return;
 	}
+	
+	MPCInst = World->GetParameterCollectionInstance(MPC);
+	
+	if (!IsValid(MPCInst)){
+		UE_LOG(LogTemp, Warning, TEXT("FlashbackMan::%hs Could not get the MPCInst. Stop."),
+			__func__);
+		return;
+	} 
+	
+	const int Strobe = ULSettings::GetFeatS(World, EFeat::A_STROBE)?1:0;
+	MPCInst->SetScalarParameterValue("Strobe", Strobe);
+	// TODO set the flashback feat as well.
+	// -- what was this?
 }
 
 void AFlashbackMan::EndPlay(const EEndPlayReason::Type EndPlayReason) {
