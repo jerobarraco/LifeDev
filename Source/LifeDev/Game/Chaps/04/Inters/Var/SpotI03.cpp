9aa8e3237247b5e22d8a2c8842e8963aaa18ea76
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-05-02 20:19:35 +1200

    * c4s9

--- Source/LifeDev/Game/Chaps/04/Inters/Var/SpotI03.cpp
+++ Source/LifeDev/Game/Chaps/04/Inters/Var/SpotI03.cpp
@@ -7,8 +7,9 @@
 ASpotI03::ASpotI03():Super() {
 	// DropDlg = TEXT("IS0_D*");
 	TriggerDlg = "IS3_T"; // when completed
-	LockedDlg = "IS3_L"; // when not full
-	LockedFullDlg = "IS3_LF"; // when full
+	LockedDlg = "IS3_L"; // when not full, try trigger
+	LockedFullDlg = ""; // when full, try trigger. after 1st trigger.
+	DropDlg = "";
 	DropFullDlg = ""; // when full
 	RewardStep = true; // moves to the next
 	Items = { LDConsts::Items::Plate02 }; // empty plate
