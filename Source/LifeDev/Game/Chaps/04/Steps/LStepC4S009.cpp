da154b6933d7573ac479799d836cc396d4b5dce7
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-05-27 12:03:53 +1200

    * set min on c4s9

--- Source/LifeDev/Game/Chaps/04/Steps/LStepC4S009.cpp
+++ Source/LifeDev/Game/Chaps/04/Steps/LStepC4S009.cpp
@@ -1,6 +1,8 @@
 // Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
 #include "LStepC4S009.h"
 
+#include "LifeDev/Game/Flashback/Flashback.h"
+
 ALStepC4S009::ALStepC4S009():Super() {
 	Name = FName("C4S9");
 	DlgId = Name; // this chapter finishes after the dialog
@@ -17,4 +19,10 @@ ALStepC4S009::ALStepC4S009():Super() {
 void ALStepC4S009::BeginPlay() {
 	Super::BeginPlay();
 	DoIntersFade(IntersFadeIn, false); // forcefully hide the ones we're showing.
+}
+
+void ALStepC4S009::Start_Implementation() {
+	Super::Start_Implementation();
+	// for this we want to really reach 0 since it's the very end (for dramatic purposes, not realism). 
+	if (FB) FB->SetMin(0);
 }
--- Source/LifeDev/Game/Chaps/04/Steps/LStepC4S009.h
+++ Source/LifeDev/Game/Chaps/04/Steps/LStepC4S009.h
@@ -17,4 +17,5 @@ public:
 
 protected:
 	virtual void BeginPlay() override;
+	virtual void Start_Implementation() override;
 };
