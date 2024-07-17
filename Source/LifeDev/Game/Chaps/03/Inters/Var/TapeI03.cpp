0de7ffa35c2365fbfcd2e88579721cae519a253e
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-07-17 19:52:43 +1200

    * stub TapeI03.cpp

---
+++ Source/LifeDev/Game/Chaps/03/Inters/Var/TapeI03.cpp
@@ -0,0 +1,26 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#include "TapeI03.h"
+
+#include "LifeDev/Game/Sys/Consts/ConstItems.h"
+
+ATapeI03::ATapeI03():Super() {
+	Texts = { FText::FromString(TEXT("One more tape")) };
+	StateNum = 1;
+	UseAnim = false;
+	RewardFlash = .1;
+	// TODO should just simply trigger the effect instead of adding an item?
+	// RewardItem = "T03";
+	RewardItem = NAME_None;
+	UseRewardDestroy = true;
+	UseFade = true;
+	Locked = true;
+
+	ULockItem = LDConsts::Items::Batts;
+	LockedDlg = FName("T03_L");
+	LockedItemDlg = FName("T03_LI"); // will hint to use the batts
+	TriggerDlg = FName("T03_Look*"); // will say what it is. triggered on pick up
+
+	// needed to be able to attach to the drawer
+	// Super::SetMobility(EComponentMobility::Movable);
+}
---
+++ Source/LifeDev/Game/Chaps/03/Inters/Var/TapeI03.h
@@ -0,0 +1,15 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#pragma once
+#include "LifeDev/Game/Chaps/All/Inters/Var/Tape00.h"
+
+#include "TapeI03.generated.h"
+
+// Tape Instance 02. Chapter 02 has some song.
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API ATapeI03: public ATape00 {
+	GENERATED_BODY()
+
+public:
+	ATapeI03();
+};
