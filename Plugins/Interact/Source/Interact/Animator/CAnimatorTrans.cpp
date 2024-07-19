4c951338c32a459f78424c64a691524ff193160b
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-07-19 19:14:25 +1200

    * cleanup

--- Plugins/Interact/Source/Interact/Animator/CAnimatorTrans.cpp
+++ Plugins/Interact/Source/Interact/Animator/CAnimatorTrans.cpp
@@ -14,10 +14,11 @@ void UCAnimatorTrans::Update_Implementation(float Alpha) {
 
 	FTransform TNew = TStart;
 	if (IsAdditive) {
-		TStart.BlendFromIdentityAndAccumulate(TNew, TEnd, (const ScalarRegister) Alpha);
+		TStart.BlendFromIdentityAndAccumulate(
+			TNew, TEnd, (const ScalarRegister) Alpha);
 		// TNew.Accumulate(TEnd, (const ScalarRegister) Alpha); // not what i want, does something different with the scale.
 	}else {
-		// Thanks Tim! this actually works very well!
+		// Thanks, Tim! this actually works very well!
 		TNew.BlendWith(TEnd, Alpha);
 	}
 	TRoot->SetRelativeTransform(TNew);
@@ -25,7 +26,6 @@ void UCAnimatorTrans::Update_Implementation(float Alpha) {
 
 void UCAnimatorTrans::BeginPlay() {
 	Super::BeginPlay();
-	if (IsValid(TRoot) && IsAdditive) {
+	if (IsValid(TRoot) && IsAdditive)
 		TStart = TRoot->GetRelativeTransform();
-	}
 }
