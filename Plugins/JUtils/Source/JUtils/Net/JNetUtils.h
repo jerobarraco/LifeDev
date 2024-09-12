0a7cc3a69b03e658729ff0a91a53dd320ebac5c8
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-09-13 06:21:42 +1200

    * added JNetUtils.h

---
+++ Plugins/JUtils/Source/JUtils/Net/JNetUtils.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "JNetUtils.generated.h"
+
+UCLASS(Blueprintable)
+class JUTILS_API UJNetUtils: public UBlueprintFunctionLibrary {
+	GENERATED_BODY()
+public:
+	
+};
