6ce9b1dc219d2f8b874a90181411b65ad7d21846
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-09-15 09:40:58 +1200

    * stub sceneview

---
+++ Plugins/JScene/Source/JScene/JSceneView.cpp
@@ -0,0 +1,8 @@
+// Copyright (C) 2023 - Jeronimo Barraco-Marmol. All rights reserved.
+// SPDX-License-Identifier: LGPL-3.0-only
+#include "JSceneView.h"
+
+#include "SceneViewExtension.h"
+
+DEFINE_LOG_CATEGORY_STATIC(LogJSceneView, Log, Log);
+
---
+++ Plugins/JScene/Source/JScene/JSceneView.h
@@ -0,0 +1,12 @@
+// Copyright (C) 2023 - Jeronimo Barraco-Marmol. All rights reserved.
+// SPDX-License-Identifier: LGPL-3.0-only
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SceneViewExtension.h"
+
+class JSCENE_API FJSceneView : public FSceneViewExtensionBase {
+
+public:
+};
