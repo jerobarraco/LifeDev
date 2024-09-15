9db6e77e9a750f5d43d85c8514aed7b0522a1bf0
Jeromino Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-09-15 14:45:56 +1200

    * cleanup

--- Source/LifeDev/Game/Env/Ghost/GhostPool.cpp
+++ Source/LifeDev/Game/Env/Ghost/GhostPool.cpp
@@ -1,10 +1,11 @@
 // Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
 #include "GhostPool.h"
 
-#include "GhostItem.h"
 #include "Pool.h"
+
 #include "Interact/Animator/CRandomizer.h"
 #include "LifeDev/Game/Flashback/Flashback.h"
+#include "GhostItem.h"
 
 AGhostPool::AGhostPool():Super() {
 	ItemClass = AGhostItem::StaticClass();
@@ -67,22 +68,3 @@ void AGhostPool::Spawn() {
 	// this could return null on many situations. beware.
 	Pooler->Get(ItemClass);
 }
-
-
-/*
-inline static int bitselect(int condition, int truereturnvalue, int falsereturnvalue)
-{
-	return (truereturnvalue & -condition) | (falsereturnvalue & ~(-condition)); //a when TRUE and b when FALSE
-}
-
-inline static float bitselect(int condition, float truereturnvalue, float falsereturnvalue)
-{
-	//Reinterpret floats. Would work because it's just a bit select, no matter the actual value
-	int& at = reinterpret_cast<int&>(truereturnvalue);
-	int& af = reinterpret_cast<int&>(falsereturnvalue);
-	int res = (at & -condition) | (af & ~(-condition)); //a when TRUE and b when FALSE
-	return  reinterpret_cast<float&>(res);
-}
-https://stackoverflow.com/a/30084391
-
-*/
