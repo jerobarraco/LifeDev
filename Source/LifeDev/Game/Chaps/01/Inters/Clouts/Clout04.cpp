62bc4802e3a97c1fd39872d023630d867ac43d2b
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-10-13 18:21:32 +1300

    * fixed material for sock01

--- Source/LifeDev/Game/Chaps/01/Inters/Clouts/Clout04.cpp
+++ Source/LifeDev/Game/Chaps/01/Inters/Clouts/Clout04.cpp
@@ -3,17 +3,19 @@
 #include "Clout04.h"
 
 #include "Interact/CInteract.h"
+#include "Interact/Animator/CAnimatorFade.h"
 #include "JUtils/Actors/CQuickMesh.h"
 
 AClout04::AClout04():Super() {
-	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		ObjMesh(TEXT("/Game/LifeDev/Game/Inters/Clouts/Sock00.Sock00"));
-	if (ObjMesh.Succeeded()) {
-		Mesh->SetStaticMesh(ObjMesh.Object);
-	}
 	RewardItem = TEXT("C1C04"); // chapter 1 clout 4
 	TriggerDlg = FName("C04");
+	
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		ObjMesh(TEXT("/Game/LifeDev/Game/Inters/Clouts/Sock00.Sock00"));
+	if (ObjMesh.Succeeded())
+		Mesh->SetStaticMesh(ObjMesh.Object);
 	Mesh->SetRelativeLocation(FVector(-10.000000,10.000000,-2.500000));
 	Interact->SetRelativeLocation(FVector(10.000000,-10.000000,2.500000));
 	Interact->SetBoxExtent(FVector(10.000000,10.000000,2.500000));
+	AnimFade->SetNewMat();
 }
