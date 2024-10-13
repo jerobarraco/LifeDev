fcddc5bc9297cdcbdc4d2cb5c896cf6ed09f8333
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-10-13 18:23:14 +1300

    * fixed Clout00.cpp material

--- Source/LifeDev/Game/Chaps/01/Inters/Clouts/Clout00.cpp
+++ Source/LifeDev/Game/Chaps/01/Inters/Clouts/Clout00.cpp
@@ -3,17 +3,19 @@
 #include "Clout00.h"
 
 #include "Interact/CInteract.h"
+#include "Interact/Animator/CAnimatorFade.h"
 #include "JUtils/Actors/CQuickMesh.h"
 
 AClout00::AClout00():Super() {
+	RewardItem = TEXT("C1C00"); // chapter 1 clout 0
+	TriggerDlg = FName("C00");
+
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		ObjMesh(TEXT("/Game/LifeDev/Game/Inters/Clouts/Shirt00.Shirt00"));
-	if (ObjMesh.Succeeded()) {
+	if (ObjMesh.Succeeded())
 		Mesh->SetStaticMesh(ObjMesh.Object);
-	}
-	RewardItem = TEXT("C1C00"); // chapter 1 clout 0
 	Mesh->SetRelativeLocation(FVector(-17.500000,17.500000,-0.000000));
 	Interact->SetRelativeLocation(FVector(17.500000,-17.500000,2.500000));
 	Interact->SetBoxExtent(FVector(18.000000,18.000000,3.000000));
-	TriggerDlg = FName("C00");
+	AnimFade->SetNewMat();
 }
