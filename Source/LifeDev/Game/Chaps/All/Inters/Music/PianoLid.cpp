d574294bed4bc40cac4db833204d72455911b25c
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-10-16 19:31:28 +1300

    * fixed pianolid autoactivate

--- Source/LifeDev/Game/Chaps/All/Inters/Music/PianoLid.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Music/PianoLid.cpp
@@ -13,7 +13,7 @@ APianoLid::APianoLid():Super() {
 	UseAnim = true;
 	UseFade = false;
 	DisableWhileAnim = false; // avoid re-enabling the Interact once triggered
-	SetActive(false); // not manually triggerable
+	Super::SetAutoActivate(false); // not manually triggerable
 
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CMesh (TEXT("/Game/LifeDev/Game/Inters/Music/Piano/Piano_Lid.Piano_Lid"));
@@ -37,7 +37,7 @@ APianoLid::APianoLid():Super() {
 
 void APianoLid::PostInitializeComponents() {
 	Super::PostInitializeComponents();
-	// return;
+
 	UCodeCurveLib* const CurveLib = UCodeCurveLib::Instance();
 	Anim->CodeCurve.BindDynamic(CurveLib, &UCodeCurveLib::InExpo);
 }
