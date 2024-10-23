2c11b6dd373ee52c48deab26f4b614d4cf42f611
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-10-23 18:45:09 +1300

    * picture 01

---
+++ Source/LifeDev/Game/Chaps/All/Inters/Papers/Picture01.h
@@ -0,0 +1,14 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#pragma once
+#include "LifeDev/Game/Interact/LInteract.h"
+
+#include "Picture01.generated.h"
+
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API APicture01: public ALInteract {
+	GENERATED_BODY()
+
+public:
+	APicture01();
+};
