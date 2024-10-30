3ceb8b23b15b89c3a591294ab28c8952a829dd33
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-10-30 19:32:57 +1300

    * added HeaterI03.cpp * modified heaters position

---
+++ Source/LifeDev/Game/Chaps/03/Inters/Var/HeaterI03.cpp
@@ -0,0 +1,15 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#include "HeaterI03.h"
+
+AHeaterI03::AHeaterI03():Super() {
+	UseAnim = false;
+	UseFade = false;
+	UseRewardDestroy = false;
+	Locked = true;
+	
+	LockedDlg = "HT03_L";
+	Texts = { FText::FromString(TEXT("Turn on")) };
+
+	Super::SetAutoActivate(true);
+}
---
+++ Source/LifeDev/Game/Chaps/03/Inters/Var/HeaterI03.h
@@ -0,0 +1,15 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#pragma once
+#include "LifeDev/Game/Chaps/All/Inters/Var/Heater00.h"
+
+#include "HeaterI03.generated.h"
+
+// chap03 Heater Instance 03
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API AHeaterI03: public AHeater00 {
+	GENERATED_BODY()
+
+public:
+	AHeaterI03();
+};
