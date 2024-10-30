061b87b5182f86f0834d15d8f0d6f42e5bad57b4
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-10-30 20:24:25 +1300

    * added stub CloutI00.cpp

---
+++ Source/LifeDev/Game/Chaps/01/Inters/Clouts/CloutI00.cpp
@@ -0,0 +1,8 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#include "CloutI00.h"
+
+ACloutI00::ACloutI00():Super() {
+	RewardItem = TEXT("C1C00"); // chapter 1 clout 0
+	TriggerDlg = FName("C00");
+}
---
+++ Source/LifeDev/Game/Chaps/01/Inters/Clouts/CloutI00.h
@@ -0,0 +1,15 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#pragma once
+#include "Clout00.h"
+
+#include "CloutI00.generated.h"
+
+// Base class for CloutI00 actors
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API ACloutI00: public AClout00 {
+	GENERATED_BODY()
+
+public:
+	ACloutI00();
+};
