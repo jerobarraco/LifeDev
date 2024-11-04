ab3c7602a828f121f55518d4caaa1e94fa5cb803
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-11-04 20:43:17 +1300

    * cleanup

--- Source/LifeDev/Core/Settings/LSettings.cpp
+++ Source/LifeDev/Core/Settings/LSettings.cpp
@@ -131,8 +131,7 @@ void ULSettings::LoadGameDone(const FString& Slot, int32 Index, USaveGame* Loade
 }
 
 int32 ULSettings::CurrentChapter() const {
-	// is valid this is important to avoid segfaults that should not happen
-	return IsValid(this) && IsValid(Save) ? Save->ChapterID : -1;
+	return IsValid(Save) ? Save->ChapterID : -1;
 }
 
 EFeat ULSettings::CurrentChapterFeat() const {
