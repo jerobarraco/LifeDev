5dcf218c81fdf79b941b3efba0ee13e19da98c38
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-11-04 19:36:51 +1300

    * added stub PictureI01.cpp * changed in-level picture to the new one

---
+++ Source/LifeDev/Game/Chaps/03/Inters/Var/PictureI01.cpp
@@ -0,0 +1,17 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#include "PictureI01.h"
+
+#include "CQuickMesh.h"
+
+APictureI01::APictureI01():Super() {
+	Texts = { FText::FromString(TEXT("Picture"))};
+	TriggerDlg = "PIC00_T";
+	IsOneShot = true;
+	RewardFlash = .09;
+	static ConstructorHelpers::FObjectFinder<UMaterialInterface>
+		CMat (TEXT("/Game/LifeDev/Game/Var/Mats/Voxel/Palettes/Palette00_DMI"));
+	if (CMat.Succeeded()) Mesh->SetMaterial(0, CMat.Object.Get());
+	APictureI01::SetAutoActivate(true);
+	// TODO, maybe i could add some card interaction to this?
+}
---
+++ Source/LifeDev/Game/Chaps/03/Inters/Var/PictureI01.h
@@ -0,0 +1,15 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#pragma once
+#include "LifeDev/Game/Chaps/All/Inters/Papers/Picture01.h"
+
+#include "PictureI01.generated.h"
+
+// chap03 big picture instance 01
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API APictureI01: public APicture01 {
+	GENERATED_BODY()
+
+public:
+	APictureI01();
+};
