af14c69dc566426a2eb6bfaab6ae308940b7214d
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-11-10 19:41:59 +1300

    * CodeCurve.h instance for bps

--- Plugins/JUtils/Source/JUtils/Math/CodeCurve.h
+++ Plugins/JUtils/Source/JUtils/Math/CodeCurve.h
@@ -16,6 +16,7 @@ UCLASS(Blueprintable)
 class JUTILS_API UCodeCurveLib: public UBlueprintFunctionLibrary {
 	GENERATED_BODY()
 public:
+	UFUNCTION(BlueprintCallable)
 	static UCodeCurveLib* Instance();
 	
 	// cant bind a const not static : lame :(
