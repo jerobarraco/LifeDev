dcf0eaa25ed79257799098d532eb9c1fd704d288
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-11-11 20:10:12 +1300

    * D14XC02

--- Source/LifeDev/Game/Chaps/03/Inters/Doors/DoorI14.cpp
+++ Source/LifeDev/Game/Chaps/03/Inters/Doors/DoorI14.cpp
@@ -5,7 +5,7 @@
 #include "LifeDev/Core/Consts/ConstItems.h"
 
 ADoorI14::ADoorI14():Super() {
-	LockedDlg = "D14_L";
+	LockedDlg = "D14_L"; // unnecessary since it uses the card1. but in case i re organize the chapter.
 	LockedItemDlg = "D14_LI";
 	Locked = true;
 	ULockDlg = "D14_U";
@@ -14,7 +14,7 @@ ADoorI14::ADoorI14():Super() {
 	
 	UseItemDlgs = {
 		{LDConsts::Items::Card0, "D14xC00"},
-		// {LDConsts::Items::Card1, "D14xC01"},
+		// {LDConsts::Items::Card1, "D14xC01"}, // done with the ULockDlg
 		{LDConsts::Items::Card2, "D14xC02"},
 	};
 }
