fb0601307eca8a7c89f71f31c8410ecb533dc465
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-11-20 20:28:06 +1300

    * cleanup

--- Source/LifeDev.Target.cs
+++ Source/LifeDev.Target.cs
@@ -10,13 +10,13 @@ public class LifeDevTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
 		// OptimizationLevel = OptimizationMode.SizeAndSpeed;
-		// bUseExecCommnadInShipping
 		ExtraModuleNames.Add("LifeDev");
 		
 		// to enable logs on shipping https://dev.epicgames.com/community/learning/knowledge-base/vzvZ/unreal-engine-enabling-logging-in-shipping-builds
 		if (Target.Platform == UnrealTargetPlatform.Linux) {
 			BuildEnvironment = TargetBuildEnvironment.UniqueIfNeeded;
 			bUseLoggingInShipping = true;
+			// bUseExecCommnadInShipping
 		} else if (Target.Platform == UnrealTargetPlatform.Win64) {
 			// can't do on windows because i have a "Installed" build. which can only do shared.
 			// https://forums.unrealengine.com/t/targets-with-a-unique-build-environment-cannot-be-built-with-an-installed-engine/1353217/2
