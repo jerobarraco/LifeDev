532fc51e1c96b9087445652310071242b3a4ba6d
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-11-20 19:29:49 +1300

    * improve Doorish.cpp defaults

--- Source/LifeDev/Game/Interact/Base/Doorish.cpp
+++ Source/LifeDev/Game/Interact/Base/Doorish.cpp
@@ -8,13 +8,14 @@
 #include "Interact/Animator/CAnimatorFade.h"
 
 ADoorish::ADoorish():Super() {
+	UseFade = false;
+	AnimFade->Meshes.Empty(); // avoid getting the material scrambled
+	UseRewardDestroy = false; // will also avoid scrambled material
+	
 	UseAnim = true;
 	Anim->IsAdditive = true;
 	// visual: run at 60 fps since it's movement
 	Anim->SetComponentTickInterval(1/60.f);
-	
-	AnimFade->Meshes.Empty(); // avoid getting the material scrambled
-	UseRewardDestroy = false; // will also avoid scrambled material
 
 	Mesh->SetCastAllShadows(true);
 	// important to call Super here. since this is called before the child constructor
