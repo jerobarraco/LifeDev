30b5519dee017af64aa7aeaa438ac83651bfcd89
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-11-25 19:36:46 +1300

    * fix crash on hotreload

--- Source/LifeDev/LifeDev.cpp
+++ Source/LifeDev/LifeDev.cpp
@@ -1,18 +1,27 @@
 // Copyright Jeronimo Barraco-Marmol
 
 #include "LifeDev.h"
-#include "Modules/ModuleManager.h"
 
-IMPLEMENT_PRIMARY_GAME_MODULE( FLifeDevModule, LifeDev, "LifeDev" );
+#include "Modules/ModuleManager.h"
+#include "ShaderCore.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FLifeDevModule, LifeDev, "LifeDev");
 
 void FLifeDevModule::StartupModule() {
 	IModuleInterface::StartupModule();
 	
 	// https://biq.medium.com/configuring-unreal-engine-for-custom-shader-development-biq-cf79f72e7137
 	const FString& ShaderDirectory = FPaths::Combine(FPaths::ProjectDir(), TEXT("Shaders"));
-	UE_LOG(LogTemp, Log, TEXT("LifeDev Shaders path '%s'"), *ShaderDirectory);
-	AddShaderSourceDirectoryMapping(TEXT("/Project/Shaders/LD"),
-		ShaderDirectory);
+	const FString VirtualShaderDirectory = TEXT("/Project/Shaders/LD");
+	// avoid crash on hotreload. yes. hotreload. and yes. a crash.
+	if (AllShaderSourceDirectoryMappings().Contains(VirtualShaderDirectory)) {
+		UE_LOG(LogTemp, Warning, TEXT("%hs Shader directory already exists. dir=%s"),
+			__func__, *VirtualShaderDirectory);
+		return;
+	}
+
+	UE_LOG(LogTemp, Log, TEXT("%hs LifeDev Shaders path '%s'"), __func__, *ShaderDirectory);
+	AddShaderSourceDirectoryMapping(VirtualShaderDirectory, ShaderDirectory);
 
 	// this won't work. probably is overriden by the engine. just go to Editor settings > Appearance > Application Scale
 	// FSlateApplication::Get().SetApplicationScale(1.3);
