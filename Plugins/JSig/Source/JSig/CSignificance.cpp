b7dc4dcd67b94368bd825f837c94f45e34a7bff6
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-11-25 19:13:44 +1300

    * added ghostpool to watched save flags

--- Plugins/JSig/Source/JSig/CSignificance.cpp
+++ Plugins/JSig/Source/JSig/CSignificance.cpp
@@ -1,6 +1,5 @@
 // Copyright (C) 2023 - Jeronimo Barraco-Marmol. All rights reserved.
 // SPDX-License-Identifier: LGPL-3.0-only
-// Probably copyright of Tom Looman too based on https://github.com/tomlooman/ActionRoguelike/blob/master/Source/ActionRoguelike/Private/Components/SSignificanceComponent.cpp
  
 #include "CSignificance.h"
 
