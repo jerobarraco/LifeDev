291ac8ea4aff6288e9ddd564980d69df6434861a
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-12-18 19:28:55 +1300

    * stove unlock

--- Source/LifeDev/Game/Chaps/04/Inters/Var/StoveI00.cpp
+++ Source/LifeDev/Game/Chaps/04/Inters/Var/StoveI00.cpp
@@ -2,9 +2,14 @@
 
 #include "StoveI00.h"
 
+#include "LifeDev/Core/Consts/ConstItems.h"
+
 AStoveI00::AStoveI00():Super() {
 	Locked = false;
-	// TriggerDlg = "Stove00_T";
+	TriggerDlg = "Stove00_T";
+	LockedDlg = "Stove00_L";
+	LockedItemDlg = "Stove00_LI";
+	ULockItem = LDConsts::Items::Matches00;
 	// RewardStep = true; // last item in the interaction chain for this step.
 	Super::SetAutoActivate(false);
 }
