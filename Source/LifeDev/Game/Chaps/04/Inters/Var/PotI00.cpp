31f24209242264273bb3cf5e1d4ffded8a26008f
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-12-19 20:30:03 +1300

    * cleanup

--- Source/LifeDev/Game/Chaps/04/Inters/Var/PotI00.cpp
+++ Source/LifeDev/Game/Chaps/04/Inters/Var/PotI00.cpp
@@ -75,7 +75,7 @@ void APotI00::DoTrigger_Implementation() {
 		// Could set the text here. but since it's deactivated it does not matter.
 		SFX_Trigger = nullptr; // no sound after
 		SetActive(false); // no more interaction for you
-		// not advancing the story here. it will advance when the player uses the plate on the chair (spot
+		// not advancing the story here. it will advance when the player uses the plate on the chair (spot)
 	}
 }
 
