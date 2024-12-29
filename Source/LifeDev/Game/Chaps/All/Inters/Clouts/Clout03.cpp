bdf8d6178aa71bb5abf37e5e4e16d4e6edf62376
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2024-12-29 20:06:44 +1300

    * re-export Bra00.gox Clout03.cpp

--- Source/LifeDev/Game/Chaps/All/Inters/Clouts/Clout03.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Clouts/Clout03.cpp
@@ -2,11 +2,12 @@
 
 #include "Clout03.h"
 
-#include "Interact/CInteract.h"
 #include "JUtils/Actors/CQuickMesh.h"
 
+#include "Interact/CInteract.h"
+#include "Interact/Animator/CAnimatorFade.h"
+
 AClout03::AClout03():Super() {
-	// TODO re-export
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		ObjMesh(TEXT("/Game/LifeDev/Game/Inters/Clouts/Bra00.Bra00"));
 	if (LIKELY(ObjMesh.Succeeded())) Mesh->SetStaticMesh(ObjMesh.Object);
@@ -14,4 +15,6 @@ AClout03::AClout03():Super() {
 	Mesh->SetRelativeLocation(FVector(67.500000,-37.500000,0.000000));
 	Interact->SetRelativeLocation(FVector(-67.500000,37.500000,2.500000));
 	Interact->SetBoxExtent(FVector(15.000000,45.000000,3.000000));
+
+	AnimFade->SetNewMat();
 }
