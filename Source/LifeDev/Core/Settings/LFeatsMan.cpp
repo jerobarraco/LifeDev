f37d6b41d67a0974319fc274b904fde7801146b6
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-01-13 18:21:56 +1300

    * removed useless ScreenTexelSize.uasset and cleanups

--- Source/LifeDev/Core/Settings/LFeatsMan.cpp
+++ Source/LifeDev/Core/Settings/LFeatsMan.cpp
@@ -149,9 +149,9 @@ void ALFeatsMan::FeatUpVisual(const EFeat Feat, const bool bEnabled) {
 void ALFeatsMan::FeatUpUnreal(const EFeat Feat, const bool bEnabled) {
 	if (Feat == EFeat::U_BATCH_TICK) {
 		IConsoleVariable* CVar = IConsoleManager::Get().FindConsoleVariable(TEXT("tick.AllowBatchedTicks"));
-		if (CVar) CVar->Set(bEnabled ? 1 : 0, EConsoleVariableFlags::ECVF_SetByCode);
+		if (LIKELY(CVar)) CVar->Set(bEnabled ? 1 : 0, EConsoleVariableFlags::ECVF_SetByCode);
 	} else if (Feat == EFeat::U_CON_TICK) {
 		IConsoleVariable* CVar = IConsoleManager::Get().FindConsoleVariable(TEXT("tick.AllowConcurrentTickQueue"));
-		if (CVar) CVar->Set(bEnabled ? 1 : 0, EConsoleVariableFlags::ECVF_SetByCode);
+		if (LIKELY(CVar)) CVar->Set(bEnabled ? 1 : 0, EConsoleVariableFlags::ECVF_SetByCode);
 	}
 }
