077d6c318d7dd70662549b5c9d8dba035938311e
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-01-29 19:27:07 +1300

    * ch01 drawer with text is now top drawer

--- Source/LifeDev/Game/Chaps/01/Inters/Doors/DrawerI01.cpp
+++ Source/LifeDev/Game/Chaps/01/Inters/Doors/DrawerI01.cpp
@@ -2,7 +2,7 @@
 
 #include "DrawerI01.h"
 
-void ADrawerI01::SetState_Implementation(int32 NewState) {
+void ADrawerI01::SetState_Implementation(const int32 NewState) {
 	Super::SetState_Implementation(NewState);
 	TriggerDlg = IsClosed() ? "": "DR01_T";
 }
--- Source/LifeDev/Game/Chaps/01/Inters/Doors/DrawerI01.h
+++ Source/LifeDev/Game/Chaps/01/Inters/Doors/DrawerI01.h
@@ -1,15 +1,15 @@
 // Copyright Jerónimo Barraco-Mármol
 
 #pragma once
-#include "LifeDev/Game/Chaps/All/Inters/Doors/Cabinet/Drawer02.h"
+#include "LifeDev/Game/Chaps/All/Inters/Doors/Cabinet/Drawer01.h"
 
 #include "DrawerI01.generated.h"
 
 // Drawer 02 instance 00
 UCLASS(Blueprintable, BlueprintType)
-class LIFEDEV_API ADrawerI01: public ADrawer02 {
+class LIFEDEV_API ADrawerI01: public ADrawer01 {
 	GENERATED_BODY()
 
 protected:
-	virtual void SetState_Implementation(int32 NewState) override;
+	virtual void SetState_Implementation(const int32 NewState) override;
 };
