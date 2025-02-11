7e2fd40c1cef99fa6de5dd0ecc5e0fc11ed9cbd4
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-02-11 19:59:49 +1300

    * cleanup

--- Plugins/JUtils/Source/JUtils/Actors/CQuickMesh.h
+++ Plugins/JUtils/Source/JUtils/Actors/CQuickMesh.h
@@ -5,7 +5,7 @@
 
 #include "CQuickMesh.generated.h"
 
-// base class for an optimized mesh. by default it well have EVERYTHING disabled, mostly.
+// base class for an optimized mesh. by default, it will have EVERYTHING disabled, mostly.
 UCLASS(Blueprintable, BlueprintType, ClassGroup=(JUtils), meta=(BlueprintSpawnableComponent))
 class JUTILS_API UCQuickMesh: public UStaticMeshComponent {
 	GENERATED_BODY()
