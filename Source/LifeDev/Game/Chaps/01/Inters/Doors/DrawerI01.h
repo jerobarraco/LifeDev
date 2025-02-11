87c59df6f43932d4b02b87c7255c39be380df3c8
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-02-11 19:49:12 +1300

    * fixed drawer on ch00

--- Source/LifeDev/Game/Chaps/01/Inters/Doors/DrawerI01.h
+++ Source/LifeDev/Game/Chaps/01/Inters/Doors/DrawerI01.h
@@ -1,13 +1,14 @@
 // Copyright Jerónimo Barraco-Mármol
 
 #pragma once
-#include "LifeDev/Game/Chaps/All/Inters/Doors/Cabinet/Drawer01.h"
+
+#include "LifeDev/Game/Chaps/All/Inters/Doors/Cabinet/Drawer02.h"
 
 #include "DrawerI01.generated.h"
 
 // Drawer 02 instance 00
 UCLASS(Blueprintable, BlueprintType)
-class LIFEDEV_API ADrawerI01: public ADrawer01 {
+class LIFEDEV_API ADrawerI01: public ADrawer02 {
 	GENERATED_BODY()
 
 protected:
