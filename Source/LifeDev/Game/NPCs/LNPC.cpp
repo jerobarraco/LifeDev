c1fde03e3ff29d96b8d198c09f0fe9344c1f4ba8
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-02-11 19:53:51 +1300

    * cleanup

--- Source/LifeDev/Game/NPCs/LNPC.cpp
+++ Source/LifeDev/Game/NPCs/LNPC.cpp
@@ -1,8 +1,6 @@
 // Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
 #include "LNPC.h"
 
-#include "Interact/Animator/CAnimatorMix.h"
-
 ALNPC::ALNPC():Super() {
 	PrimaryActorTick.bStartWithTickEnabled = false;
 }
