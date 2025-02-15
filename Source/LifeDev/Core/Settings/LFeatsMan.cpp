42da7f88a20c78eb7eeaf24e63d9454555ebd026
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-02-15 20:09:30 +1300

    * cleanup

--- Source/LifeDev/Core/Settings/LFeatsMan.cpp
+++ Source/LifeDev/Core/Settings/LFeatsMan.cpp
@@ -148,10 +148,12 @@ void ALFeatsMan::FeatUpVisual(const EFeat Feat, const bool bEnabled) {
 
 void ALFeatsMan::FeatUpUnreal(const EFeat Feat, const bool bEnabled) {
 	if (Feat == EFeat::U_TICK_BATCH) {
-		IConsoleVariable* const CVar = IConsoleManager::Get().FindConsoleVariable(TEXT("tick.AllowBatchedTicks"));
+		IConsoleVariable* const CVar =
+			IConsoleManager::Get().FindConsoleVariable(TEXT("tick.AllowBatchedTicks"));
 		if (LIKELY(CVar)) CVar->Set(bEnabled ? 1 : 0, EConsoleVariableFlags::ECVF_SetByCode);
 	} else if (Feat == EFeat::U_TICK_CON) {
-		IConsoleVariable* const CVar = IConsoleManager::Get().FindConsoleVariable(TEXT("tick.AllowConcurrentTickQueue"));
+		IConsoleVariable* const CVar =
+			IConsoleManager::Get().FindConsoleVariable(TEXT("tick.AllowConcurrentTickQueue"));
 		if (LIKELY(CVar)) CVar->Set(bEnabled ? 1 : 0, EConsoleVariableFlags::ECVF_SetByCode);
 	}
 }
