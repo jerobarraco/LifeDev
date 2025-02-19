9c7dbc9ad3706176cfc0631ad15a78e4e77cb9a0
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-02-19 19:26:51 +1300

    * wip added c1s7

---
+++ Source/LifeDev/Game/Chaps/01/Steps/LStepC1S007.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
+
+#include "LStepC1S007.h"
+
+#include "LifeDev/Core/Consts/ConstItems.h"
+
+ALStepC1S007::ALStepC1S007():Super() {
+	Name = FName("C1S7");
+	InputEnabled = true; // does it matters? it will show dialogs
+	// uses pawn camera
+	CamTarget = nullptr;
+	UsePawnCam = true;
+	UseFadeTime = false;
+	DlgId = "N01.1";
+	FBDlgAutoTo = .5;
+	UseRain = false;
+
+	// wait for the player to talk with the npc and get the card
+	ItemsFinish = {
+		LDConsts::Items::Card1,
+				LDConsts::Items::Poem1,
+}
+;
+
+	// the clothes are transient items that don't need to be carried
+	// the rest of the items are going to be used or required
+	ItemsRem = {"C1C00", "C1C01", "C1C02", "C1C03", "C1C04"}; 
+}
---
+++ Source/LifeDev/Game/Chaps/01/Steps/LStepC1S007.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LifeDev/Core/Story/LStep.h"
+
+#include "LStepC1S007.generated.h"
+
+// npc finished dialog. wait for cards.
+UCLASS(Blueprintable, BlueprintType)
+class ALStepC1S007 : public ALStep {
+	GENERATED_BODY()
+
+public:
+	ALStepC1S007();
+};
