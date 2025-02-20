9c1e5d5e171f076e3d5a96de08f213bb63301a4b
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-02-21 10:45:47 +1300

    * log verbosity

--- Plugins/JUtils/Source/JUtils/Actors/Pool.cpp
+++ Plugins/JUtils/Source/JUtils/Actors/Pool.cpp
@@ -86,7 +86,7 @@ AActor* UPool::Get() {
 
 	if (Ready.Num()<=0) {
 		if (!CanGrow) {
-			UE_LOG(LogJPool, Warning,
+			UE_LOG(LogJPool, Verbose,
 				TEXT("%hs: Pool is exhausted, and can't grow. so can't return an actor. Stop."
 					" ItemType=%s"),
 				__func__, *ItemType->GetName());
