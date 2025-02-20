d8ba1200accc11daba5a09107a5ebc0d46aa32e2
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-02-20 18:09:01 +1300

    * fixed card dialog. * fixed some dialogs

--- Source/LifeDev/Game/Chaps/All/Inters/Cards/Card04.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Cards/Card04.cpp
@@ -6,7 +6,7 @@
 
 ACard04::ACard04():Super() {
 	// overriden description for this chapter. will show on trigger.
-	TriggerDlg = "C3_Look*";
+	TriggerDlg = "C4_Look*"; // TODO
 	RewardItem = LDConsts::Items::Card4;
 	// the step will finish on this, set on the step.
 
