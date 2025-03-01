de33927976514889ff36c78f7d71b1b22546caec
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-01 18:48:45 +1300

    * how to import a font

--- Source/LifeDev/Game/Env/WorldText/LWorldText.h
+++ Source/LifeDev/Game/Env/WorldText/LWorldText.h
@@ -6,7 +6,7 @@
 #include "LWorldText.generated.h"
 
 // i thought on using a decal, but docs says it works only on static objects (which was a lie)
-// but its a risk https://dev.epicgames.com/documentation/en-us/unreal-engine/decal-actors-in-unreal-engine
+// but it's a risk https://dev.epicgames.com/documentation/en-us/unreal-engine/decal-actors-in-unreal-engine
 // but also rendering text on a material seems difficult. and i need to create an actor ANYWAY
 
 // Base class for in-world texts.
