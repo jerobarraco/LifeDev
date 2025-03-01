c24994d09ce55f019df00f00a7f76b4f6d1d43d7
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-01 18:30:23 +1300

    * setting defaults

--- Source/LifeDev/Game/Env/WorldText/LWorldText.cpp
+++ Source/LifeDev/Game/Env/WorldText/LWorldText.cpp
@@ -5,11 +5,19 @@
 #include "Components/TextRenderComponent.h"
 
 ALWorldText::ALWorldText():Super() {
+	PrimaryActorTick.bStartWithTickEnabled = false;
+	PrimaryActorTick.bCanEverTick = false;
+
 	Root = CreateDefaultSubobject<UTextRenderComponent>(TEXT("TEXT"));
 	SetRootComponent(Root);
+	static ConstructorHelpers::FObjectFinder<UMaterialInterface>
+		CMat(TEXT("/Game/LifeDev/Game/Env/WorldText/WorldText_M"));
+	Root->SetMaterial(0, CMat.Object);
+	Root->SetWorldSize(25);
+	Root->SetVerticalAlignment(EVerticalTextAligment::EVRTA_TextCenter);
+	Root->SetHorizontalAlignment(EHorizTextAligment::EHTA_Center);
+	
 	// Root->SetFont() // TODO
-	// Root->SetMaterial // TODO
-	// Root->SetWorldSize() // TODO
 }
 
 // TODO do smth with the fb?
