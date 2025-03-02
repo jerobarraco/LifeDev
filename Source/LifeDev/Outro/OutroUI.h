c55ccbc0d3a49f840c3ca1202e7e677c9c92a408
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-02 19:29:08 +1300

    * stub buttons

--- Source/LifeDev/Outro/OutroUI.h
+++ Source/LifeDev/Outro/OutroUI.h
@@ -6,6 +6,8 @@
 
 #include "OutroUI.generated.h"
 
+class UJButton;
+class UWidgetSwitcher;
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOutroUIDone);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOutroUIQuit);
 
@@ -18,4 +20,19 @@ public:
 	void SetFlags(const TMap<FName, float>& Flags);
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent, meta=(ForceAsFunction))
 	void SetPage(const int32 Page=0);
+	
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidgetOptional))
+	TObjectPtr<UWidgetSwitcher> Switcher = nullptr;
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidgetOptional))
+	TObjectPtr<UJButton> BtnRetry = nullptr;
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidgetOptional))
+	TObjectPtr<UJButton> BtnCredits = nullptr;
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidgetOptional))
+	TObjectPtr<UJButton> BtnStats = nullptr;
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidgetOptional))
+	TObjectPtr<UJButton> BtnQuit = nullptr;
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidgetOptional))
+	TObjectPtr<UJButton> BtnBack = nullptr;
+	UPROPERTY(BlueprintReadWrite, meta=(BindWidgetOptional))
+	TObjectPtr<UJButton> BtnBack2 = nullptr;
 };
