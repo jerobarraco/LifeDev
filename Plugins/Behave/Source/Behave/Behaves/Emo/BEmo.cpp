42d4fecc1a989c814bb375da18b206ce49723afe
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-05 18:55:48 +1300

    * emo damp

--- Plugins/Behave/Source/Behave/Behaves/Emo/BEmo.cpp
+++ Plugins/Behave/Source/Behave/Behaves/Emo/BEmo.cpp
@@ -3,3 +3,18 @@
 
 #include "BEmo.h"
 
+void UBEmo::Tick_Implementation(const float DT) {
+	Super::Tick_Implementation(DT);
+	const float Damp = EmoDamp * DT;
+	// TODO does this works?
+	bool AllZ = true;
+	for (TTuple<FName, float>& KV: Values) {
+		KV.Value = FMath::Clamp(KV.Value - Damp, 0, 1);
+		if (!FMath::IsNearlyZero(KV.Value))
+			AllZ = false;
+	}
+
+	if (AllZ)
+		Values[T_Bore] += Damp*2; // increase bore by emo_damp if no emotion is present
+}
+
--- Plugins/Behave/Source/Behave/Behaves/Emo/BEmo.h
+++ Plugins/Behave/Source/Behave/Behaves/Emo/BEmo.h
@@ -18,11 +18,12 @@ public:
 	static const inline FName T_Anger = "Emo.Anger";
 	static const inline FName T_Sad = "Emo.Sad";
 	static const inline FName T_Fear = "Emo.Fear";
-	static const inline FName T_Shame = "Emo.Shame";
+	static const inline FName T_Shame = "Emo.Shame"; // not same as guilt, but we can fake one with the other
+	static const inline FName T_Joy = "Emo.Joy";
 	static const inline FName T_Bore = "Emo.Bore";
 
 	TArray<FName> Tokens = {
-		T_Anger, T_Sad, T_Fear, T_Shame, T_Bore
+		T_Anger, T_Sad, T_Fear, T_Shame, T_Joy, T_Bore
 	};
 
 	TMap<FName, float> Values = {
@@ -33,5 +34,7 @@ public:
 		{T_Bore, 0}
 	};
 protected:
-	float EmoDamp = .001;
+	virtual void Tick_Implementation(const float DT) override;
+	
+	static inline float EmoDamp = .001;
 };
