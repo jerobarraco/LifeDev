d7cbcd7d8b3e5a9ecb0c1be884788b1dcc1c6635
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-05 18:50:32 +1300

    * default values

--- Plugins/Behave/Source/Behave/Behaves/Emo/BEmo.h
+++ Plugins/Behave/Source/Behave/Behaves/Emo/BEmo.h
@@ -25,6 +25,13 @@ public:
 		T_Anger, T_Sad, T_Fear, T_Shame, T_Bore
 	};
 
+	TMap<FName, float> Values = {
+		{T_Anger, 0},
+		{T_Sad, 0},
+		{T_Fear, 0},
+		{T_Shame, 0},
+		{T_Bore, 0}
+	};
 protected:
 	float EmoDamp = .001;
 };
