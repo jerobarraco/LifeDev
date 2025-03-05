ede7ca7e71d191790a40b1be565274fcdb3c173f
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-05 17:59:40 +1300

    * base cbehave

---
+++ Plugins/Behave/Source/Behave/CBehave.h
@@ -0,0 +1,15 @@
+// Copyright (C) 2023 - Jeronimo Barraco-Marmol. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+
+#pragma once
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+
+#include "CBehave.generated.h"
+
+UCLASS(Blueprintable, BlueprintType, ClassGroup=(Behave), meta=(BlueprintSpawnableComponent))
+class BEHAVE_API UCBehave: public UActorComponent {
+	GENERATED_BODY()
+public:
+};
