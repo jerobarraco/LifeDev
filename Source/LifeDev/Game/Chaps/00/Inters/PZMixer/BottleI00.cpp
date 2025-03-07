50372fcb3f9d2a043198b9cb076525fb3792c838
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-07 20:42:52 +1300

    * fixed bottles fade

--- Source/LifeDev/Game/Chaps/00/Inters/PZMixer/BottleI00.cpp
+++ Source/LifeDev/Game/Chaps/00/Inters/PZMixer/BottleI00.cpp
@@ -2,8 +2,11 @@
 
 #include "BottleI00.h"
 
+#include "Interact/Animator/CAnimatorFade.h"
+
 ABottleI00::ABottleI00():Super() {
 	RewardItem = "Bottle00";
 	UseRewardDestroy = true;
 	UseFade = true;
+	AnimFade->MatBase = nullptr;
 }
