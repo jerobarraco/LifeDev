0953c18abf363f55e833d2897930d3550d7dc540
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-08 18:30:56 +1300

    * added lsavegroup

---
+++ Source/LifeDev/Core/Settings/UI/LSaveGroup.h
@@ -0,0 +1,15 @@
+// Copyright (C) 2024 - Jeronimo Barraco-Marmol. All rights reserved.
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GroupBox.h"
+
+#include "LSaveGroup.generated.h"
+
+// LD basic savefile box
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API ULSaveGroup : public UGroupBox {
+	GENERATED_BODY()
+
+public:
+};
