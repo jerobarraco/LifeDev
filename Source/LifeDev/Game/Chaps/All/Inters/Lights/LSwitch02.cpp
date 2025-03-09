5ec3034a47475962a009908b17a9433d6e2d4bc3
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-09 19:28:10 +1300

    * switch2 setup

---
+++ Source/LifeDev/Game/Chaps/All/Inters/Lights/LSwitch02.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
+#include "LSwitch02.h"
+
+#include "CQuickMesh.h"
+#include "Interact/CInteract.h"
+#include "Interact/Animator/CAnimatorMix.h"
+
+ALSwitch02::ALSwitch02(): Super() {
+	StateNum = 2;
+	const FVector Loc(5,-7.500000,12.500000);
+	const FRotator State0Rot(10,0.,0);
+	const FRotator State1Rot(-10,0, 0);
+	Trans = {
+		FTransform(State0Rot, Loc),
+		FTransform(State1Rot, Loc),
+	};
+	IRoot->SetRelativeTransform(Trans[0]);
+	Anim->IsAdditive = false;
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		CMesh (TEXT("/Game/LifeDev/Game/Inters/Lights/Switch00/btn2"));
+	Mesh->SetStaticMesh(CMesh.Object);
+	Mesh->SetRelativeLocation(FVector(-7.5,7.500000,-12.5));
+
+	Interact->SetRelativeLocation(FVector(7.500000,-7.5,12.5));
+	// a bit in doubt of the Z. it could protrude. but people's instinct might be to reach for the extreme.
+	Interact->SetBoxExtent(FVector(5,5,7.5));
+}
---
+++ Source/LifeDev/Game/Chaps/All/Inters/Lights/LSwitch02.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LSwitch00.h"
+
+#include "LSwitch02.generated.h"
+
+UCLASS(Blueprintable, BlueprintType)
+class ALSwitch02 : public ALSwitch00 {
+	GENERATED_BODY()
+
+public:
+	ALSwitch02();
+};
