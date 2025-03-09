e8f32e3714bf8875f256dc5df39c0a4db50850c1
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-09 18:49:02 +1300

    * stub LSwitch01.cpp

---
+++ Source/LifeDev/Game/Chaps/All/Inters/Lights/LSwitch01.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
+#include "LSwitch01.h"
+
+ALSwitch01::ALSwitch01(): Super() {
+}
---
+++ Source/LifeDev/Game/Chaps/All/Inters/Lights/LSwitch01.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LSwitch00.h"
+
+#include "LSwitch01.generated.h"
+
+UCLASS(Blueprintable, BlueprintType)
+class ALSwitch01 : public ALSwitch00 {
+	GENERATED_BODY()
+
+public:
+	ALSwitch01();
+};
