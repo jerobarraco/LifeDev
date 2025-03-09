64f9e164445b3e1fbac6240b49310b18b9d32c2a
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-09 19:13:23 +1300

    * wip switch01

--- Source/LifeDev/Game/Chaps/All/Inters/Lights/LSwitch01.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Lights/LSwitch01.cpp
@@ -7,14 +7,28 @@
 
 ALSwitch01::ALSwitch01(): Super() {
 	
-	Anim->IsAdditive = true;
-	Anim->TEnd.SetLocation(FVector(-2.5,0,0));
+	StateNum = 4; // 4 is the max. could be clamped on children
+
+	const FVector Loc(0.000000,-7.500000,12.500000);
+	const FRotator State0Rot(0,0.,0);
+	const FRotator State1Rot(0,0, -90);
+	const FRotator State2Rot(0,0,-180);
+	const FRotator State3Rot(0,0,-270);
+	Trans = {
+		FTransform(State0Rot, Loc),
+		FTransform(State1Rot, Loc),
+		FTransform(State2Rot, Loc),
+		FTransform(State3Rot, Loc),
+	};
+	IRoot->SetRelativeTransform(Trans[0]);
+	Anim->IsAdditive = false;
 
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CMesh (TEXT("/Game/LifeDev/Game/Inters/Lights/Switch00/btn3"));
 	Mesh->SetStaticMesh(CMesh.Object);
-	Mesh->SetRelativeLocation(FVector(0,0,0));
+	Mesh->SetRelativeLocation(FVector(0.000000,7.500000,-12.500000));
 
+	// TODO
 	Interact->SetRelativeLocation(FVector(7.500000,-12.500000,17.500000));
 	Interact->SetBoxExtent(FVector(5));
 }
