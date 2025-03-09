1b70bcc10fd6f4a0d7f81414887d44b13becfd77
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-09 18:47:38 +1300

    * lswitch00

--- Source/LifeDev/Game/Chaps/All/Inters/Lights/LSwitch00.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Lights/LSwitch00.cpp
@@ -2,20 +2,14 @@
 #include "LSwitch00.h"
 
 #include "CQuickMesh.h"
+#include "Interact/Animator/CAnimatorFade.h"
 
 ALSwitch00::ALSwitch00(): Super() {
-	// interacts start "off/closed"
-	Texts = {
-		NSLOCTEXT("Switch00", "TurnOn", "Turn On"),
-		NSLOCTEXT("Switch00", "TurnOff", "Turn Off"),
-	};
-
 	Base = CreateDefaultSubobject<UCQuickMesh>(TEXT("Base"));
 	Base->SetupAttachment(Root);
-	// TODO this is a hack. find something better.
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CMesh (TEXT("/Game/LifeDev/Game/Inters/Bath/Toilet02/Btn2"));
 	Base->SetStaticMesh(CMesh.Object);
-	Base->SetRelativeScale3D(FVector(4.000000,1.000000,6.000000));
-	Base->SetRelativeLocation(FVector(-30.,27.500000,-465));
+	Base->SetRelativeLocation(FVector(0,0,0));
+	AnimFade->Meshes.Add(Base); // unused but...
 }
--- Source/LifeDev/Game/Interact/Base/Lights/LSwitch.cpp
+++ Source/LifeDev/Game/Interact/Base/Lights/LSwitch.cpp
@@ -7,26 +7,33 @@
 #include "Interact/Animator/CAnimatorMix.h"
 
 ALSwitch::ALSwitch():Super() {
-	UseAnim = true;
 	UseFade = false;
 	UseRewardDestroy = false;
 	UseStateLoop = true;
 	UseAttachedSFX = true;
 	StateNum = 2;
 
-	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh (TEXT("/Game/LifeDev/Game/Inters/Tv00/Tv00-Btn"));
-	Mesh->SetStaticMesh(CMesh.Object);
-	Mesh->SetRelativeLocation(FVector(-57.500000,2.500000,-12.500000));
-
-	Interact->SetRelativeLocation(FVector(57.500000,-2.500000,12.500000));
-	Interact->SetBoxExtent(FVector(5));
-
+	// interacts start "off/closed"
+	Texts = {
+		NSLOCTEXT("Switch00", "TurnOn", "Turn On"),
+		NSLOCTEXT("Switch00", "TurnOff", "Turn Off"),
+	};
+	
+	UseAnim = true;
 	Anim->Duration = .5;
 	Anim->IsAdditive = true;
-	Anim->TEnd.SetLocation(FVector(0.0,-2.5,0));
+	Anim->TEnd.SetLocation(FVector(-2.5,0,0));
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		CMesh (TEXT("/Game/LifeDev/Game/Inters/Lights/Switch00/btn"));
+	Mesh->SetStaticMesh(CMesh.Object);
+	Mesh->SetRelativeLocation(FVector(0,0,0));
+
+	Interact->SetRelativeLocation(FVector(7.500000,-12.500000,17.500000));
+	Interact->SetBoxExtent(FVector(5));
 
 	static ConstructorHelpers::FObjectFinder<USoundBase>
 		SOpen(TEXT("/Game/LifeDev/Game/Inters/Generic/Button_Press-007.Button_Press-007"));
 	SFX_Start = {SOpen.Object, SOpen.Object}; // reusing the same. close, open
+
 }
