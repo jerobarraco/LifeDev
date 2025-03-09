277add9a46945d9e998a95ace51e65b4a4a832b3
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-09 18:51:28 +1300

    * fixed the base

--- Source/LifeDev/Game/Chaps/All/Inters/Lights/LSwitch00.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Lights/LSwitch00.cpp
@@ -8,7 +8,7 @@ ALSwitch00::ALSwitch00(): Super() {
 	Base = CreateDefaultSubobject<UCQuickMesh>(TEXT("Base"));
 	Base->SetupAttachment(Root);
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh (TEXT("/Game/LifeDev/Game/Inters/Bath/Toilet02/Btn2"));
+		CMesh (TEXT("/Game/LifeDev/Game/Inters/Lights/Switch00/base"));
 	Base->SetStaticMesh(CMesh.Object);
 	Base->SetRelativeLocation(FVector(0,0,0));
 	AnimFade->Meshes.Add(Base); // unused but...
--- Source/LifeDev/Game/Chaps/All/Inters/Lights/LSwitch01.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Lights/LSwitch01.cpp
@@ -1,5 +1,20 @@
 // Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
 #include "LSwitch01.h"
 
+#include "CQuickMesh.h"
+#include "Interact/CInteract.h"
+#include "Interact/Animator/CAnimatorMix.h"
+
 ALSwitch01::ALSwitch01(): Super() {
+	
+	Anim->IsAdditive = true;
+	Anim->TEnd.SetLocation(FVector(-2.5,0,0));
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		CMesh (TEXT("/Game/LifeDev/Game/Inters/Lights/Switch00/btn"));
+	Mesh->SetStaticMesh(CMesh.Object);
+	Mesh->SetRelativeLocation(FVector(0,0,0));
+
+	Interact->SetRelativeLocation(FVector(7.500000,-12.500000,17.500000));
+	Interact->SetBoxExtent(FVector(5));
 }
