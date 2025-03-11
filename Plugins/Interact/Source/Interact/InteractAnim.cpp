247ef87036c2dc39692430b68f7b7dcd9242fb85
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-11 20:36:07 +1300

    * fixed bug on interact and disablewhileanim

--- Plugins/Interact/Source/Interact/InteractAnim.cpp
+++ Plugins/Interact/Source/Interact/InteractAnim.cpp
@@ -41,8 +41,7 @@ void AInteractAnim::DoTrigger_Implementation() { // called by trytrigger
 	// do after Trigger, so it actually disable during the animation
 	// also not doing during SetState since that can also be called by other means.
 	// done here and not on AnimEnd due to the same reason.
-	// if (IsOneShot) DisableWhileAnim = false; // done on AnimEnd. it actually amkes more sense to me.
-	if (DisableWhileAnim || IsOneShot) SetActive(false);
+	if ((UseAnim && DisableWhileAnim) || IsOneShot) SetActive(false);
 	Super::DoTrigger_Implementation();
 }
 
@@ -84,7 +83,7 @@ void AInteractAnim::AnimBegin_Implementation() {
 
 void AInteractAnim::AnimEnd_Implementation() {
 	// at this point the state ( isOpen ) flag is toggled
-	if (DisableWhileAnim && !IsOneShot) SetActive(true);
+	if ((UseAnim && DisableWhileAnim) && !IsOneShot) SetActive(true);
 
 	if (LIKELY(State >= 0) && State < SFX_Stop.Num()) {
 		USoundBase* const Snd2 = SFX_Stop[State];
