327d0b404eed6cf2fad0f0ee1e348fdc3c651b34
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-12 19:08:47 +1300

    * cleanup

--- Source/LifeDev/Game/Interact/LInteract.cpp
+++ Source/LifeDev/Game/Interact/LInteract.cpp
@@ -41,7 +41,9 @@ ALInteract::ALInteract():Super() {
 
 void ALInteract::SetState_Implementation(const int32 NewState) {
 	Super::SetState_Implementation(NewState);
-	if (LIKELY(IsValid(Diags))) { // TODO test
+	// TODO fix issue of this triggering when the actor first spawns
+	// (e.g. lights and puzzles)
+	if (LIKELY(IsValid(Diags))) {
 		const FString& BName = LDConsts::Dlgs::Inter::StatePre;
 		// FString Label;
 		// UJUtilsMisc::ObjectLabel(this, Label);
