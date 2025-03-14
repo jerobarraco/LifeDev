44830bee4ce95fbf3e225c7ee2006e60867dc5d3
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-14 17:46:43 +1300

    * seq do

--- Plugins/Behave/Source/Behave/Actions/BSeq.cpp
+++ Plugins/Behave/Source/Behave/Actions/BSeq.cpp
@@ -2,3 +2,17 @@
 // SPDX-License-Identifier: MIT
 
 #include "BSeq.h"
+
+EBDoRes UBSeq::Do_Implementation(const float DT) {
+	UBBase* const C = GetCurChild();
+	if (UNLIKELY(!C)) return EBDoRes::ABORT; // anomaly
+
+	const EBDoRes R = C->Do(DT);
+	if (R == EBDoRes::ABORT) return EBDoRes::ABORT; // bubble up
+
+	if (R == EBDoRes::STOP) {
+		StartChild(CurChildI+1);
+		if (CurChildI<0)
+			return EBDoRes::STOP;
+	}
+}
