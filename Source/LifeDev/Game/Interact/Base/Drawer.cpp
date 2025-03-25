cdea6a3b9e5742912602a79a4b48ab8892d85843
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-25 19:15:56 +1300

    * revert change

--- Source/LifeDev/Game/Interact/Base/Drawer.cpp
+++ Source/LifeDev/Game/Interact/Base/Drawer.cpp
@@ -5,7 +5,9 @@
 #include "Interact/Animator/CAnimatorMix.h"
 
 ADrawer::ADrawer():Super() {
-	//  Set the anim root to the root component so that i can attach items to it
+	// Set the anim root to the root component so that i can attach items to it.
+	// Anim is set to TRoot to RootComp to allow to attach stuff to it.
+	// but that means the Tend is always in word coords depending on rotation. and it sucks.
 	Anim->TRoot = RootComponent;
 	Anim->TEnd.SetLocation(FVector(40, 0,0));
 
