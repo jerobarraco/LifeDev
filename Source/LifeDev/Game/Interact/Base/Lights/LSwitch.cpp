8cfa9ce4997d7168e0870c8b7d72ce89e71aa582
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-26 21:02:55 +1300

    * cleanup

--- Source/LifeDev/Game/Interact/Base/Lights/LSwitch.cpp
+++ Source/LifeDev/Game/Interact/Base/Lights/LSwitch.cpp
@@ -16,15 +16,12 @@ ALSwitch::ALSwitch():Super() {
 	// interacts start "off/closed"
 	// using "toggle" since sometimes it desyncs from the lights
 	Texts = {
-		NSLOCTEXT("Switch00", "TurnOn", "Turn On"),
-		NSLOCTEXT("Switch00", "TurnOff", "Turn Off"),
+		NSLOCTEXT("Switch00", "TurnOn", "Toggle"),
+		NSLOCTEXT("Switch00", "TurnOff", "Toggle"),
 	};
 	
 	UseAnim = true;
-	Anim->Duration = .5;
-	Anim->IsAdditive = true;
-	Trans.Empty();
-	Anim->TEnd.SetLocation(FVector(-2.5,0,0));
+	Anim->Duration = .25;
 
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CMesh (TEXT("/Game/LifeDev/Game/Inters/Lights/Switch00/btn"));
@@ -33,7 +30,9 @@ ALSwitch::ALSwitch():Super() {
 
 	Interact->SetRelativeLocation(FVector(7.500000,-7.500000,12.500000));
 	Interact->SetBoxExtent(FVector(5));
-
+	// TODO move this piece to Switch03 
+	Anim->IsAdditive = true;
+	Anim->TEnd.SetLocation(FVector(-2.5,0,0));
 	static ConstructorHelpers::FObjectFinder<USoundBase>
 		SOpen(TEXT("/Game/LifeDev/Game/Inters/Generic/Button_Press-007.Button_Press-007"));
 	SFXs = {SOpen.Object, SOpen.Object}; // reusing the same. close, open
