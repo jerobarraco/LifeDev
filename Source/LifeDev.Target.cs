a3823565c2403a3dbb2d632e4116037e4da9f957
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-26 18:14:17 +1300

    * disable pdb

--- Source/LifeDev.Target.cs
+++ Source/LifeDev.Target.cs
@@ -21,6 +21,12 @@ public class LifeDevTarget : TargetRules {
 		CppStandardEngine = CppStandardVersion.Cpp20;
 		CppStandard = CppStandardVersion.Cpp20;
 		
+		// https://forums.unrealengine.com/t/disabling-pdb-debug-gen-in-development-configuration-engine-source/617448/4
+		if(Configuration == UnrealTargetConfiguration.Shipping) { 
+			// disable pdb. it takes time and space. and i test builds. if someone has an issue ask to re-test with a debug build with logs and all.
+			bOmitPCDebugInfoInDevelopment = true;
+		}
+		
 		// https://forums.unrealengine.com/t/ue-5-5-source-linux-compilation-errors/2148769/2?u=nande
 		// Disable Intel Implicit SPMD Program Compiler forced by UE 5.5 on Linux.
 		// bCompileISPC = true; // setting to false could fix some errors on linux, but disabling uba fixes it too. and also disabling this will require an engine recompilation.
