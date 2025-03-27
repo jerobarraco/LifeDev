ae205f4305be940d8ebfdd941a637728988c44cc
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-03-27 19:34:19 +1300

    * adjustments

--- Source/LifeDev/Game/Chaps/All/Inters/Lights/LSwitch02.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Lights/LSwitch02.cpp
@@ -10,20 +10,15 @@ ALSwitch02::ALSwitch02(): Super() {
 	const FVector Loc(5,-7.500000,12.500000);
 	const FRotator State0Rot(10,0.,0);
 	const FRotator State1Rot(-10,0, 0);
-	// Trans = {
-		// FTransform(State0Rot, Loc),
-		// FTransform(State1Rot, Loc),
-	// };
 	Anim->IsAdditive = false;
+	// only twa states no need to use Trans
 	Anim->TStart = FTransform(State0Rot, Loc, FVector(1));
 	Anim->TEnd = FTransform(State1Rot, Loc, FVector(1));
-	// IRoot->SetRelativeTransform(Anim->TStart); // I could just use TStart and TEnd for this
-	// IRoot->SetRelativeTransform(Trans[0]); // I could just use TStart and TEnd for this
 
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CMesh (TEXT("/Game/LifeDev/Game/Inters/Lights/Switch00/btn2"));
 	Mesh->SetStaticMesh(CMesh.Object);
-	Mesh->SetRelativeLocation(FVector(-7.5,7.500000,-15));
+	Mesh->SetRelativeLocation(FVector(-7.5,10,-15));
 
 	Interact->SetRelativeLocation(FVector(7.500000,-7.5,15));
 	Interact->SetBoxExtent(FVector(5)); // luckily with this button size it doesn't protrude that much
