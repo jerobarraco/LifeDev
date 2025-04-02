1feda73ae0c567704b4494a6cea53d350d641e83
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-02 18:55:13 +1300

    * new stub switch05

---
+++ Source/LifeDev/Game/Chaps/All/Inters/Bath/LSwitch05.cpp
@@ -0,0 +1,30 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#include "LSwitch05.h"
+
+#include "CQuickMesh.h"
+#include "Interact/CInteract.h"
+#include "Interact/Animator/CAnimatorMix.h"
+
+ALSwitch05::ALSwitch05():Super() {
+	StateNum = 1;
+	
+	const FVector Loc(-7.500000,32.500000,-77.500000);
+	Mesh->SetRelativeLocation(Loc);
+	Anim->TEnd.SetLocation(FVector(0, 0, -2.5));
+	// Anim->IsBouncing = true; //uses the curve
+	Anim->IsAdditive = true;
+	Anim->Duration = .4;
+	Anim->IsReversed = false; // curve is reversed
+	static ConstructorHelpers::FObjectFinder<UCurveFloat>
+		CCF(TEXT("/Niagara/DefaultAssets/Curves/Templates/RampUpDown"));
+	Anim->Curve = CCF.Object;
+
+	static ConstructorHelpers::FObjectFinder<UStaticMesh>
+		CMesh(TEXT("/Game/LifeDev/Game/Inters/Bath/Toilet02/Btn2")); // already grey
+	Mesh->SetStaticMesh(CMesh.Object);
+
+	Interact->SetRelativeLocation(FVector(7.500000,-32.500000,77.500000));
+	Interact->SetBoxExtent(FVector(2.5)); // luckily with this button size it doesn't protrude that much
+
+}
---
+++ Source/LifeDev/Game/Chaps/All/Inters/Bath/LSwitch05.h
@@ -0,0 +1,15 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#pragma once
+#include "LifeDev/Game/Interact/Base/LSwitch.h"
+
+#include "LSwitch05.generated.h"
+
+// Base for toilet button A
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API ALSwitch05: public ALSwitch {
+	GENERATED_BODY()
+
+public:
+	ALSwitch05();
+};
