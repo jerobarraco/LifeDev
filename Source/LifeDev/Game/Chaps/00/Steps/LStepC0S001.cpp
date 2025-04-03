a62e3c74bc9f4cfb49699e40463529f8f0986cff
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-03 20:00:48 +1300

    * boss camera anim working

--- Source/LifeDev/Game/Chaps/00/Steps/LStepC0S001.cpp
+++ Source/LifeDev/Game/Chaps/00/Steps/LStepC0S001.cpp
@@ -30,20 +30,37 @@ ALStepC0S001::ALStepC0S001():Super() {
 
 	Target = CreateDefaultSubobject<USceneComponent>(TEXT("Target"));
 	Target->SetupAttachment(Root);
-
+	
 #if WITH_EDITORONLY_DATA
 	TargetArrow = CreateDefaultSubobject<UCQuickMesh>(TEXT("TargetArrow"));
 	TargetArrow->SetupAttachment(Target);
+	TargetArrow->SetHiddenInGame(true);
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CCam (TEXT("/Niagara/DefaultAssets/S_Arrow.S_Arrow"));
 	if (CCam.Succeeded()) TargetArrow->SetStaticMesh(CCam.Object);
 #endif
+
+	Anim = CreateDefaultSubobject<UCAnimatorTrans>(TEXT("Anim"));
+	Anim->TRoot = Cam;
 }
 
 void ALStepC0S001::Start_Implementation() {
 	Super::Start_Implementation();
+	Anim->TEnd = Target->GetRelativeTransform();
 	FB->SetMax(1);
+	FB->OnChange.AddUniqueDynamic(this, &ALStepC0S001::FBUpd);
 	// FB->SetVal(1, 10);
+}
+
+void ALStepC0S001::Stop_Implementation() {
+	FB->OnChange.RemoveAll(this);
+	Super::Stop_Implementation();
+}
+
+void ALStepC0S001::FBUpd(const float Value) {
+	// notice this depends on the tick interval for the FB. it will give the most accurate animation though.
+	// i will go with something simple for now.
+	Anim->Update(Value); // set the value to match the fb. let the anim do the calculations.
 }
 
 // don't change the fb val for the next chapter here. since it's not the responsibility of this class.
--- Source/LifeDev/Game/Chaps/00/Steps/LStepC0S001.h
+++ Source/LifeDev/Game/Chaps/00/Steps/LStepC0S001.h
@@ -7,6 +7,7 @@
 
 #include "LStepC0S001.generated.h"
 
+class UCAnimatorTrans;
 class UCQuickMesh;
 // chapter 0: the step where the char is in bed berating
 UCLASS(Blueprintable, BlueprintType)
@@ -18,9 +19,16 @@ public:
 
 protected:
 	virtual void Start_Implementation() override;
+	virtual void Stop_Implementation() override;
+
+	UFUNCTION()
+	void FBUpd(const float Value);
 
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
 	TObjectPtr<USceneComponent> Target = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	TObjectPtr<UCAnimatorTrans> Anim = nullptr;
 
 #if WITH_EDITORONLY_DATA
 	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
