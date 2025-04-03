b37b5c5e6c76d62e21a1a7fa122d95b26688c2b6
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-03 18:33:35 +1300

    * fix switchi00

--- Source/LifeDev/Game/Chaps/00/Inters/Var/LSwitchI00.cpp
+++ Source/LifeDev/Game/Chaps/00/Inters/Var/LSwitchI00.cpp
@@ -11,6 +11,7 @@ ALSwitchI00::ALSwitchI00():Super() {
 	IsOneShot = true;
 	// only used for the ch0, kinda unnecessary, but...
 	UseActiveOnce = true;
+	UseAutoActivate = true;
 	StateNum = 2;
 	static ConstructorHelpers::FObjectFinder<UCurveFloat>
 		CCurve(TEXT("/Interact/Interact_C.Interact_C"));
