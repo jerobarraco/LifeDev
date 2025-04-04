da639ee4728355fd4abd2117f6460c52c235a73c
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-04 18:04:54 +1300

    * stub animatortime

---
+++ Plugins/Interact/Source/Interact/Animator/CAnimatorTime.cpp
@@ -0,0 +1,3 @@
+// Copyright (C) 2023 - Jeronimo Barraco-Marmol. All rights reserved.
+// SPDX-License-Identifier: MIT
+#include "CAnimatorTime.h"
---
+++ Plugins/Interact/Source/Interact/Animator/CAnimatorTime.h
@@ -0,0 +1,12 @@
+// Copyright (C) 2023 - Jeronimo Barraco-Marmol. All rights reserved.
+// SPDX-License-Identifier: MIT
+#pragma once
+#include "CAnimator.h"
+#include "CAnimatorTime.generated.h"
+
+// Animator for the time dilation
+UCLASS(Blueprintable, BlueprintType, Placeable, ClassGroup=(Interact), meta=(BlueprintSpawnableComponent))
+class INTERACT_API UCAnimatorTime: public UCAnimator {
+	GENERATED_BODY()
+public:
+};
