1640ece7504ef50682a011a30ac30caccd0d799d
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-04 18:19:30 +1300

    * wip

--- Plugins/Interact/Source/Interact/Animator/CAnimatorTime.cpp
+++ Plugins/Interact/Source/Interact/Animator/CAnimatorTime.cpp
@@ -1,3 +1,24 @@
 // Copyright (C) 2023 - Jeronimo Barraco-Marmol. All rights reserved.
 // SPDX-License-Identifier: MIT
 #include "CAnimatorTime.h"
+
+void UCAnimatorTime::Update_Implementation(const float Alpha) {
+	const UWorld* const World = GetWorld();
+	if (UNLIKELY(!World)) return;
+
+	AWorldSettings* const Settings = World->GetWorldSettings(false, false);
+	if (UNLIKELY(!World )) return;
+
+	Super::Update_Implementation(Alpha);
+	const float NewDilation = FMath::LerpStable(From, To, Alpha);
+	Settings->SetTimeDilation(NewDilation); // has a clamp. also future proof.
+	Dilation = Settings->TimeDilation;// get the actual one for the future.
+	if (FMath::IsNearlyZero(Dilation))
+		Dilation = UE_SMALL_NUMBER; // don't crash
+}
+
+void UCAnimatorTime::TickComponent(float DeltaTime, const ELevelTick TickType,
+	FActorComponentTickFunction* const ThisTickFunction) {
+	DeltaTime /= Dilation;
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
--- Plugins/Interact/Source/Interact/Animator/CAnimatorTime.h
+++ Plugins/Interact/Source/Interact/Animator/CAnimatorTime.h
@@ -8,5 +8,15 @@
 UCLASS(Blueprintable, BlueprintType, Placeable, ClassGroup=(Interact), meta=(BlueprintSpawnableComponent))
 class INTERACT_API UCAnimatorTime: public UCAnimator {
 	GENERATED_BODY()
+
 public:
+	virtual void Update_Implementation(const float Alpha) override;
+	virtual void TickComponent(float DeltaTime, const ELevelTick TickType, FActorComponentTickFunction* const ThisTickFunction) override;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category=SetUp)
+	float From = 1;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category=SetUp)
+	float To = 1;
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Transient)
+	float Dilation = 1;
 };
