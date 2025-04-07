8f2c7d199d2017c02159d0fa9aff2b40e4a37adc
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-07 18:51:58 +1200

    * fixed door 14 enabled

--- Source/LifeDev/Game/Chaps/03/Inters/Doors/DoorI14.cpp
+++ Source/LifeDev/Game/Chaps/03/Inters/Doors/DoorI14.cpp
@@ -5,20 +5,17 @@
 #include "LifeDev/Core/Consts/ConstItems.h"
 
 ADoorI14::ADoorI14():Super() {
-	LockedDlg = "D14_L"; // unnecessary since it uses the card1. but in case i re-organize the chapter.
-	LockedItemDlg = "D14_LI";
+	// commented to help with refactoring later
+	// LockedDlg = "D14_L"; // unnecessary since it uses the card1. but in case i re-organize the chapter.
+	// LockedItemDlg = "D14_LI";
+	// ULockDlg = "D14_U";
 	IsLocked = true;
-	ULockDlg = "D14_U";
 	ULockItem = LDConsts::Items::Card1;
 	DisableWhileAnim = false; // avoid getting re-enabled due to state change
 	// ULockItem = LDConsts::Items::Card3;
-	
-	UseItemDlgs = {
-		{LDConsts::Items::Card0, "D14xC00"},
-		// {LDConsts::Items::Card1, "D14xC01"}, // done with the ULockDlg
-		{LDConsts::Items::Card2, "D14xC02"},
-	};
-	
+
 	// this door is quite confusing. and while it seems interesting it rather make it better for the player.
+	// so disable and start open
+	UseAutoActivate = false;
 	SetStateNow(1);
 }
