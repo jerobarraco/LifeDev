ab8aa9f1a3b5109939783a899faa52f997daaa67
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-09 20:01:53 +1200

    * autodiag

--- Source/LifeDev/Game/Chaps/00/Inters/Var/HeaterI02.cpp
+++ Source/LifeDev/Game/Chaps/00/Inters/Var/HeaterI02.cpp
@@ -8,9 +8,8 @@ AHeaterI02::AHeaterI02():Super() {
 	UseAutoActivate = true;
 	RewardFlash = .1;
 	
-	LockedDlg = "HT02_L";
-	Texts = {
-		NSLOCTEXT("HeaterI02", "TurnOn", "Turn On")};
+	// LockedDlg = "HT02_L";
+	Texts = { NSLOCTEXT("HeaterI02", "State0", "Turn On")};
 
 	// static since we won't animate it
 	Super::SetMobility(EComponentMobility::Static);
