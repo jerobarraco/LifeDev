ee091cc11bd49e326e626c41829da204a02d3bd0
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-09 20:16:30 +1200

    * autodiags

--- Source/LifeDev/Game/Chaps/01/Inters/Doors/WDoorI00.cpp
+++ Source/LifeDev/Game/Chaps/01/Inters/Doors/WDoorI00.cpp
@@ -5,7 +5,7 @@
 AWDoorI00::AWDoorI00():Super() {
 	IsLocked = true;
 	ULockItem = "KW0";
-	ULockDlg = "W0_ULCK";
-	LockedItemDlg = "W0_LCKIT";
-	LockedDlg = "W0_LCK";
+	// ULockDlg = "W0_ULCK";
+	LockedItemDlg = "W0_LCKIT"; // todo have to set this up on the data table. with a group with "Pick"
+	// LockedDlg = "W0_LCK"; // already set on the table
 }
