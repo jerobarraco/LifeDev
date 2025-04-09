283b9f2f8ea03f4a17deb6b02f9d96deb7f2a34b
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-09 20:29:39 +1200

    * some diags

--- Source/LifeDev/Core/Settings/LSettings.cpp
+++ Source/LifeDev/Core/Settings/LSettings.cpp
@@ -204,7 +204,7 @@ bool ULSettings::GetFeatS(const UObject* const O, const EFeat Feat) {
 
 FName ULSettings::GetObjectLabel(const UObject* const Object) {
 	const AInteract* const Inter = Cast<AInteract>(Object);
-	if (Inter) return Inter->GetLabel();
+	if (LIKELY(Inter)) return Inter->GetLabel();
 
 	return Inter->GetFName();
 }
