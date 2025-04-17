43c01fddf79a04606a5eabf453cadbe81a013754
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-17 19:24:09 +1200

    * loc

--- Source/LifeDev/Game/Chaps/00/Inters/Clouts/CloutI06.cpp
+++ Source/LifeDev/Game/Chaps/00/Inters/Clouts/CloutI06.cpp
@@ -7,6 +7,6 @@
 ACloutI06::ACloutI06():Super() {
 	TriggerDlg = "C06_T";
 	RewardItem = LDConsts::Items::Bra; // reusing the chap01 for now
-	static FText T = FText::FromString(TEXT("Dirty Clothes"));
+	static FText T = NSLOCTEXT("CloutI06", "State0", "Dirty Clothes");
 	Texts = { T };
 }
