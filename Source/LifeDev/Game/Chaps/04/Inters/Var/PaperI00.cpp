8f160f7a830ab500bb419598777b964ff257581c
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-21 14:59:18 +1200

    * actually fix PaperI00.cpp

--- Source/LifeDev/Game/Chaps/04/Inters/Var/PaperI00.cpp
+++ Source/LifeDev/Game/Chaps/04/Inters/Var/PaperI00.cpp
@@ -10,7 +10,7 @@
 APaperI00::APaperI00():Super() {
 	// just for looks
 	UseFade = true;
-
+	UseAutoActivate = false; // we got one that can't be interacted. override parent.
 	Text = CreateDefaultSubobject<UTextRenderComponent>(TEXT("Text"));
 	Text->SetupAttachment(Mesh);
 	Text->SetRelativeLocation(FVector(0.400000,-19.500000,0.600000));
