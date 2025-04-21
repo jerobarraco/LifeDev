a59bbb9652d4bdb33c5554553420d2aabcc55b89
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-21 12:35:08 +1200

    * fix logic bug setactorhidden

--- Source/LifeDev/Game/Interact/Base/LPuzzle.cpp
+++ Source/LifeDev/Game/Interact/Base/LPuzzle.cpp
@@ -70,7 +70,7 @@ void ALPuzzle::SetUseItemDlgs(const TMap<FName, FName>& Dlgs) const{
 
 void ALPuzzle::SetActorHiddenInGame(const bool NewHidden) {
 	Super::SetActorHiddenInGame(NewHidden);
-	if (UNLIKELY(CPuzzle)) return;
+	if (UNLIKELY(!CPuzzle)) return;
 	CPuzzle->SetHiddensInGame(NewHidden);
 }
 
