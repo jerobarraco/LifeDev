0f9298a572bcc0ec5925453c9c0486c95bcb56d3
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-24 18:17:57 +1200

    * base stub intro class

---
+++ Plugins/Inventory/Source/Inventory/Intro/Intro.h
@@ -0,0 +1,11 @@
+// Copyright (C) 2023 - Jeronimo Barraco-Marmol. All rights reserved.
+// SPDX-License-Identifier: MIT
+#pragma once
+
+#include "Intro.generated.h"
+UCLASS(Blueprintable, BlueprintType)
+class AIntroMan: public AInfo {
+	GENERATED_BODY()
+public:
+	
+};
