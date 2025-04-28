616f293e16e4d5bc346606a2154b5296c6e19e68
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-28 19:12:49 +1200

    * load the dt

--- Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard01.cpp
+++ Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard01.cpp
@@ -2,15 +2,27 @@
 
 #include "LogicCard01.h"
 
+#include "JUtils/Misc/JUtilsMisc.h"
+
 ULogicCard01::ULogicCard01() {
 	Entries.Reserve(1);
-	Entries = {
-		{.From = {}, .To ={}}
-	}; // TODO create defaults
-	// TODO at some point have a data table?
+
+	// since this is an "instance" class, this is fine.
+	static ConstructorHelpers::FObjectFinder<UDataTable>
+		CDT(TEXT("/Game/LifeDev/...."));
+	DT = CDT.Object; // TODO
 }
 
 void ULogicCard01::Use_Implementation() {
 	Super::Use_Implementation();
 	// find a group of items, replace with another
+}
+
+void ULogicCard01::BeginPlay_Implementation() {
+	Super::BeginPlay_Implementation();
+	if (UNLIKELY(!DT)) {
+		UE_LOG(LogTemp, Warning, TEXT("%hs datatable not set"), __func__);
+	}
+
+	UJUtilsMisc::ReadTable<FCard01Entry>(DT, Entries);
 }
--- Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard01.h
+++ Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard01.h
@@ -27,7 +27,12 @@ class LIFEDEV_API ULogicCard01: public ULogicCard {
 public:
 	ULogicCard01();
 	virtual void Use_Implementation() override;
+	virtual void BeginPlay_Implementation() override;
+
 	UPROPERTY(BlueprintReadWrite)
 	TArray<FCard01Entry> Entries;
+
 protected:
+	UPROPERTY(Transient)
+	TObjectPtr<UDataTable> DT = nullptr;
 };
