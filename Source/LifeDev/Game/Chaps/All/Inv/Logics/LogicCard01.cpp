7b7c7ce5ca4993b7d6ced7af4b3198d65741fc78
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-28 20:06:51 +1200

    * adding some auto dialogs * improve hte code * adde some entries

--- Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard01.cpp
+++ Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard01.cpp
@@ -2,6 +2,7 @@
 
 #include "LogicCard01.h"
 
+#include "Diags/Diags.h"
 #include "Inventory/Inventory.h"
 
 ULogicCard01::ULogicCard01() {
@@ -14,14 +15,47 @@ ULogicCard01::ULogicCard01() {
 	// TODO be able to check the amount of each object
 }
 
+
 void ULogicCard01::Use_Implementation() {
 	Super::Use_Implementation();
+	static FString NOk("Item.Use.C1.Ok.");
+	static FName NFail("Item.Use.C1.Fail"); // avoid conversion each time.
 	if (UNLIKELY(!DT)) {
 		UE_LOG(LogTemp, Warning, TEXT("%hs datatable not set"), __func__);
 		return;
 	}
+	if (UNLIKELY(!Inv)) {
+		UE_LOG(LogTemp, Warning, TEXT("%hs no inventory."), __func__);
+		return;
+	}
 
 	// find a group of items, replace with another
+	bool HasAll = false;
+	for (TMap<FName, uint8*>::TConstIterator I(DT->GetRowMap().CreateConstIterator()); I; ++I ) {
+		const FCard01Entry* pE = reinterpret_cast<FCard01Entry*>(I.Value());
+		if (UNLIKELY(!pE)) continue;
+
+		HasAll = true;
+		for (const FName& F: pE->From) {
+			if (Inv->Has(F)) continue;
+			HasAll = false;
+			break;
+		}
+
+		if (LIKELY(!HasAll)) continue; 
+
+		if (pE->Consume)
+			for (const FName& F: pE->From)
+				Inv->Mod(F, -1);
+
+		for (const FName& F: pE->To)
+			Inv->Mod(F, 1);
+
+		Diags->AddId(FName(NOk+I.Key().ToString()));
+		return;
+	}
+
+	Diags->AddId(NFail);
 }
 
 void ULogicCard01::BeginPlay_Implementation() {
