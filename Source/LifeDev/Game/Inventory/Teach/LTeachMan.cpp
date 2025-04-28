e38e02cfb9210c7367d568a4bd894821628ff1c1
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-28 18:25:15 +1200

    * added mf for fake light

--- Source/LifeDev/Game/Inventory/Teach/LTeachMan.cpp
+++ Source/LifeDev/Game/Inventory/Teach/LTeachMan.cpp
@@ -235,7 +235,7 @@ void ALTeachMan::FeatUp(const EFeat Feat, const bool Enabled) {
 	// https://www.youtube.com/watch?v=g-WPhYREFjk
 	// according to Fedor Pikus this is BAD (most of the time) unfortunately he didn't say way
 	// and while i don't know him nor i have put the time to dis/prove it,
-	// i think it's well researched and seems true. and there's no reason to complicate this   
+	// i think it's well researched and seems true. and there's no reason to complicate this
 	// void(ALTeachMan::* X[] )() = {&ALTeachMan::DeInitFeat, &ALTeachMan::InitFeat};
 	// (this->*X[Enabled])();
 	if (Enabled) { InitFeat(); } else { DeInitFeat(); }
