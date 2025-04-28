25d9e4518da04e54e8742019cb407219645a310c
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-28 18:55:41 +1200

    * struct

--- Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard01.cpp
+++ Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard01.cpp
@@ -1,3 +1,16 @@
 // Copyright Jerónimo Barraco-Mármol
 
 #include "LogicCard01.h"
+
+ULogicCard01::ULogicCard01() {
+	Entries.Reserve(1);
+	Entries = {
+		{.From = {}, .To ={}}
+	}; // TODO create defaults
+	// TODO at some point have a data table?
+}
+
+void ULogicCard01::Use_Implementation() {
+	Super::Use_Implementation();
+	// find a group of items, replace with another
+}
--- Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard01.h
+++ Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard01.h
@@ -6,9 +6,28 @@
 
 #include "LogicCard01.generated.h"
 
+USTRUCT(Blueprintable, BlueprintType)
+struct FCard01Entry {
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TArray<FName> From;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	TArray<FName> To;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+	bool Consume = true;
+};
+
 // item logic for card 01
 UCLASS(Blueprintable, BlueprintType)
 class LIFEDEV_API ULogicCard01: public ULogicCard {
 	GENERATED_BODY()
+
 public:
+	ULogicCard01();
+	virtual void Use_Implementation() override;
+	UPROPERTY(BlueprintReadWrite)
+	TArray<FCard01Entry> Entries;
+protected:
 };
