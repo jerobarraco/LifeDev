a19e3716daa36489b5b9f4999748dc4a054f419e
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-04-30 18:49:00 +1200

    * reorganized files * load room00 at end of c0s1

--- Source/LifeDev/Game/Chaps/00/Steps/LStepC0S001.cpp
+++ Source/LifeDev/Game/Chaps/00/Steps/LStepC0S001.cpp
@@ -26,6 +26,9 @@ ALStepC0S001::ALStepC0S001():Super() {
 	UseRain = true;
 	UseGhosts = true;
 	GhostPos = FVector(210,-42,-65);
+	static ConstructorHelpers::FObjectFinder<UDataLayerAsset>
+		CDLR0(TEXT("/Game/LifeDev/Game/Sys/DataLayers/Rooms/Room00"));
+	if (LIKELY(CDLR0.Succeeded())) DL_Load.Add(CDLR0.Object);
 }
 
 void ALStepC0S001::Start_Implementation() {
