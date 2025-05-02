2574d1f0635357866bf418def7b902aaa7a12675
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-05-02 20:18:32 +1200

    * set interact properties * added phone to the level

--- Source/LifeDev/Game/Chaps/All/Inters/Var/Phone/Phone00.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Var/Phone/Phone00.cpp
@@ -3,10 +3,15 @@
 #include "Phone00.h"
 
 #include "CQuickMesh.h"
+#include "Components/AudioComponent.h"
+#include "Interact/CInteract.h"
 
 APhone00::APhone00():Super() {
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CM(TEXT("/Game/LifeDev/Game/Inters/Var/Phone00_P"));
 	Mesh->SetStaticMesh(CM.Object);
+	Interact->SetRelativeLocation(FVector(15.,-15,5));
+	Interact->SetBoxExtent(FVector(15, 15, 5));
+	SFX->SetRelativeLocation(FVector(0,0, 5));
 	SetMobility(EComponentMobility::Static);
 }
