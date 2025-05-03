c6a2987487390b3cce0831fb1f1ffd56afa9261c
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-05-03 18:20:57 +1200

    * moving a tsoftobjectptr to cpp

--- Source/LifeDev/Game/Chaps/04/Steps/LStepC4S004.cpp
+++ Source/LifeDev/Game/Chaps/04/Steps/LStepC4S004.cpp
@@ -18,6 +18,11 @@ ALStepC4S004::ALStepC4S004():Super() {
 	RandFB->DelayMax = .85;
 	RandFB->ValueMin = -.015;
 	RandFB->ValueMax = .005;
+	IntersActivate = {
+		TSoftObjectPtr<AInteract>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.StaticMeshActor_UAID_D8BBC116E501C1E701_2080650083"))
+		// this one does not work
+		// TSoftObjectPtr<AInteract>(FSoftObjectPath("/Game /__ExternalActors__/LifeDev/Game/Sys/Game_L/9/UP/CQK85FYMK0W4PWZQYSXILC.uasset"))
+	};
 }
 
 // enables the fridge and rice and pot
