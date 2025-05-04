38e2fe5ff83f5b271ad00a8bfccbba38f385adcc
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-05-04 18:33:46 +1200

    * datalayer soft load c4s0

--- Source/LifeDev/Game/Chaps/04/Steps/LStepC4S000.cpp
+++ Source/LifeDev/Game/Chaps/04/Steps/LStepC4S000.cpp
@@ -19,38 +19,6 @@ ALStepC4S000::ALStepC4S000():Super() {
 	FinishPostWait = false;
 	TeleportChar = true;
 	ItemsFinish = {LDConsts::Items::Card4};
-
-	Music = FSoftObjectPath(TEXT("/Game/LifeDev/Game/Env/Music/Music07/Music06-07_MS.Music06-07_MS"));
-	// needed
-	if (UNLIKELY(IsRunningCookCommandlet())) Music.LoadSynchronous();
-	// TODo use softpaths
-
-	static ConstructorHelpers::FObjectFinder<UDataLayerAsset>
-		CDL1 (TEXT("/Game/LifeDev/Game/Sys/DataLayers/Chaps/Chap04_DL.Chap04_DL"));
-	if (LIKELY(CDL1.Succeeded())) DL_Load.Add(CDL1.Object);
-	static ConstructorHelpers::FObjectFinder<UDataLayerAsset>
-		CDLK0 (TEXT("/Game/LifeDev/Game/Sys/DataLayers/Rooms/Kitchen"));
-	if (LIKELY(CDLK0.Succeeded())) DL_Load.Add(CDLK0.Object);
-
-	static ConstructorHelpers::FObjectFinder<UDataLayerAsset>
-		CDL2 (TEXT("/Game/LifeDev/Game/Sys/DataLayers/Chaps/Chap03_DL.Chap03_DL"));
-	if (LIKELY(CDL2.Succeeded())) DL_Unload.Add(CDL2.Object);
-	static ConstructorHelpers::FObjectFinder<UDataLayerAsset>
-		CDLH0 (TEXT("/Game/LifeDev/Game/Sys/DataLayers/Rooms/Hall00"));
-	if (LIKELY(CDLH0.Succeeded())) DL_Unload.Add(CDLH0.Object);
-	static ConstructorHelpers::FObjectFinder<UDataLayerAsset>
-		CDLL0 (TEXT("/Game/LifeDev/Game/Sys/DataLayers/Rooms/Living00"));
-	if (LIKELY(CDLL0.Succeeded())) DL_Unload.Add(CDLL0.Object);
-
-	// unload unnecessary outside props
-	static ConstructorHelpers::FObjectFinder<UDataLayerAsset>
-		CDLO3 (TEXT("/Game/LifeDev/Game/Sys/DataLayers/Outside/Outside_C.Outside_C"));
-	if (LIKELY(CDLO3.Succeeded())) DL_Unload.AddUnique(CDLO3.Object);
-
-	static ConstructorHelpers::FObjectFinder<UDataLayerAsset>
-		CDLO4 (TEXT("/Game/LifeDev/Game/Sys/DataLayers/Outside/Outside_D.Outside_D"));
-	if (LIKELY(CDLO4.Succeeded())) DL_Unload.Add(CDLO4.Object);
-
 	ItemsEnsure = {
 		LDConsts::Items::Card0,
 		LDConsts::Items::Card1,
@@ -58,6 +26,28 @@ ALStepC4S000::ALStepC4S000():Super() {
 		LDConsts::Items::Card3,
 		LDConsts::Items::Walkman
 	};
+
+	Music = FSoftObjectPath(TEXT("/Game/LifeDev/Game/Env/Music/Music07/Music06-07_MS.Music06-07_MS"));
+	// needed
+	if (UNLIKELY(IsRunningCookCommandlet())) Music.LoadSynchronous();
+
+	DL_Load.Add(TSoftObjectPtr<UDataLayerAsset>(
+		FSoftObjectPath("/Game/LifeDev/Game/Sys/DataLayers/Chaps/Chap04_DL.Chap04_DL")));
+	DL_Load.Add(TSoftObjectPtr<UDataLayerAsset>(
+		FSoftObjectPath("/Game/LifeDev/Game/Sys/DataLayers/Rooms/Kitchen.Kitchen")));
+
+	// unload
+	DL_Unload.Add(TSoftObjectPtr<UDataLayerAsset>(
+		FSoftObjectPath("/Game/LifeDev/Game/Sys/DataLayers/Chaps/Chap03_DL.Chap03_DL")));
+	DL_Unload.Add(TSoftObjectPtr<UDataLayerAsset>(
+		FSoftObjectPath("/Game/LifeDev/Game/Sys/DataLayers/Rooms/Hall00.Hall00")));
+	DL_Unload.Add(TSoftObjectPtr<UDataLayerAsset>(
+		FSoftObjectPath("/Game/LifeDev/Game/Sys/DataLayers/Rooms/Living00.Living00")));
+	// unload unnecessary outside props
+	DL_Unload.Add(TSoftObjectPtr<UDataLayerAsset>(
+		FSoftObjectPath("/Game/LifeDev/Game/Sys/DataLayers/Outside/Outside_C.Outside_C")));
+	DL_Unload.Add(TSoftObjectPtr<UDataLayerAsset>(
+		FSoftObjectPath("/Game/LifeDev/Game/Sys/DataLayers/Outside/Outside_D.Outside_D")));
 }
 
 void ALStepC4S000::TryStart_Implementation() {
