d6a597b02947b53dafcaa48e575f2c3ff7a44d45
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-05-06 18:53:51 +1200

    * poem hint

--- Source/LifeDev/Game/Chaps/All/Inters/Poems/PoemI01.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Poems/PoemI01.cpp
@@ -6,4 +6,7 @@ APoemI01::APoemI01():Super() {
 	RewardItem = "P01";
 	// TriggerDlg = "P01_Look"; // using the autodialog
 	RewardFlash = -.1f;
+	RewardIntersHint = {
+		TSoftObjectPtr<AInteract>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.B_Cassette00_C_UAID_D8BBC116E501478C01_1236035498"))
+	};
 }
