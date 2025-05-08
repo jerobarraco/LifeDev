e50786a37cfb0558852e3c532b69bc2ff94a587f
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-05-08 20:45:53 +1200

    * attempt to fix interact prompt hiding on reward

--- Plugins/Interact/Source/Interact/CInteractor.cpp
+++ Plugins/Interact/Source/Interact/CInteractor.cpp
@@ -175,16 +175,20 @@ void UCInteractor::DoEnd() {
 
 void UCInteractor::DoStart(UCInteract* const Component) {
 	const UCInteract* const PHover = HoverComp.Get();
+	const bool ValidHover = IsValid(PHover);
+	// TODO fix
 	// on every tick almost
 	// skip retries
-	if (LIKELY(Component == PHover)) return; // pretty likely it's the same from the previous frame
+	// pretty likely it's the same from the previous frame 
+	// Valid is important since an object could be destroyed on reward. comp == pHover but it won't be valid anymore.
+	if (LIKELY((Component == PHover) & ValidHover)) return;
 
 	UE_LOG(LogCInteractor, Log, TEXT("%hs: %s"),
 		__func__, *GetNameSafe(this));
 
 	DoEnd(); // does checks and nullifies. this would allow to clean by calling doStart with null
 
-	if (UNLIKELY(!IsValid(Component))) return;
+	if (UNLIKELY(!IsValid(Component) & !ValidHover)) return;
 
 	HoverComp = Component;
 	Component->Hover(true, Cast<APawn>(GetOwner()));
