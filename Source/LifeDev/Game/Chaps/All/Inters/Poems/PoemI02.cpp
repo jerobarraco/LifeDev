b91c9fc97a88961663533ae3a14331a38f44a27e
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-05-08 20:22:45 +1200

    * fixed poem on hall02 * moved a bunch of stuff to hall dl

--- Source/LifeDev/Game/Chaps/All/Inters/Poems/PoemI02.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Poems/PoemI02.cpp
@@ -7,7 +7,7 @@
 APoemI02::APoemI02():Super() {
 	RewardFlash = -.2f;
 	RewardItem = LDConsts::Items::Poem2;
-
-	// Can't be static since it will be inside of a drawer
+	Super::SetActorHiddenInGame(true); // made visible by step
+	// Can't be static since it will be inside a drawer
 	Super::SetMobility(EComponentMobility::Movable);
 }
