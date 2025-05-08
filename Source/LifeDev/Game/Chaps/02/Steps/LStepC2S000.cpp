bcd7bfb00b9c631152480964650efefdce899477
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-05-08 20:14:11 +1200

    * fix compile

--- Source/LifeDev/Game/Chaps/02/Steps/LStepC2S000.cpp
+++ Source/LifeDev/Game/Chaps/02/Steps/LStepC2S000.cpp
@@ -52,7 +52,7 @@ ALStepC2S000::ALStepC2S000():Super() {
 	DL_Unload.Add(TSoftObjectPtr<UDataLayerAsset>(
 		FSoftObjectPath("/Game/LifeDev/Game/Sys/DataLayers/Outside/Outside_B.Outside_B")));
 	ActorsShow = { // battery
-		TSoftObjectPtr<AActor>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.BattsI00_UAID_D8BBC116E5012EC501_1469610310"));
+		TSoftObjectPtr<AActor>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.BattsI00_UAID_D8BBC116E5012EC501_1469610310"))
 	};
 }
 
