5b554685c93e6fb8428f7ebfb5743814fccb0b87
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-05-12 20:05:22 +1200

    * fixed and cleaned some diags

--- Source/LifeDev/Game/Chaps/01/Inters/Doors/DrawerI00.cpp
+++ Source/LifeDev/Game/Chaps/01/Inters/Doors/DrawerI00.cpp
@@ -3,7 +3,7 @@
 #include "DrawerI00.h"
 
 // warning. this is used in multiple places.
-// at least on ch01 besides the bed. and ch00 besides the bed.
+// at least ch00 besides the bed.
 
 void ADrawerI00::SetState_Implementation(const int32 NewState) {
 	Super::SetState_Implementation(NewState);
--- Source/LifeDev/Game/Chaps/01/Inters/Var/TvI00.cpp
+++ Source/LifeDev/Game/Chaps/01/Inters/Var/TvI00.cpp
@@ -2,10 +2,9 @@
 
 #include "TvI00.h"
 
-ATvI00::ATvI00():Super() {
-}
+ATvI00::ATvI00():Super() { }
 
 void ATvI00::SetState_Implementation(const int32 NewState) {
 	Super::SetState_Implementation(NewState);
-	TriggerDlg = IsEven() ? "": "TV00_T";
+	// TriggerDlg = IsEven() ? "": "TV00_T";
 }
