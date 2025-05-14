53b660ccad792e7d051d4617e166b04ebc478c4c
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-05-14 20:20:05 +1200

    * clouti02 hint

--- Source/LifeDev/Game/Chaps/01/Inters/Clouts/CloutI02.cpp
+++ Source/LifeDev/Game/Chaps/01/Inters/Clouts/CloutI02.cpp
@@ -4,4 +4,7 @@
 
 ACloutI02::ACloutI02():Super() {
 	RewardItem = TEXT("C1C02"); // chapter 1 clout 3
+	RewardIntersHint = {
+		TSoftObjectPtr<AInteract>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.Spot00_UAID_D8BBC116E5019D8C01_1333541631"))
+	};
 }
