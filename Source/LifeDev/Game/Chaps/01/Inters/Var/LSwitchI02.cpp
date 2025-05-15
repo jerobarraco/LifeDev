5daf8ffaffde1a54927b04bc3745bb49d1cd271e
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-05-15 19:10:51 +1200

    * added switchi02 * fixed drawers

---
+++ Source/LifeDev/Game/Chaps/01/Inters/Var/LSwitchI02.cpp
@@ -0,0 +1,27 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#include "LSwitchI02.h"
+
+ALSwitchI02::ALSwitchI02(): Super() {
+	IsOneShot = false;
+	UseActiveOnce = false;
+	UseAutoActivate = true; // important
+
+	static ConstructorHelpers::FObjectFinder<USoundBase>
+		CSfx(TEXT("/Game/LifeDev/Game/Inters/Generic/Sparks/Tone_Sound_Design_Electric_Spark_Gritty_Thin_Crackle_Dense_Steady_0"));
+	SFXs[1] = CSfx.Object;
+
+	static ConstructorHelpers::FObjectFinder<UNiagaraSystem>
+		CPart0(TEXT("/Game/LifeDev/Game/Inters/Generic/Sparks/Sparks_N"));
+	Particles = {
+		nullptr,
+		CPart0.Object
+	};
+	
+	RewardIntersTrigger = {
+		TSoftObjectPtr<AInteract>(FSoftObjectPath(
+			"/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.Light00_B_C_UAID_D8BBC116E5013DAE01_2079906675")),
+		TSoftObjectPtr<AInteract>(FSoftObjectPath(
+			"/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.B_Fluorescent_C_UAID_D8BBC116E501E0B901_1128252023"))
+	};
+}
---
+++ Source/LifeDev/Game/Chaps/01/Inters/Var/LSwitchI02.h
@@ -0,0 +1,15 @@
+// Copyright Jerónimo Barraco-Mármol
+
+#pragma once
+#include "LifeDev/Game/Chaps/All/Inters/Lights/LSwitch02.h"
+
+#include "LSwitchI02.generated.h"
+
+// instance for ch00 bathroom sparky switch
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API ALSwitchI02: public ALSwitch02 {
+	GENERATED_BODY()
+
+public:
+	ALSwitchI02();
+};
