2e7495b1acb8f443e0be1f26a26731c1b8143efe
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-05-15 20:36:44 +1200

    * replaced sfx_trigger on clout

--- Source/LifeDev/Game/Interact/Base/Clout.cpp
+++ Source/LifeDev/Game/Interact/Base/Clout.cpp
@@ -6,6 +6,7 @@
 #include "Interact/Animator/CAnimatorMix.h"
 
 AClout::AClout():Super() {
+	StateNum = 1;
 	RewardFlash = .1;
 	// a bit lazy, ideally should be set to true on each child instance.
 	// Assumes all children will fade. will create a new material.
@@ -16,13 +17,14 @@ AClout::AClout():Super() {
 	// but that's: UNLIKELY(that)
 	UseAutoActivate = true;
 
-	static ConstructorHelpers::FObjectFinder<USoundBase>
-		CSnd (TEXT("/Game/LifeDev/Game/Inters/Clouts/Clouts.Clouts"));
-	SFX_Trigger = CSnd.Object;
-	Mesh->SetCastAllShadows(true);
-
 	UseAnim = false;
 	Anim->TRoot = nullptr;
 	Anim->Mat = nullptr;
+	static ConstructorHelpers::FObjectFinder<USoundBase>
+		CSnd (TEXT("/Game/LifeDev/Game/Inters/Clouts/Clouts.Clouts"));
+	// SFX_Trigger = CSnd.Object;
+	SFXs = {CSnd.Object};
+	Mesh->SetCastAllShadows(true);
+
 	Super::SetMobility(EComponentMobility::Static);
 }
