73023f56f5b7f7c254e040b1adbc4f56012a3e11
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-05-19 20:19:45 +1200

    * fix bra on ch01

--- Source/LifeDev/Game/Chaps/All/Inters/Clouts/Clout03.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Clouts/Clout03.cpp
@@ -7,6 +7,9 @@
 #include "Interact/CInteract.h"
 
 AClout03::AClout03():Super() {
+	Texts = { NSLOCTEXT("Clout03", "State0", "A bra") }; // used on the bra in ch01, on both.
+	// one of them is not meant to be picked
+
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		ObjMesh(TEXT("/Game/LifeDev/Game/Inters/Clouts/Bra00.Bra00"));
 	if (LIKELY(ObjMesh.Succeeded())) Mesh->SetStaticMesh(ObjMesh.Object);
