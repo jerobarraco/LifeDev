74fd2d1a428473a7b76668c6aa8f77c194b704df
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-06-09 19:15:08 +1200

    * stub loadscreen

---
+++ Plugins/JUtils/Source/JUtils/UI/LoadScr.h
@@ -0,0 +1,12 @@
+// Copyright (C) 2023 - Jeronimo Barraco-Marmol. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#pragma once
+#include "LoadScr.generated.h"
+
+UCLASS(Blueprintable)
+class JUTILS_API ULoadScr: public UGameInstanceSubsystem {
+	GENERATED_BODY()
+public:
+	
+};
