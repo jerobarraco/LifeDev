9a71ce8426299f13e2ea40eaef6d0b766c686a8f
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-06-10 18:48:37 +1200

    * load scr on the settings

--- Source/LifeDev/Core/Settings/LSysSettings.h
+++ Source/LifeDev/Core/Settings/LSysSettings.h
@@ -67,6 +67,11 @@ public:
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Config, Category="Sys")
 	FString CloseURL = TEXT("https://forms.gle/aYCr8zRR3wWTomyu8");
 
+	// The widget to load for the loading screen.
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Config, Category=Sys, meta=(MetaClass="/Script/UMG.UserWidget"))
+	TSoftObjectPtr<UUserWidget> LoadingScr = TSoftObjectPtr<UUserWidget>(
+		FSoftObjectPath("/JUtils/UI/TestLoadScr.TestLoadScr"));
+
 	// The list of items
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Config, Category="Inventory",
 		meta=(RequiredAssetDataTags="RowStructure=/Script/Inventory.Item"))
