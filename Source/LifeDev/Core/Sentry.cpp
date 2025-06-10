2e2ae569ed29740755764572de7d5367b669d0f6
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-06-10 19:41:08 +1200

    * patch sentry being called in bg thread

--- Source/LifeDev/Core/Sentry.cpp
+++ Source/LifeDev/Core/Sentry.cpp
@@ -20,7 +20,8 @@ DEFINE_LOG_CATEGORY_STATIC(LogSentry, Log, Log);
 USentry* USentry::Instance(const UObject* const O) {
 	const ULGameInstance* const Instance = ULGameInstance::Instance(O);
 	if (UNLIKELY(!IsValid(Instance))) return nullptr;
-
+	// todo fix properly. probably sending calls to the game thread
+	if (!IsInGameThread()) return nullptr; // no can do. it will get into a crash loop
 	return Instance->GetSubsystem<USentry>();
 }
 
