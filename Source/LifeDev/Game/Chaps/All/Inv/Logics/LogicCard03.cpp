82a2e1d4b020babc21bb3a686a1b981e5a665855
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-06-16 19:08:02 +1200

    * added base logic for card03 * added to the inventory

--- Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard03.cpp
+++ Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard03.cpp
@@ -2,9 +2,26 @@
 
 #include "LogicCard03.h"
 
+#include "Inventory/Inventory.h"
+#include "JUtils/Misc/JUtilsMisc.h"
+
 ULogicCard03::ULogicCard03() {
+}
+
+namespace LD::Card03 {
+	static const char* Names[] = {
+		"C0", "C1", "C2", "C4", "C5" // not c3, it would spam 
+	};
 }
 
 void ULogicCard03::Use_Implementation() {
 	Super::Use_Implementation();
+	
+	UInventory* const Inventory = UInventory::Instance(this);
+	if (UNLIKELY(Inventory)) return;
+	
+	constexpr size_t Size = UJUtilsMisc::ArraySize(LD::Card03::Names);
+	for (size_t i = 0; i<Size; i++) {
+		Inventory->SetCool(LD::Card03::Names[i]);
+	}
 }
