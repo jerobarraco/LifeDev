ceee33bf45c2d119eec10c5d50e02cbb10f884d9
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-06-20 20:07:44 +1200

    * fixed compilation

--- Source/LifeDev/Game/Interact/LInteractSpot.h
+++ Source/LifeDev/Game/Interact/LInteractSpot.h
@@ -8,13 +8,14 @@
 // TODO deprecated. since most functionality will be moved to base interacts.
 // TODO just delete useInc and UseOrder. i'm not using them anyway.
 
+// DEPRECATED
 // A Place to drop other interacts.
 // It's always locked, and triggers only once when all the items are dropped (used with).
 // It will trigger LockedDlg (when not done) and TriggerDlg (once upon done) accordingly.
 // disabled: if attempt to use an item once it's done it will trigger FullDlg
 // always has two states: enabled, done.
 // by default is disabled after completed.
-UCLASS(Blueprintable, BlueprintType, Deprecated)
+UCLASS(Blueprintable, BlueprintType)
 class LIFEDEV_API ALInteractSpot: public ALInteract {
 	GENERATED_BODY()
 
