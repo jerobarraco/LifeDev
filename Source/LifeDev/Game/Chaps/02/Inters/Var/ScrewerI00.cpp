d9509f5b0d798a1f0a6ebfa1fc807d8be49a9443
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-06-27 19:41:06 +1200

    * screwer is static

--- Source/LifeDev/Game/Chaps/02/Inters/Var/ScrewerI00.cpp
+++ Source/LifeDev/Game/Chaps/02/Inters/Var/ScrewerI00.cpp
@@ -6,5 +6,5 @@ AScrewerI00::AScrewerI00():Super() {
 	RewardItem = "Screwer00";
 	UseFade = true;
 	UseAutoActivate = true;
-	AScrewerI00::SetMobility(EComponentMobility::Movable);
+	AScrewerI00::SetMobility(EComponentMobility::Type::Static);
 }
