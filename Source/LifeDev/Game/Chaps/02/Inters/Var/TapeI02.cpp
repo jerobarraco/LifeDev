9573016c0a06872e6937016b7726dcb00319cc6a
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-06-27 20:08:44 +1200

    * hint on TapeI02.cpp

--- Source/LifeDev/Game/Chaps/02/Inters/Var/TapeI02.cpp
+++ Source/LifeDev/Game/Chaps/02/Inters/Var/TapeI02.cpp
@@ -8,18 +8,20 @@ ATapeI02::ATapeI02():Super() {
 	UseAutoActivate = true;
 	UseAnim = false;
 	UseFade = true;
-	IsLocked = true;
+	UseHint = true;
 	StateNum = 1;
 	Texts = { NSLOCTEXT("TapeI02","State0","Another of my tapes") };
+	UseRewardDestroy = true;
 	RewardFlash = .1;
-	// TODO find a way to not have to reward the tape. it's confusing.
+	// note: find a way to not have to reward the tape. it's confusing.
 	// but i need it because that's how the step is advanced. happen at different times.
 	RewardItem = LDConsts::Items::Tape2;
-	UseRewardDestroy = true;
+
+	IsLocked = true;
 	// important to require batteries since im going to use it directly on c2s3
-	// ULockItemReq = LDConsts::Items::Batts;
 	UnlockItems = { LDConsts::Items::Batts };
-	LockedDlg = FName("T02_L");
+	LockedDlg = FName("T02_L"); // todo autodialog
+
 	// LockedItemDlg = FName("T02_LI"); // will hint to use the batts
 	// TriggerDlg = uses autodiag // will say what it is. triggered on pick up
 	// Don't trigger the dialog here since it will trigger in the step, otherwise it will break the step
