ec3508bc5a13b408ad57ffd2b9394cfcf72a8ce9
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-07-11 19:56:18 +1200

    * improved hinting on DoorI12.cpp

--- Plugins/Interact/Source/Interact/CPuzzle.cpp
+++ Plugins/Interact/Source/Interact/CPuzzle.cpp
@@ -324,7 +324,7 @@ void UCPuzzle::ShowHint() {
 	UE_LOG(LogCPuzzle, Log, TEXT("%hs Obj=%s"), __func__, *GetNameSafe(GetOwner()));
 	for (AInteract* const I: Interacts) {
 		if (UNLIKELY(!IsValid(I))) continue;
-		I->SetUseHint(true); // reset in case it got dehint on trigger.
+		I->SetUseHint(true); // reset in case it got de-Hint on trigger.
 		I->ShowHint();
 	}
 }
--- Source/LifeDev/Game/Chaps/02/Inters/Doors/DoorI12.cpp
+++ Source/LifeDev/Game/Chaps/02/Inters/Doors/DoorI12.cpp
@@ -6,5 +6,7 @@ ADoorI12::ADoorI12():Super() {
 	IsLocked = true;
 	// LockedDlg = "D12_L";
 	UseHint = true; // this door goes nowhere, but hint the player in the story flow.
-	// todo hint condition door10 is not triggered
+	// disable hint once the user tried the doori10
+	// checking for locked and trigger since the player could trigger the door without the locked
+	HintCondition = "!({Inter.Locked.DoorI10}|{Inter.Trigger.DoorI10})";
 }
