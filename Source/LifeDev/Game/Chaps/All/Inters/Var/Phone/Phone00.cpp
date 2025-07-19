448253907ba9e9a8a9d73a065f70fb7ba941cba2
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-07-19 20:18:05 +1200

    * fixed base phone text

--- Source/LifeDev/Game/Chaps/All/Inters/Var/Phone/Phone00.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Var/Phone/Phone00.cpp
@@ -7,12 +7,18 @@
 #include "Interact/CInteract.h"
 
 APhone00::APhone00():Super() {
+	StateNum = 1;
+	Texts = { NSLOCTEXT("Phone00", "State0", "Phone") };
+	UseAnim = false;
+	UseFade = false;
+
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CM(TEXT("/Game/LifeDev/Game/Inters/Rooms/Phone/Phone00_P"));
 	Mesh->SetStaticMesh(CM.Object);
 	Interact->SetRelativeLocation(FVector(15.,-15,5));
 	Interact->SetBoxExtent(FVector(15, 15, 5));
 	SFX->SetRelativeLocation(FVector(0,0, 5));
+
 	SetMobility(EComponentMobility::Static);
 	
 	static ConstructorHelpers::FObjectFinder<USoundBase>
