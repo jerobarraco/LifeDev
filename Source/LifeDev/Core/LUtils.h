3cf57c9837354e619432bcbe1ea424f706c6eb03
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-07-30 09:53:49 +1200

    * added stub lbutton

--- Source/LifeDev/Core/LUtils.h
+++ Source/LifeDev/Core/LUtils.h
@@ -1,4 +1,3 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Engine/DataTable.h"
---
+++ Source/LifeDev/Core/Settings/UI/LButton.h
@@ -0,0 +1,10 @@
+// Copyright (C) 2023 - Jeronimo Barraco-Marmol. All rights reserved.
+
+#pragma once
+#include "JButton.h"
+
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API ULButton: public UJButton {
+public:
+	
+};
