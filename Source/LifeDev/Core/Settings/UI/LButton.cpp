7997933c0fff1fc1ce4857b6a1375542e71296ab
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-07-30 10:10:30 +1200

    * attempt at adding sentry to lbutton

--- Source/LifeDev/Core/Settings/UI/LButton.cpp
+++ Source/LifeDev/Core/Settings/UI/LButton.cpp
@@ -2,6 +2,11 @@
 
 #include "LButton.h"
 
+#include "LifeDev/Core/Sentry.h"
+
 void ULButton::DoClick_Implementation() {
+	const USentry* const Sentry = USentry::Instance(this);
+	if (LIKELY(Sentry)) Sentry->AddHint("LButton", {{"Name", GetNameSafe(this)}});
+	
 	Super::DoClick_Implementation();
 }
