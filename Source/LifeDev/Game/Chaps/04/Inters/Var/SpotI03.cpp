ba5bd1ee72742b88d81973a391c38606ad15eb27
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-01 17:48:36 +1200

    * SpotI03.cpp

--- Source/LifeDev/Game/Chaps/04/Inters/Var/SpotI03.cpp
+++ Source/LifeDev/Game/Chaps/04/Inters/Var/SpotI03.cpp
@@ -6,7 +6,7 @@
 
 ASpotI03::ASpotI03():Super() {
 	TriggerDlg = "IS3_T"; // when completed
-	LockedDlg = "IS3_L"; // when not full, try trigger
+	// LockedDlg = "IS3_L"; // when not full, try trigger
 	UnlockItems = { LDConsts::Items::Plate02 }; // empty plate
 
 	// UseAutoActivate = false; // the steps will enable it // redundant
