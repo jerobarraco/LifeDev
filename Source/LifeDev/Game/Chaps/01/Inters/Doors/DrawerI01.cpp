1a03ca70436673e3c1bc26d1cb9da046ec54f976
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-01 20:35:33 +1200

    * ported DrawerI01.cpp

--- Source/LifeDev/Game/Chaps/01/Inters/Doors/DrawerI01.cpp
+++ Source/LifeDev/Game/Chaps/01/Inters/Doors/DrawerI01.cpp
@@ -2,11 +2,3 @@
 
 #include "DrawerI01.h"
 
-
-// warning. this is potentially used in multiple places.
-// at least on ch00 besides the bed.
-
-void ADrawerI01::SetState_Implementation(const int32 NewState) {
-	Super::SetState_Implementation(NewState);
-	TriggerDlg = IsEven() ? "": "DR01_T";
-}
--- Source/LifeDev/Game/Chaps/01/Inters/Doors/DrawerI01.h
+++ Source/LifeDev/Game/Chaps/01/Inters/Doors/DrawerI01.h
@@ -6,11 +6,13 @@
 
 #include "DrawerI01.generated.h"
 
+// warning. this is potentially used in multiple places.
+// at least on ch00 besides the bed.
+
+// TODO replace the triggeldlg with auto dialog, then remove this class like draweri00
+
 // Drawer 02 instance 00
 UCLASS(Blueprintable, BlueprintType)
 class LIFEDEV_API ADrawerI01: public ADrawer02 {
 	GENERATED_BODY()
-
-protected:
-	virtual void SetState_Implementation(const int32 NewState) override;
 };
