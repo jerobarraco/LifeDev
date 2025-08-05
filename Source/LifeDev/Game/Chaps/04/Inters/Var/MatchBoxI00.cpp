7c5ec7c613002b0314ea0fefe45b553bd4651273
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-06 10:12:10 +1200

    * MatchBoxI00.cpp

--- Source/LifeDev/Game/Chaps/04/Inters/Var/MatchBoxI00.cpp
+++ Source/LifeDev/Game/Chaps/04/Inters/Var/MatchBoxI00.cpp
@@ -10,7 +10,7 @@ AMatchBoxI00::AMatchBoxI00():Super() {
 	UseFade = true;
 	UseHint = true;
 	// Locked = true;
-	TriggerDlg = "Matches00_T";
+	// TriggerDlg = "Matches00_T";
 	// LockedDlg = "Matches00_L";
 	RewardFlash = .1; // story works better this way
 	//UseAutoActivate = false; // activated by the npci06 // redundant
