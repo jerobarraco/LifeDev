8395af65a88a1ab47126c2c242c64482d1c9a9c3
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-06 20:08:11 +1200

    * wip logic card 02 reset other cards

--- Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard02.cpp
+++ Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard02.cpp
@@ -2,9 +2,31 @@
 
 #include "LogicCard02.h"
 
+#include "Inventory/Inventory.h"
+#include "JUtils/Misc/JUtilsMisc.h"
+#include "LifeDev/Core/Consts/ConstItems.h"
+
 ULogicCard02::ULogicCard02() {
 }
 
 void ULogicCard02::Use_Implementation() {
 	Super::Use_Implementation();
+	// reset the timer on one of the cards or lowers the fb
+
+	UInventory* const Inv = UInventory::Instance(this); // todo move to begin play
+
+	static TArray<FName> Cards = {
+		// notice card02 (this) is not listed
+		LDConsts::Items::Card0, LDConsts::Items::Card1,
+		LDConsts::Items::Card3, LDConsts::Items::Card4
+	};
+	UJUtilsMisc::ArrayShuffle(Cards);
+
+	const int32 Num = Cards.Num();
+	for (uint8 i =0; i<Num; ++i) {
+		const FName& n = Cards[i];
+		if (Inv->SetCool(n)) return; // notice this is a return
+	}
+
+	
 }
