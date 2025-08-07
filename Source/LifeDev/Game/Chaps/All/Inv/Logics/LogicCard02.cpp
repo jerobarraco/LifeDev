25e332e134085f68b3d39c928c8e84a3d5866885
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-07 19:24:28 +1200

    * cleanup

--- Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard02.cpp
+++ Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard02.cpp
@@ -2,9 +2,6 @@
 
 #include "LogicCard02.h"
 
-#include "Inventory/Inventory.h"
-#include "JUtils/Misc/JUtilsMisc.h"
-#include "LifeDev/Core/Consts/ConstItems.h"
 #include "LifeDev/Game/Flashback/Flashback.h"
 
 ULogicCard02::ULogicCard02() {
@@ -12,10 +9,8 @@ ULogicCard02::ULogicCard02() {
 
 void ULogicCard02::Use_Implementation() {
 	Super::Use_Implementation();
-	// reset the timer on one of the cards or lowers the fb
 
-	if (UNLIKELY(!Inv)) return;
-
+	// lowers the fb
 	if (UNLIKELY(!FB)) return;
 	FB->ModVal(FMath::FRandRange(-.05, -.1));
 }
