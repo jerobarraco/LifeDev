6bdb966a982e98a0b7cbff06dad5575a83d00940
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-07 19:30:28 +1200

    * card02 modmin and modmax are config

--- Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard02.cpp
+++ Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard02.cpp
@@ -12,5 +12,6 @@ void ULogicCard02::Use_Implementation() {
 
 	// lowers the fb
 	if (UNLIKELY(!FB)) return;
-	FB->ModVal(FMath::FRandRange(-.05, -.1));
+
+	FB->ModVal(FMath::FRandRange(ModMin, ModMax));
 }
--- Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard02.h
+++ Source/LifeDev/Game/Chaps/All/Inv/Logics/LogicCard02.h
@@ -7,11 +7,18 @@
 #include "LogicCard02.generated.h"
 
 // item logic for card 02 (yin)
-UCLASS(Blueprintable, BlueprintType)
+UCLASS(Blueprintable, BlueprintType, Config=LifeDev, DefaultConfig)
 class LIFEDEV_API ULogicCard02: public ULogicCard {
 	GENERATED_BODY()
 
 public:
 	ULogicCard02();
 	virtual void Use_Implementation() override;
+
+protected:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Config)
+	float ModMin = -.05;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Config)
+	float ModMax = -.1;
 };
