1e6039251b4977af747dd0fc3e93165ec8167465
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-08 20:35:20 +1200

    * used item animation working

--- Source/LifeDev/Game/Inventory/LInventoryUI.cpp
+++ Source/LifeDev/Game/Inventory/LInventoryUI.cpp
@@ -1,7 +1,7 @@
  // Copyright (C) 2023 - Jeronimo Barraco-Marmol. All rights reserved.
 
 #include "LInventoryUI.h"
-void ULInventoryUI::FadeUsed(const bool Fwd) {
+void ULInventoryUI::FadeUsed(const bool Fwd) { // todo move to uinventoryui
 	if (UNLIKELY(!AUsed)) return;
 
 	PlayAnimation(AUsed, 0, 1,
