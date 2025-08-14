6e25d2617ced37c6893fad281caccfd26bb5f54b
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-14 19:55:10 +1200

    * stub set item

--- Source/LifeDev/Game/Inventory/LInventoryUI.cpp
+++ Source/LifeDev/Game/Inventory/LInventoryUI.cpp
@@ -1,6 +1,9 @@
  // Copyright (C) 2023 - Jeronimo Barraco-Marmol. All rights reserved.
 
 #include "LInventoryUI.h"
+
+#include "Inventory/InventoryItemUI.h"
+
 void ULInventoryUI::FadeUsed(const bool Fwd) { // todo move to uinventoryui
 	if (UNLIKELY(!AUsed)) return;
 
@@ -21,10 +24,20 @@ void ULInventoryUI::Hide_Implementation() {
 void ULInventoryUI::SetItemMod_Implementation(const FName& Name, const int32 Diff, const FItem& Item) {
 	Super::SetItemMod_Implementation(Name, Diff, Item);
 
+	if (UNLIKELY(Name.IsNone())) return;
+
 	if (Item.Count<=0) {
 		Items.Remove(Name);
-		// todo fade
+		// todo fade, add timer. then remove.
 		return;
 	}
-	
+
+	TObjectPtr<UInventoryItemUI>* const pIt = Items.Find(Name);
+	UInventoryItemUI* const It = pIt ? pIt->Get() : nullptr;
+	if (!pIt) {
+		// todo create here
+	}
+
+	if (UNLIKELY(!It)) return; // safeguard
+	It->SetItem(Name, Item);
 }
