bd9a63b30d6f691efffe13690eca7b1fc6898cfa
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-17 19:30:12 +1200

    * initialize

--- Source/LifeDev/Core/Settings/UI/LComboStr.cpp
+++ Source/LifeDev/Core/Settings/UI/LComboStr.cpp
@@ -3,16 +3,15 @@
 #include "LComboStr.h"
 
 ULComboStr::ULComboStr() {
-	// static ConstructorHelpers::FObjectFinder<USlateWidgetStyleAsset>
-	// 	CS(TEXT("/Game/LifeDev/Core/UI/Btns/LButtonSmall_S"));
+	static ConstructorHelpers::FObjectFinder<USlateWidgetStyleAsset>
+		CCB(TEXT("/Game/LifeDev/Core/UI/Combo/LCombo_S"));
+	StyleCB = CCB.Object;
 	// static ConstructorHelpers::FObjectFinder<USlateWidgetStyleAsset>
 	// 	CST(TEXT("/Game/LifeDev/Core/UI/Btns/LTextSmall_S"));
-	// StyleCB = CS.Object;
 	// StyleItem = CST.Object;
 }
 
 void ULComboStr::ResetStyle() {
-	UE_LOG(LogTemp, Log, TEXT("%hs"), __func__);
 	if (StyleCB) {
 		const FComboBoxStyle* const S = StyleCB->GetStyle<FComboBoxStyle>();
 		if (LIKELY(S)) SetWidgetStyle(*S);
