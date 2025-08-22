52a4741431e58bd06b7a4a25ca28f2ccb55cc42d
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-22 19:22:40 +1200

    * cleanup. setusehint on piano

--- Source/LifeDev/Game/Chaps/03/Inters/Piano/PuzzleI04.cpp
+++ Source/LifeDev/Game/Chaps/03/Inters/Piano/PuzzleI04.cpp
@@ -35,6 +35,7 @@ void APuzzleI04::PostLoad() {
 
 	static const TArray<bool> Locks = {false, false, false, false};
 	SetLocks(Locks);
+	SetUseHints(true);
 }
 
 void APuzzleI04::Done_Implementation(const bool Ok) {
