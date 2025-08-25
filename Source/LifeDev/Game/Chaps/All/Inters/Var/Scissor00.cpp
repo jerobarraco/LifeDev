f60505f0da9edea51625aa291821b158d187a0cf
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-25 20:00:38 +1200

    * stub Scissor00.cpp

---
+++ Source/LifeDev/Game/Chaps/All/Inters/Var/Scissor00.cpp
@@ -0,0 +1,6 @@
+// Copyright (C) 2023-2025 Jeronimo Barraco-Marmol
+
+#include "Scissor00.h"
+
+AScissor00::AScissor00() {
+}
---
+++ Source/LifeDev/Game/Chaps/All/Inters/Var/Scissor00.h
@@ -0,0 +1,15 @@
+// Copyright (C) 2023-2025 Jeronimo Barraco-Marmol
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LifeDev/Game/Interact/LInteract.h"
+#include "Scissor00.generated.h"
+
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API AScissor00 : public ALInteract {
+	GENERATED_BODY()
+
+public:
+	AScissor00();
+};
