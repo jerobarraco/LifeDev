18cb37f180a93c6a50f67a54e27760f0db636b77
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-25 20:25:11 +1200

    * sounds

--- Source/LifeDev/Game/Chaps/All/Inters/Utils/Scissor00.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Utils/Scissor00.cpp
@@ -17,9 +17,12 @@ AScissor00::AScissor00() {
 	Mesh->SetRelativeLocation(FVector(-4.500000,0.,-1.));
 	Interact->SetRelativeLocation(FVector(4.500000,-12.500000,1.000000));
 	Interact->SetBoxExtent(FVector(4.500000,12.500000,1.));
+	// TODO fix material
 	static ConstructorHelpers::FObjectFinder<USoundBase>
-		CSnd(TEXT("/Game/LifeDev/Game/Inters/Utils/scissor_cut"));
-	SFXs = { CSnd.Object }; // TODO
+		CSnd(TEXT("/Game/LifeDev/Game/Inters/Utils/Scissor/scissor_cut"));
+	static ConstructorHelpers::FObjectFinder<USoundBase>
+		CSndB(TEXT("/Game/LifeDev/Game/Inters/Utils/Scissor/Scissors_16_Slow_Snip"));
+	SFXs = { CSnd.Object, CSndB.Object };
 	
 	ALInteract::SetMobility(EComponentMobility::Static);
 }
