0c7052c756108b1453325b172bc549010afdf15e
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-27 10:51:26 +1200

    * tapei03 hidden

--- Source/LifeDev/Game/Chaps/03/Inters/Var/TapeI03.cpp
+++ Source/LifeDev/Game/Chaps/03/Inters/Var/TapeI03.cpp
@@ -10,17 +10,19 @@ ATapeI03::ATapeI03():Super() {
 	Texts = { NSLOCTEXT("TapeI03", "State0", "One more tape") };
 	StateNum = 1;
 	UseAnim = false;
-	RewardFlash = -.1;
-	RewardItem = LDConsts::Items::Tape3;
-	UseRewardDestroy = true;
 	UseFade = true;
-	IsLocked = true;
 	UseHint = true; // auto hint since it's rewarded
 	UseAutoActivate = true; // rewarded by the puzzle.
 	// autoActivate works well, and it's needed so it gets activated on reward.
-
+	IsLocked = true;
 	UnlockItems = { LDConsts::Items::Batts };
+
+	RewardFlash = -.1;
+	RewardItem = LDConsts::Items::Tape3;
+
 	// LockedDlg = FName("T03_L");
 	// LockedItemDlg = FName("T03_LI"); // will hint to use the batts
 	// TriggerDlg = FName("T03_Look"); // will say what it is. triggered on pick up
+	SetActorHiddenInGame(true); // puzzlei06 has it as reward actor, which should hide on start. but in any case
 }
+
