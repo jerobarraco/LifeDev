d2a580ae0deb885ad268b53ebf53375bc958bf8b
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-27 10:47:57 +1200

    * use actorshow

--- Source/LifeDev/Game/Chaps/02/Steps/LStepC2S002.cpp
+++ Source/LifeDev/Game/Chaps/02/Steps/LStepC2S002.cpp
@@ -13,8 +13,8 @@ ALStepC2S002::ALStepC2S002():Super() {
 	// wait for the tape (important the next step lowers the fb to allow to find other items)
 	FinishItems = { LDConsts::Items::Tape2 };
 	RemoveItems = { LDConsts::Items::Batts };
-	IntersFadeIn = { // fade in so that increasing the step does not hide them
-		TSoftObjectPtr<ALInteract>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.PuzzleI00_UAID_D8BBC116E501E2CD01_1084873435"))};
+	ActorsShow = {
+		TSoftObjectPtr<AActor>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.PuzzleI00_UAID_D8BBC116E501E2CD01_1084873435"))};
 	IntersHint = { // tape
 		TSoftObjectPtr<AInteract>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.TapeI02_UAID_D8BBC116E5012DC501_2098743131"))};
 }
