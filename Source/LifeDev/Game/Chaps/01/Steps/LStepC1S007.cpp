4d8b945f8105e4e8c75b74fe531805b260e59a69
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-27 11:57:22 +1200

    * fix batts * made persevere usable

--- Source/LifeDev/Game/Chaps/01/Steps/LStepC1S007.cpp
+++ Source/LifeDev/Game/Chaps/01/Steps/LStepC1S007.cpp
@@ -16,7 +16,7 @@ ALStepC1S007::ALStepC1S007():Super() {
 
 	// the clothes are transient items that don't need to be carried
 	// the rest of the items are going to be used or required
-	RemoveItems = {"C1C00", "C1C01", "C1C02", "C1C03", "C1C04"};
+	RemoveItems = {"C1C00", "C1C01", "C1C02", "C1C03", "C1C04", LDConsts::Items::Bra};
 
 	ActorsShow = {
 		TSoftObjectPtr<AActor>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.Card01_UAID_D8BBC116E501139001_1298044567"))
--- Source/LifeDev/Game/Chaps/03/Inters/Var/BattsI02.cpp
+++ Source/LifeDev/Game/Chaps/03/Inters/Var/BattsI02.cpp
@@ -5,6 +5,7 @@
 ABattsI02::ABattsI02():Super() {
 	Texts = { NSLOCTEXT("BattsI02", "State0", "Pick up batteries") };
 	UseHint = true;
+	SetActorHiddenInGame(true);
 	RewardIntersHint = {
 		TSoftObjectPtr<AInteract>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.PuzzleI06_UAID_D8BBC116E5014A2C02_1407893984"))};
 }
