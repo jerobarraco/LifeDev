dbf4846a71c6657f41a6af693d4c53487cb4ed23
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-27 11:03:55 +1200

    * hide and show

--- Source/LifeDev/Game/Chaps/04/Steps/LStepC4S004.cpp
+++ Source/LifeDev/Game/Chaps/04/Steps/LStepC4S004.cpp
@@ -17,7 +17,11 @@ ALStepC4S004::ALStepC4S004():Super() {
 	RandFB->DelayMax = .85;
 	RandFB->ValueMin = -.015;
 	RandFB->ValueMax = .005;
+	ActorsShow = {
+		TSoftObjectPtr<AActor>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.CharNRG_UAID_D8BBC116E50193F001_1649313494"))
+	};
 	IntersActivate = {
+		// pot
 		TSoftObjectPtr<AInteract>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.StaticMeshActor_UAID_D8BBC116E501C1E701_2080650083"))
 		// this one does not work
 		// TSoftObjectPtr<AInteract>(FSoftObjectPath("/Game /__ExternalActors__/LifeDev/Game/Sys/Game_L/9/UP/CQK85FYMK0W4PWZQYSXILC.uasset"))
