cdeffe230622d703ae9c25d9ed6872352c57a78d
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-27 11:20:57 +1200

    * show/hide done

--- Source/LifeDev/Game/Chaps/04/Steps/LStepC4S009.cpp
+++ Source/LifeDev/Game/Chaps/04/Steps/LStepC4S009.cpp
@@ -13,6 +13,10 @@ ALStepC4S009::ALStepC4S009():Super() {
 	CamBlendTime = 2; // waittime will be forced to this
 	UseFBDlgAuto = true;
 	FBDlgAutoTo = -.05;
+	ActorsShow = {
+		TSoftObjectPtr<AActor>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.PaperI00_UAID_D8BBC116E50101EF01_1328482739")),
+		TSoftObjectPtr<AActor>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.Pencil_UAID_D8BBC116E5014CEF01_1096293939"))};
+	IntersTrigger = {TSoftObjectPtr<AInteract>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.CharNRG_UAID_D8BBC116E50193F001_1481028493"))};
 }
 
 void ALStepC4S009::BeginPlay() {
