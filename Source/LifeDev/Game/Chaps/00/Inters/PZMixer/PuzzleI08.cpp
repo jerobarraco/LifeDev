678ed704c2755cd2d481a5fc3bf5729bb2358a9a
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-28 09:38:25 +1200

    * cleanup recycle

--- Source/LifeDev/Game/Chaps/00/Inters/PZMixer/PuzzleI08.cpp
+++ Source/LifeDev/Game/Chaps/00/Inters/PZMixer/PuzzleI08.cpp
@@ -3,6 +3,6 @@
 #include "PuzzleI08.h"
 
 APuzzleI08::APuzzleI08():Super() {
-	ResetOnFail = true;
-	ResetTimeout = 1;
+	// ResetOnFail = true;
+	// ResetTimeout = 1;
 }
--- Source/LifeDev/Game/Chaps/00/Inters/PZMixer/PuzzleI08.h
+++ Source/LifeDev/Game/Chaps/00/Inters/PZMixer/PuzzleI08.h
@@ -4,10 +4,8 @@
 #include "LifeDev/Game/Interact/Base/LPuzzle.h"
 
 #include "PuzzleI08.generated.h"
-// TODO unfinished. feel free to reuse
 
-// Chap 00 Puzzle Instance 08
-// this works in tandem with a spot
+// TODO recycle
 UCLASS(Blueprintable, BlueprintType)
 class LIFEDEV_API APuzzleI08: public ALPuzzle {
 	GENERATED_BODY()
