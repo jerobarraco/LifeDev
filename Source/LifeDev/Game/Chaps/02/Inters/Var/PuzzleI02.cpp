d366118b4f6ad8cfb33743d8814ea0b18950b120
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-28 09:23:35 +1200

    * PuzzleI02.cpp pieces

--- Source/LifeDev/Game/Chaps/02/Inters/Var/PuzzleI02.cpp
+++ Source/LifeDev/Game/Chaps/02/Inters/Var/PuzzleI02.cpp
@@ -3,18 +3,24 @@
 #include "PuzzleI02.h"
 
 #include "Interact/CPuzzle.h"
+#include "JUtils/Misc/JMiscConsts.h"
 #include "LifeDev/Core/Consts/ConstFlags.h"
 
 APuzzleI02::APuzzleI02():Super() {
-	CPuzzle->Type = EPuzzleType::COMBINATION;
-	CPuzzle->Solution = {0, 0, 3}; // S.A.D.
-
 	UseHint = true;
 	RewardFlash = .15;
 	RewardFlag = LDConsts::Flags::Stats::Puzzles::Cube;
 	// static FName DoneId = "PZ02_T";
 	// TriggerDlg = DoneId;
 	RewardActor = TSoftObjectPtr<ALInteract>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.Card02_UAID_D8BBC116E501DFC401_1977970429"));
+
+	CPuzzle->Type = EPuzzleType::COMBINATION;
+	CPuzzle->Solution = {0, 0, 3}; // S.A.D.
+	CPuzzle->SetPieces({
+		SoftOP(AInteract, "/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.WCube03_UAID_D8BBC116E501E2CD01_1200587439"),
+		SoftOP(AInteract, "/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.WCube01_UAID_D8BBC116E501E2CD01_1181927438"),
+		SoftOP(AInteract, "/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.WCube01_UAID_D8BBC116E501E2CD01_1176815437"),
+	});
 }
 
 void APuzzleI02::BeginPlay() {
