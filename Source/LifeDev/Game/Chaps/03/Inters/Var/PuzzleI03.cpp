ad37c4f532c968dd6a0ea765b622efceacd25853
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-08-28 09:34:10 +1200

    * fix

--- Source/LifeDev/Game/Chaps/03/Inters/Var/PuzzleI03.cpp
+++ Source/LifeDev/Game/Chaps/03/Inters/Var/PuzzleI03.cpp
@@ -18,11 +18,11 @@ APuzzleI03::APuzzleI03():Super() {
 	CPuzzle->Type = EPuzzleType::COMBINATION;
 	CPuzzle->Solution = {0, 1, 0, 0, 4}; // S.H.A.M.E
 	CPuzzle->SetPieces({
-		SoftOP(AInteract, "/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.WCube04_UAID_D8BBC116E501EDD901_1493147028")
-		SoftOP(AInteract, "/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.WCube02_UAID_D8BBC116E501EDD901_1351720023")
-		SoftOP(AInteract, "/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.WCube01_UAID_D8BBC116E501EDD901_1421491025")
-		SoftOP(AInteract, "/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.WCube03_UAID_D8BBC116E501EDD901_1442181026")
-		SoftOP(AInteract, "/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.WCube01_UAID_D8BBC116E501EDD901_1487611027")
+		SoftOP(AInteract, "/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.WCube04_UAID_D8BBC116E501EDD901_1493147028"),
+		SoftOP(AInteract, "/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.WCube02_UAID_D8BBC116E501EDD901_1351720023"),
+		SoftOP(AInteract, "/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.WCube01_UAID_D8BBC116E501EDD901_1421491025"),
+		SoftOP(AInteract, "/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.WCube03_UAID_D8BBC116E501EDD901_1442181026"),
+		SoftOP(AInteract, "/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.WCube01_UAID_D8BBC116E501EDD901_1487611027"),
 	});
 }
 
