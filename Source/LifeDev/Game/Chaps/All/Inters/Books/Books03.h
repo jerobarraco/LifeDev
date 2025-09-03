b413a4415d60ce8a188a292df6d266427ef159d0
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-09-04 10:49:35 +1200

    * books replaced on room02

--- Source/LifeDev/Game/Chaps/All/Inters/Books/Books03.h
+++ Source/LifeDev/Game/Chaps/All/Inters/Books/Books03.h
@@ -1,13 +1,14 @@
 // Copyright Jerónimo Barraco-Mármol
 
 #pragma once
-#include "LifeDev/Game/Interact/Base/Var/Books.h"
+#include "LifeDev/Game/Interact/Base/Var/BooksB.h"
 
 #include "Books03.generated.h"
 
 // base class for interactable books. only 1 book
+// this is not used at the moment. maybe i should reparent to booksB
 UCLASS(Blueprintable, BlueprintType)
-class LIFEDEV_API ABooks03: public ABooks {
+class LIFEDEV_API ABooks03: public ABooksB {
 	GENERATED_BODY()
 
 public:
