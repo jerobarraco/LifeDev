2b08fe186a9b898dfd04a83772ab2c6a5d80cc3d
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-09-05 20:27:22 +1200

    * adjusted again

--- Source/LifeDev/Game/Chaps/03/Steps/LStepC3S002.cpp
+++ Source/LifeDev/Game/Chaps/03/Steps/LStepC3S002.cpp
@@ -13,8 +13,8 @@ ALStepC3S002::ALStepC3S002():Super() {
 	// required items, and it's hard to find them with a fb
 	RandFB->DelayMin = .5;
 	RandFB->DelayMax = 2;
-	RandFB->ValueMin = -.01;
-	RandFB->ValueMax = .005; // you wouldn't think is so easy to get out of a flashback, do you?
+	RandFB->ValueMin = -.02;
+	RandFB->ValueMax = .007; // you wouldn't think is so easy to get out of a flashback, do you?
 	UseFBRand = true;
 	UseRain = true;
 	FinishItems = {
