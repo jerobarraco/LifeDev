7aa9554c4e60396cea63762259050a8925573fe1
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-09-05 18:41:40 +1200

    * ported hint to cpp

--- Source/LifeDev/Game/Chaps/01/Inters/Var/BattsI00.cpp
+++ Source/LifeDev/Game/Chaps/01/Inters/Var/BattsI00.cpp
@@ -6,7 +6,9 @@ ABattsI00::ABattsI00():Super() {
 	Texts = { NSLOCTEXT("BattsI00", "State0", "Pick up batteries") };
 	UseHint = true;
 	HintCondition = "{Inter.Locked.WalkmanI00}";
-
+	RewardIntersHint = {
+		TSoftObjectPtr<AInteract>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.B_Walkman_C_UAID_D8BBC116E501248901_1198390170"))
+	};
 	// needs to move to be able to be attached to the drawer
 	Super::SetMobility(EComponentMobility::Movable);
 }
