a1de9983b0c666079090748ed65d36e632d1e5ed
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-09-16 09:05:06 +1200

    * intershint

--- Source/LifeDev/Game/Chaps/00/Inters/PZMixer/BottleI00.cpp
+++ Source/LifeDev/Game/Chaps/00/Inters/PZMixer/BottleI00.cpp
@@ -3,10 +3,14 @@
 #include "BottleI00.h"
 
 #include "Interact/Animator/CAnimatorFade.h"
+#include "JUtils/Misc/JMiscConsts.h"
 
 ABottleI00::ABottleI00():Super() {
 	RewardItem = "Bottle00";
 	UseRewardDestroy = true;
 	UseFade = true;
 	AnimFade->MatBase = nullptr;
+	RewardIntersHint = {
+		SoftOP(AInteract, "/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.SpotI04_UAID_D8BBC116E501525D02_1512082135"),
+	};
 }
--- Source/LifeDev/Game/Chaps/00/Inters/PZMixer/BottleI01.cpp
+++ Source/LifeDev/Game/Chaps/00/Inters/PZMixer/BottleI01.cpp
@@ -3,6 +3,7 @@
 #include "BottleI01.h"
 
 #include "Interact/Animator/CAnimatorFade.h"
+#include "JUtils/Misc/JMiscConsts.h"
 
 ABottleI01::ABottleI01():Super() {
 	RewardItem = "Bottle01";
@@ -11,5 +12,8 @@ ABottleI01::ABottleI01():Super() {
 	AnimFade->MatBase = nullptr;
 	Texts = {
 		NSLOCTEXT("Bottle00", "Text0", "Conditioner")
+	};
+	RewardIntersHint = {
+		SoftOP(AInteract, "/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.SpotI04_UAID_D8BBC116E501525D02_1512082135"),
 	};
 }
