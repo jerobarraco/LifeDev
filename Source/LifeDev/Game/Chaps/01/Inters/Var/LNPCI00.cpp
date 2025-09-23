56590f97ebfe142090f675aad20521ca756104f3
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-09-24 09:55:05 +1200

    * stub lnpci00

---
+++ Source/LifeDev/Game/Chaps/01/Inters/Var/LNPCI00.cpp
@@ -0,0 +1,5 @@
+#include "LNPCI00.h"
+
+ALNPCI00::ALNPCI00() {
+	
+}
---
+++ Source/LifeDev/Game/Chaps/01/Inters/Var/LNPCI00.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "LifeDev/Game/Chaps/All/NPCs/LNPC01.h"
+
+#include "LNPCI00.generated.h"
+
+// chap01 char18
+UCLASS(Blueprintable, BlueprintType)
+class ALNPCI00: public ALNPC01 {
+	GENERATED_BODY()
+
+public:
+	ALNPCI00();
+};
