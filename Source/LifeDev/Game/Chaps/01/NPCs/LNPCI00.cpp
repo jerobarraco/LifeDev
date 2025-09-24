c89615c1983ed4c370c68374dbb3639119022774
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-09-25 09:33:38 +1200

    * renamed npc01i00

--- Source/LifeDev/Game/Chaps/01/NPCs/LNPCI00.cpp
+++ Source/LifeDev/Game/Chaps/01/NPCs/LNPCI00.cpp
@@ -1,9 +1,9 @@
 // Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
-#include "LNPC01I00.h"
+#include "LNPCI00.h"
 
 #include "Story/Story.h"
 
-ALNPC01I00::ALNPC01I00():Super() {
+ALNPCI00::ALNPCI00():Super() {
 	// LockedDlg = "N01.L"; LNPCI00
 	Texts = { NSLOCTEXT("CH01", "NPC01I00", "Looks dangerous...") };
 	UseFade = true;
@@ -13,18 +13,18 @@ ALNPC01I00::ALNPC01I00():Super() {
 	SetActorHiddenInGame(true);
 }
 
-void ALNPC01I00::EndPlay(const EEndPlayReason::Type EndPlayReason) {
+void ALNPCI00::EndPlay(const EEndPlayReason::Type EndPlayReason) {
 	if (LIKELY(Story)) Story->OnStart.RemoveAll(this);
 
 	Super::EndPlay(EndPlayReason);
 }
 
-void ALNPC01I00::DoTrigger_Implementation() {
+void ALNPCI00::DoTrigger_Implementation() {
 	Super::DoTrigger_Implementation();
 	StandUp();
 }
 
-void ALNPC01I00::StandUp() {
+void ALNPCI00::StandUp() {
 	SetPoseStand();
 	AddActorLocalRotation(FRotator(0, -120, 0)); // turn around
 }
--- Source/LifeDev/Game/Chaps/01/NPCs/LNPCI00.h
+++ Source/LifeDev/Game/Chaps/01/NPCs/LNPCI00.h
@@ -5,17 +5,17 @@
 #include "CoreMinimal.h"
 #include "LifeDev/Game/Chaps/All/NPCs/LNPC01.h"
 
-#include "LNPC01I00.generated.h"
+#include "LNPCI00.generated.h"
 
 class UCAnimatorCam;
 
 // the instance for the npc01 in chapter 01 at boss step 
 UCLASS(Blueprintable, BlueprintType)
-class LIFEDEV_API ALNPC01I00 : public ALNPC01 {
+class LIFEDEV_API ALNPCI00 : public ALNPC01 {
 	GENERATED_BODY()
 
 public:
-	ALNPC01I00();
+	ALNPCI00();
 
 protected:
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
