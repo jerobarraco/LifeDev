365aa1ffb5da378326fc76c60a4ce1736b332f72
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-09-26 10:11:39 +1200

    * removed some old dlg consts

--- Source/LifeDev/Core/Consts/ConstDlgs.h
+++ Source/LifeDev/Core/Consts/ConstDlgs.h
@@ -5,34 +5,34 @@ namespace LDConsts {
 		namespace Item {
 			inline static const FName NotUsable = FName("Item.Fail.NotUsable");
 			inline static const FName NotReady = FName("Item.Fail.NotReady");
-			inline static const FName BadTarget = FName("Item.Fail.BadTarget");
-			inline static const FName NoTarget = FName("Item.Fail.NoTarget");
 			inline static const FString UsePre(TEXT("Item.Use."));
 			inline static const FString LookPre(TEXT("Item.Look."));
+			// inline static const FName BadTarget = FName("Item.Fail.BadTarget");
+			// inline static const FName NoTarget = FName("Item.Fail.NoTarget");
 		}
 		namespace Inter {
 			// the PRE expects the actor label at the end
-			inline static const FString ActivatePre("Inter.Activate.");
-			inline static const FString DeactivatePre("Inter.Deactivate.");
+			// inline static const FString ActivatePre("Inter.Activate.");
+			// inline static const FString DeactivatePre("Inter.Deactivate.");
+			// inline static const FString UnlockBadPre(TEXT("Inter.UnlockBad."));
 			inline static const FString LookPre("Inter.Look.");
 			inline static const FString StatePre("Inter.State.");
 			inline static const FString TriggerPre("Inter.Trigger.");
 			inline static const FString LockedPre("Inter.Locked.");
 			inline static const FString UnlockPre(TEXT("Inter.Unlock.")); // not as flag since trigger implies unlock
-			inline static const FString UnlockBadPre(TEXT("Inter.UnlockBad."));
-			// this one triggers twice. Inter.ItemUse."Interact"."Item" and Inter.ItemUse."Interact"
+			// this one triggers twice. Inter.ItemUse. "Interact"."Item" and Inter.ItemUse."Interact"
 			inline static const FString UseItemPre(TEXT("Inter.ItemUse."));
 			namespace Puzzle {
 				inline static const FString ResetPre(TEXT("Inter.Puzzle.Reset."));
 				inline static const FString UpdatePre(TEXT("Inter.Puzzle.Update."));
 			}
-			namespace Spot {
-				// these are pretty much deprecated now that object use is not used.
-				inline static const FString DropPre(TEXT("Inter.Spot.Drop."));
-				inline static const FString BadPre(TEXT("Inter.Spot.Bad."));
-				inline static const FString BadOrderPre(TEXT("Inter.Spot.BadOrder."));
-				inline static const FString FullPre(TEXT("Inter.Spot.Full."));
-			}
+			// these are pretty much deprecated now that object use is not used.
+			// namespace Spot {
+				// inline static const FString DropPre(TEXT("Inter.Spot.Drop."));
+				// inline static const FString BadPre(TEXT("Inter.Spot.Bad."));
+				// inline static const FString BadOrderPre(TEXT("Inter.Spot.BadOrder."));
+				// inline static const FString FullPre(TEXT("Inter.Spot.Full."));
+			// }
 		}
 		namespace Step {
 			inline static const FString StartPre("Step.Start.");
