59345ffa821f59baef3771815cdf89328d27cdd5
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-10-01 19:44:34 +1300

    * renaem BooksI01.cpp and set the hint

--- Source/LifeDev/Game/Chaps/04/Inters/Var/BooksI01.cpp
+++ Source/LifeDev/Game/Chaps/04/Inters/Var/BooksI01.cpp
@@ -3,13 +3,15 @@
 #include "BooksI01.h"
 
 ABooksI01::ABooksI01() {
-	Texts = { NSLOCTEXT("BooksI01", "State", "A book") };
-	// TriggerDlg = "BK01_T";
 	BookCount = 1;
 	RndSeed = 6;
+
+	Texts = { NSLOCTEXT("BooksI01", "State", "A book") };
+	// TriggerDlg = "BK01_T";
+	UseAutoActivate = true;
 	UseRewardDestroy = false;
+	UseHint = true; // hint at the start.
 	IsOneShot = true;
 	IsLocked = false;
-	UseAutoActivate = true;
 	RewardActor = TSoftObjectPtr<AActor>(FSoftObjectPath("/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.Card04_UAID_D8BBC116E501FB3602_1428206695"));
 }
