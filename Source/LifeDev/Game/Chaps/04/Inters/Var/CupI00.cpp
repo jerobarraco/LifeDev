efb8de0c483417536f339ae8993aeef19b54ff48
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-10-03 19:40:22 +1300

    * proper material

--- Source/LifeDev/Game/Chaps/04/Inters/Var/CupI00.cpp
+++ Source/LifeDev/Game/Chaps/04/Inters/Var/CupI00.cpp
@@ -1,5 +1,9 @@
 #include "CupI00.h"
 
+#include "CQuickMesh.h"
+
 ACupI00::ACupI00() {
-	
+	static ConstructorHelpers::FObjectFinder<UMaterialInterface>
+		ObjMat(TEXT("/Game/LifeDev/Game/Var/Mats/Voxel/Palettes/Palette01_DMI"));
+	if(LIKELY(ObjMat.Succeeded())) Mesh->SetMaterial(0, ObjMat.Object);
 }
