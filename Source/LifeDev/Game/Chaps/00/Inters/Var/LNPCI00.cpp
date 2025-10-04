65f2d3a3cd49efc353b1359503e604180508e1af
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-10-05 09:30:56 +1300

    * wip npci00

---
+++ Source/LifeDev/Game/Chaps/00/Inters/Var/LNPCI00.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
+#include "LNPCI00.h"
+
+ALNPCI00::ALNPCI00():Super() {
+	Texts = { NSLOCTEXT("CH00", "NPC01I00", "...") };
+	UseFade = true;
+	UseStateLoop = false;
+	UseRewardDestroy = false;
+}
---
+++ Source/LifeDev/Game/Chaps/00/Inters/Var/LNPCI00.h
@@ -0,0 +1,23 @@
+// Copyright (c) 2023 Jeronimo Barraco-Marmol. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LifeDev/Game/Chaps/All/NPCs/LNPC04.h"
+
+#include "LNPCI00.generated.h"
+
+class UCAnimatorCam;
+
+// the instance for the npc01 in chapter 01 at boss step 
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API ALNPCI00 : public ALNPC04 {
+	GENERATED_BODY()
+
+public:
+	ALNPCI00();
+
+protected:
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	virtual void DoTrigger_Implementation() override;
+};
