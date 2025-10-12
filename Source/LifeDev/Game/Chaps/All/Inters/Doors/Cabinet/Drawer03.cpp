296799d254fe97c1099efdd0c4d03c32563168a5
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-10-12 20:05:58 +1300

    * updated values

--- Source/LifeDev/Game/Chaps/All/Inters/Doors/Cabinet/Drawer03.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Doors/Cabinet/Drawer03.cpp
@@ -11,8 +11,8 @@ ADrawer03::ADrawer03():Super() {
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CMesh(TEXT("/Game/LifeDev/Game/Inters/Arch/Cabinets/Cabinet02-Drawer"));
 	Mesh->SetStaticMesh(CMesh.Object);
-	Mesh->SetRelativeLocation(FVector(80.000000,-62.500000,0.000000));
-	Interact->SetRelativeLocation(FVector(-20.000000,62.5,20.000000));
-	Interact->SetBoxExtent(FVector(5.000000,19.000000,5.000000));
+	Mesh->SetRelativeLocation(FVector(-5.000000,5.000000,-15.000000));
+	Interact->SetRelativeLocation(FVector(75.000000,-50.000000,22.500000));
+	Interact->SetBoxExtent(FVector(5.000000,45.000000,7.500000));
 	SFX->SetRelativeLocation(FVector(5.000000,0.000000,2.500000));
 }
