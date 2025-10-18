fcbe09e61b3a645476453daf64efdff977da81ad
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-10-18 20:22:01 +1300

    * increase shader wait

--- Plugins/Story/Source/Story/Story.cpp
+++ Plugins/Story/Source/Story/Story.cpp
@@ -142,8 +142,8 @@ bool UStory::Start(const FName Name) {
 			AsyncTask(ENamedThreads::Type::AnyBackgroundThreadNormalTask, [this] {
 				// attempt at waiting for shaders to compile on load.
 				while (FShaderPipelineCache::NumPrecompilesRemaining()>0) { // is it ok to spinlock this thread? should i try a different one?
-					std::this_thread::sleep_for(std::chrono::milliseconds(10));
 					UE_LOG(LogStory, Log, TEXT("UStory::Waiting on shaders. %i"), FShaderPipelineCache::NumPrecompilesRemaining());
+					std::this_thread::sleep_for(std::chrono::milliseconds(500));
 				}
 				AsyncTask(ENamedThreads::Type::GameThread, [this] {
 					// do fade out
