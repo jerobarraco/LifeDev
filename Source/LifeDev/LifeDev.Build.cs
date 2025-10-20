d2bc3d97faf5504c18ad8654b66c9bb461abe3c5
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-10-20 19:08:35 +1300

    * stub haptics

--- Source/LifeDev/LifeDev.Build.cs
+++ Source/LifeDev/LifeDev.Build.cs
@@ -29,6 +29,6 @@ public class LifeDev : ModuleRules {
 		// https://forums.unrealengine.com/t/setting-global-preprocessor-definitions-for-a-module/339593/2?u=nande
 		PublicDefinitions.Add("UE_SAVED_DIR_OVERRIDE=./MyData");
 		PrivateDefinitions.Add("UE_SAVED_DIR_OVERRIDE=./MyData");
-		PrivateDefinitions.Add("LD_DEMO=0");
+		PrivateDefinitions.Add("LD_DEMO=1");
 	}
 }
