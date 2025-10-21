dbc2cfae974517a213d8628ba4514b9212b13565
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-10-21 20:32:42 +1300

    * delete pdb file

--- Scripts/PackageWinNet.bat
+++ Scripts/PackageWinNet.bat
@@ -23,6 +23,10 @@ pushd %UNREAL_ENGINE_ROOT% || exit /b 1
 call ./Engine/Build/BatchFiles/RunUAT.bat BuildCookRun -project="%WORKSPACE%/%PROJECT_NAME%.uproject" -noP4 -platform=Win64 -clientconfig=%CONFIG% -serverconfig=%CONFIG% %clean% -cook -allmaps -build -stage -pak -stage -stagingdirectory="%WORKSPACE%/Build/" %DDC%
 :: -AdditionalCookerOptions="-cookprocesscount=4"
 popd
+
+
+del "%WORKSPACE%\Build\Windows\LifeDev\Binaries\Win64\LifeDev-Win64-Shipping.pdb"
+
 exit /b 0
 :: archive does nothing that staging does.
 :: -archive -archivedirectory="%WORKSPACE%/Build/"
