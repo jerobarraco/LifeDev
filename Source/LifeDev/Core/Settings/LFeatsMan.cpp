bd31848b64b6f0b9bcf5289307ba123c0b2b9c37
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-10-21 18:53:06 +1300

    * fix loading rumble feat and d_warn

--- Source/LifeDev/Core/Settings/LFeatsMan.cpp
+++ Source/LifeDev/Core/Settings/LFeatsMan.cpp
@@ -225,10 +225,10 @@ void ALFeatsMan::LoadFeats() {
 	FeatUpUnreal(EFeat::U_TICK_BATCH, Settings && Settings->GetFeat(EFeat::U_TICK_BATCH));
 	FeatUpUnreal(EFeat::U_TICK_CON, Settings && Settings->GetFeat(EFeat::U_TICK_CON));
 
-	FeatUpGame(EFeat::G_RUMBLE, Settings && Settings->GetFeat(EFeat::U_TICK_CON));
-	
+	FeatUpGame(EFeat::G_RUMBLE, Settings && Settings->GetFeat(EFeat::G_RUMBLE));
+
 	FeatUpDbg(EFeat::DBG_TESTDL, Settings && Settings->GetFeat(EFeat::DBG_TESTDL));
-	FeatUpDbg(EFeat::DBG_D_WARN, Settings && Settings->GetFeat(EFeat::G_STATUS));
+	FeatUpDbg(EFeat::DBG_D_WARN, Settings && Settings->GetFeat(EFeat::DBG_D_WARN));
 	// no need to initialize d_show or d_text
 }
 
