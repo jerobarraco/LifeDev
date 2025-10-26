080d620be27c6c282c5436dc1c35f60e53907156
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-10-26 16:44:00 +1300

    * added generic niagara effect type * optimized a bit scale

--- Source/LifeDev/Core/Story/LStep.cpp
+++ Source/LifeDev/Core/Story/LStep.cpp
@@ -19,7 +19,8 @@
 // todo ha a function to set the default ghost rumble
 // todo increase the rumble value with fb, is that possible? yes with 		Controller->ForceFeedbackScale
 // duration needs to change as well, maybe can be done.
-// i need a way to control each motor individually with the same ff asset. so simulate a heartbeat 
+// i need a way to control each motor individually with the same ff asset. so simulate a heartbeat
+
 DEFINE_LOG_CATEGORY_STATIC(LogLStoryStep, Log, Log);
 
 ALStep::ALStep():Super() {
