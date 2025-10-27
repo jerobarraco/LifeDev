f7140368be0d88e6eae5a45dcec554d3862e8b43
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-10-28 08:56:56 +1300

    * fixed hint on poem02

--- Source/LifeDev/Game/Chaps/All/Inters/Poems/PoemI02.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Poems/PoemI02.cpp
@@ -5,10 +5,12 @@
 #include "LifeDev/Core/Consts/ConstItems.h"
 
 APoemI02::APoemI02():Super() {
-	UseHint = false; // hint is set on the step that waits for it. 
+	// UseHint = false; // hint is set on the step that waits for it.
 	RewardFlash = -.2f;
 	RewardItem = LDConsts::Items::Poem2;
 	UseAutoActivate = true;
+	UseHint = true;
+	HintCondition = "{Step.Start.C2S4}"; // when reaching step 3 hint. as the step waits for it.
 	// it's active since the start.
 	// Super::SetActorHiddenInGame(true); // made visible by step. this triggers a call to setActive(false) but it's ok.
 	// Can't be static since it will be inside a drawer
