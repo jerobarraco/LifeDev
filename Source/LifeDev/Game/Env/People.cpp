7c51cc795bce8ca806dc94214aa09d3f0ea75029
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-10-29 19:17:20 +1300

    * wip stub people class

---
+++ Source/LifeDev/Game/Env/People.cpp
@@ -0,0 +1,6 @@
+// Copyright (C) 2023-2025 Jeronimo Barraco-Marmol
+
+#include "People.h"
+
+APeople::APeople():Super() {
+}
---
+++ Source/LifeDev/Game/Env/People.h
@@ -0,0 +1,16 @@
+// Copyright (C) 2023-2025 Jeronimo Barraco-Marmol
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LifeDev/Game/Interact/LInteract.h"
+
+#include "People.generated.h"
+
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API APeople : public ALInteract {
+	GENERATED_BODY()
+
+public:
+	APeople();
+};
