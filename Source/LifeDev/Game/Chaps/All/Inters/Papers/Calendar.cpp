72de7147609bd354b4c318883a0dcb8f60b02a6f
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-10-31 20:08:59 +1300

    * fixed shadows on Calendar.cpp

--- Source/LifeDev/Game/Chaps/All/Inters/Papers/Calendar.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Papers/Calendar.cpp
@@ -16,7 +16,8 @@ ACalendar::ACalendar():Super() {
 		CMesh(TEXT("/Game/LifeDev/Game/Inters/Calendar/Calendar.Calendar"));
 	Mesh->SetStaticMesh(CMesh.Object);
 	Mesh->SetRelativeLocation(FVector(-22.5,22.5,0));
-	
+	Mesh->SetCastAllShadows(true);
+
 	Interact->SetRelativeLocation(FVector(22.500000,-22.500000,2.500000));
 	Interact->SetBoxExtent(FVector(22.500000,22.500000,2.500000));
 	SFX->SetRelativeLocation(FVector(22.500000,-22.500000,2.500000));
