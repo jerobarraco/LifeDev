795b1d331f09ae79e7ece212c89fc8966990b435
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-11-05 10:54:26 +1300

    * pencil not contact

--- Source/LifeDev/Game/Chaps/All/Inters/Var/Pencil.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Var/Pencil.cpp
@@ -15,8 +15,8 @@ APencil::APencil():Super() {
 		CMesh(TEXT("/Game/LifeDev/Game/Inters/Var/Pencil_P"));
 	Mesh->SetStaticMesh(CMesh.Object);
 	Mesh->SetRelativeLocation(FVector(0,0.750000,-0.750000));
-	// Mesh->SetCastAllShadows(true);
-	Mesh->SetCastContactShadow(true);
+	Mesh->SetCastAllShadows(true);
+	// Mesh->SetCastContactShadow(true);
 	
 	Interact->SetBoxExtent(FVector(7.5,1,1));
 	Interact->SetRelativeLocation(FVector(7,-0.750000,0.750000));
@@ -27,5 +27,4 @@ APencil::APencil():Super() {
 	SFXs = { CSnd.Object };
 
 	APencil::SetMobility(EComponentMobility::Static);
-	
 }
