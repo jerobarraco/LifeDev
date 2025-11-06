1f902577e12c12554f4bae405539d41c8f845db7
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-11-06 19:10:24 +1300

    * stub ppl00

---
+++ Source/LifeDev/Game/Chaps/All/Env/Ppl00.cpp
@@ -0,0 +1,3 @@
+// Copyright (C) 2023-2025 Jeronimo Barraco-Marmol
+
+#include "Ppl00.h"
---
+++ Source/LifeDev/Game/Chaps/All/Env/Ppl00.h
@@ -0,0 +1,16 @@
+// Copyright (C) 2023-2025 Jeronimo Barraco-Marmol
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "LifeDev/Game/Env/Ppl.h"
+
+#include "Ppl00.generated.h"
+
+// A ppl that appears when you're not looking. and says something when you look at it.
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API APpl00 : public APpl {
+	GENERATED_BODY()
+
+};
