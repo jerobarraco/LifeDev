5c670ea8302b881b24fd46585ed761717329fe3b
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-11-06 19:17:55 +1300

    * wip ppl sig component

---
+++ Source/LifeDev/Game/Chaps/All/Env/CPplSig.h
@@ -0,0 +1,15 @@
+// Copyright (C) 2023-2025 Jeronimo Barraco-Marmol
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "JSig/CSignificance.h"
+#include "CPplSig.generated.h"
+
+
+UCLASS(Blueprintable, BlueprintType, meta=(BlueprintSpawnableComponent))
+class LIFEDEV_API UCPplSig : public UCSignificance {
+	GENERATED_BODY()
+
+public:
+};
