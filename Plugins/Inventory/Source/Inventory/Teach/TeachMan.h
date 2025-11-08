49b30ae57dfe8dabd5bab2509578521f328aa128
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-11-09 09:50:10 +1300

    * added keykeytarget

--- Plugins/Inventory/Source/Inventory/Teach/TeachMan.h
+++ Plugins/Inventory/Source/Inventory/Teach/TeachMan.h
@@ -62,7 +62,15 @@ public:
 	// https://github.com/ibbles/LearningUnrealEngine/blob/master/Text%20and%20string%20formatting.md
 	UFUNCTION(BlueprintCallable)
 	void SetKeyNames(const ETeachTarget Target, const TMap<FString, FText> Names);
-	
+
+	// TODO this belongs in the JUTils 50/50
+	UFUNCTION(BlueprintCallable)
+	FORCEINLINE ETeachTarget GetKeyTarget(const FKey& Key) const {
+		if (Key.IsGamepadKey()) return ETeachTarget::PAD;
+		if (Key.IsTouch()) return ETeachTarget::TOUCH;
+		return ETeachTarget::DESK;
+	};
+
 	// default duration for teach items. when the item time is <=0.
 	UPROPERTY(BlueprintReadWrite, Config)
 	float Time = 30;
