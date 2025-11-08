49faa0343572feedb4effd55ae9e5e8ed49f647c
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-11-08 20:11:40 +1300

    * stub flames

---
+++ Source/LifeDev/Game/Chaps/All/Inters/Kitchen/Flames.cpp
@@ -0,0 +1,8 @@
+// Copyright (C) 2023-2025 Jeronimo Barraco-Marmol
+
+
+#include "Flames.h"
+
+AFlames::AFlames() {
+}
+
---
+++ Source/LifeDev/Game/Chaps/All/Inters/Kitchen/Flames.h
@@ -0,0 +1,20 @@
+// Copyright (C) 2023-2025 Jeronimo Barraco-Marmol
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LifeDev/Game/Interact/LInteract.h"
+#include "Flames.generated.h"
+
+UCLASS(Blueprintable, BlueprintType)
+class LIFEDEV_API AFlames : public ALInteract {
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AFlames();
+
+protected:
+
+public:
+};
