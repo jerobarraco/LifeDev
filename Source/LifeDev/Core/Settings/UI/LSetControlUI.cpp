ac30e498f6a9a4f372f9a7fc5f92a38965de46d9
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-11-10 09:50:10 +1300

    * replace working

--- Source/LifeDev/Core/Settings/UI/LSetControlUI.cpp
+++ Source/LifeDev/Core/Settings/UI/LSetControlUI.cpp
@@ -4,6 +4,7 @@
 
 #include "InputMappingContext.h"
 #include "UserSettings/EnhancedInputUserSettings.h"
+#include "PlayerMappableKeySettings.h"
 
 #include "JButton.h"
 #include "LInputSelector.h"
@@ -49,11 +50,36 @@ void ULSetControlUI::ApplyKeyNames() {
 		TArray<FEnhancedActionKeyMapping> Mappings = Imc->GetMappings();
 		for (const FEnhancedActionKeyMapping& M: Mappings) {
 			const FKey& Key = M.Key;
+			const UPlayerMappableKeySettings* const KeySettings = M.GetPlayerMappableKeySettings();
+			if (!KeySettings) continue;
+			
+			const FName& FNamePre = KeySettings->Name;
+			if (FNamePre.IsNone()) continue; // means the key is unmappable
+			
+			// derive the generic name from the name. remove the last _gp _kb
+			const FString& NamePre = FNamePre.ToString();
+			FString NameLeft, NameRight;
+			NamePre.Split("_", &NameLeft, &NameRight, ESearchCase::IgnoreCase, ESearchDir::FromEnd);
+			const FName Name(NameLeft);
+			if (Name.IsNone()) continue;
+
+			const FText& Text = Key.GetDisplayName(); // i hope this works. it binds to the key.
+			// const FText& Text = KeySettings->DisplayName; // doesn't automatically provide the text, maybe i will have to set it myself. not for now
+			const ETeachTarget Tgt = UJUtilsSys::GetKeyTarget(Key);
+			TMap<FString, FText>& Map = Names.FindOrAdd(Tgt);
+			Map.Add(Name.ToString(), Text);
+			UE_LOG(LogTemp, Log, TEXT("%hs Added key text=%s name=%s tgt=%s"), __func__,
+				*Text.ToString(), *Name.ToString(), *UEnum::GetValueAsString(Tgt));
+		}
+
+		/*
+		for (const FEnhancedActionKeyMapping& M: Mappings) {
+			const FKey& Key = M.Key;
 			const FName& FNamePre = Key.GetFName();
 			if (FNamePre.IsNone()) continue; // means the key is unmappable
 			// const FName& Name = M.Key.GetMenuCategory(); // the category doesn't give what i want.
 			// it actually gives the target "gamepad" "keyboard" etc
-
+			
 			// derive the generic name from the name. remove the last _gp _kb
 			const FString& NamePre = FNamePre.ToString();
 			FString NameLeft, NameRight;
@@ -68,10 +94,11 @@ void ULSetControlUI::ApplyKeyNames() {
 			UE_LOG(LogTemp, Log, TEXT("%hs Added key text=%s name=%s tgt=%s"), __func__,
 				*Text.ToString(), *Name.ToString(), *UEnum::GetValueAsString(Tgt));
 		}
-	}
 	// TODO category doesn't give what i need.
+*/
+	}
 
-	// todo fix will crash if names[] not set
+	// fix, will crash if names[] not set
 	if (Names.Contains(ETeachTarget::DESK))
 		TeachMan->SetKeyNames(ETeachTarget::DESK, Names[ETeachTarget::DESK]);
 	if (Names.Contains(ETeachTarget::PAD))
