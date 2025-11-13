b32ecaa53aad935d3286f0c1bedc2030e0231f52
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-11-14 09:41:13 +1300

    * fixed mesh for scewer

--- Source/LifeDev/Game/Chaps/02/Inters/Var/ScrewerI00.cpp
+++ Source/LifeDev/Game/Chaps/02/Inters/Var/ScrewerI00.cpp
@@ -2,6 +2,7 @@
 
 #include "ScrewerI00.h"
 
+#include "CQuickMesh.h"
 #include "Interact/Animator/CAnimatorFade.h"
 
 AScrewerI00::AScrewerI00():Super() {
@@ -12,5 +13,9 @@ AScrewerI00::AScrewerI00():Super() {
 	UseFade = true;
 	AnimFade->MatBase = nullptr; // the mesh uses a palette. force using the mesh's material.
 
+	static ConstructorHelpers::FObjectFinder<UMaterialInterface>
+		ObjMat(TEXT("/Game/LifeDev/Game/Var/Mats/Voxel/Palettes/Mask/Palette01_MMI"));
+	if(LIKELY(ObjMat.Succeeded())) Mesh->SetMaterial(0, ObjMat.Object);
+	
 	AScrewerI00::SetMobility(EComponentMobility::Type::Static);
 }
