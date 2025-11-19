6d948e8d0d0155ece9864a1abc2bf17949a1c126
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-11-19 19:34:17 +1300

    * tweaks to bed

--- Source/LifeDev/Game/Chaps/00/Inters/Var/BedI00.cpp
+++ Source/LifeDev/Game/Chaps/00/Inters/Var/BedI00.cpp
@@ -18,7 +18,7 @@ ABedI00::ABedI00():Super() {
 	Interact->SetBoxExtent(FVector(102.5,55,32.5));
 	
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
-		CMesh(TEXT("/Game/LifeDev/Game/Inters/Beds/Bed00.Bed00"));
+		CMesh(TEXT("/Game/LifeDev/Game/Inters/Beds/Bed00"));
 	Mesh->SetStaticMesh(CMesh.Object);
 	Mesh->SetUseDynShadow(true);
 
