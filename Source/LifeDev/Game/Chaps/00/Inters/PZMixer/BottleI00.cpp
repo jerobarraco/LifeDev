071e2a939a4582158c8fbf45763e1f249668ab5a
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2025-11-21 09:45:58 +1300

    * fixed bottle materials

--- Source/LifeDev/Game/Chaps/00/Inters/PZMixer/BottleI00.cpp
+++ Source/LifeDev/Game/Chaps/00/Inters/PZMixer/BottleI00.cpp
@@ -2,14 +2,20 @@
 
 #include "BottleI00.h"
 
+#include "CQuickMesh.h"
 #include "Interact/Animator/CAnimatorFade.h"
 #include "JUtils/Misc/JMiscConsts.h"
 
 ABottleI00::ABottleI00():Super() {
+	Texts = { NSLOCTEXT("Bottle00", "Text0", "Shampoo") };
 	RewardItem = "Bottle00";
 	UseRewardDestroy = true;
 	UseFade = true;
-	AnimFade->MatBase = nullptr;
+	AnimFade->MatBase = nullptr; // use the mesh's mat
+	static ConstructorHelpers::FObjectFinder<UMaterialInterface>
+		CMat(TEXT("/Game/LifeDev/Game/Var/Mats/Voxel/Palettes/Mask/Palette03_MMI"));
+	if (LIKELY(CMat.Object)) Mesh->SetMaterial(0, CMat.Object);
+
 	RewardIntersHint = {
 		SoftOP(AInteract, "/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.SpotI04_UAID_D8BBC116E501525D02_1512082135"),
 	};
--- Source/LifeDev/Game/Chaps/00/Inters/PZMixer/BottleI01.cpp
+++ Source/LifeDev/Game/Chaps/00/Inters/PZMixer/BottleI01.cpp
@@ -2,17 +2,20 @@
 
 #include "BottleI01.h"
 
+#include "CQuickMesh.h"
 #include "Interact/Animator/CAnimatorFade.h"
 #include "JUtils/Misc/JMiscConsts.h"
 
 ABottleI01::ABottleI01():Super() {
+	Texts = { NSLOCTEXT("Bottle00", "Text0", "Conditioner") };
 	RewardItem = "Bottle01";
 	UseRewardDestroy = true;
 	UseFade = true;
-	AnimFade->MatBase = nullptr;
-	Texts = {
-		NSLOCTEXT("Bottle00", "Text0", "Conditioner")
-	};
+	AnimFade->MatBase = nullptr; // use the mesh's mat
+	static ConstructorHelpers::FObjectFinder<UMaterialInterface>
+		CMat(TEXT("/Game/LifeDev/Game/Var/Mats/Voxel/Palettes/Mask/Palette07_MMI"));
+	if (LIKELY(CMat.Object)) Mesh->SetMaterial(0, CMat.Object);
+	
 	RewardIntersHint = {
 		SoftOP(AInteract, "/Game/LifeDev/Game/Sys/Game_L.Game_L:PersistentLevel.SpotI04_UAID_D8BBC116E501525D02_1512082135"),
 	};
