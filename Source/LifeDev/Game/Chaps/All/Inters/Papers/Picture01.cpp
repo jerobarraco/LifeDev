40b630c8dd113734616c54ffda7ba94989ce906c
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2026-01-20 19:40:44 +1300

    * reparent picture

--- Source/LifeDev/Game/Chaps/All/Inters/Papers/Picture01.cpp
+++ Source/LifeDev/Game/Chaps/All/Inters/Papers/Picture01.cpp
@@ -9,31 +9,18 @@
 #include "JUtils/Actors/CQuickMesh.h"
 
 APicture01::APicture01():Super() {
-	UseAnim = false;
-	UseFade = false;
-	UseRewardDestroy = false;
-	StateNum = 1;
-	Texts = { NSLOCTEXT("PictureI01", "State0", "A Picture") };
 
 	static ConstructorHelpers::FObjectFinder<UStaticMesh>
 		CMesh(TEXT("/Game/LifeDev/Game/Inters/Picture/Picture01_P"));
 	Mesh->SetStaticMesh(CMesh.Object);
 	Mesh->SetRelativeRotation(FRotator(90,0,90).Quaternion());
 	Mesh->SetRelativeLocation(FVector(25,0,0));
-	Mesh->SetUseDynShadow(true);
-	Mesh->bUseAsOccluder = false; // optim. against wall, can't occlude.
 
 	Interact->SetRelativeLocation(FVector(30.000000,-25.000000,7.5));
 	Interact->SetBoxExtent(FVector(35,30,7.5));
 	SFX->SetRelativeLocation(FVector::ZeroVector);
 
-	static ConstructorHelpers::FObjectFinder<USoundBase>
-		CSnd(TEXT("/Game/LifeDev/Game/Inters/Generic/Drawers/DrawerOpen_C"));
-	// SFXTrigger = CSnd.Object;
-	SFXs = { CSnd.Object };
-
 	// force using the material from the mesh. (though we don't use reward destroy)
 	AnimFade->MatBase = nullptr;
 	AnimFade->Mat = nullptr;
-	Super::SetMobility(EComponentMobility::Static);
 }
--- Source/LifeDev/Game/Chaps/All/Inters/Papers/Picture01.h
+++ Source/LifeDev/Game/Chaps/All/Inters/Papers/Picture01.h
@@ -1,12 +1,13 @@
 // Copyright Jerónimo Barraco-Mármol
 
 #pragma once
-#include "LifeDev/Game/Interact/LInteract.h"
+#include "Picture00.h"
 
 #include "Picture01.generated.h"
 
+// Base class for a picture frame that uses the palette version.
 UCLASS(Blueprintable, BlueprintType)
-class LIFEDEV_API APicture01: public ALInteract {
+class LIFEDEV_API APicture01: public APicture00 {
 	GENERATED_BODY()
 
 public:
