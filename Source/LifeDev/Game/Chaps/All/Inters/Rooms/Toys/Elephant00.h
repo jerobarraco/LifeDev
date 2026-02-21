f080293697c14d517038986b2ff6ec26c200e239
Jeronimo Barraco-Marmol <3729895+jerobarraco@users.noreply.github.com>
2026-02-21 18:33:58 +1300

    * moved toy * added stub elephant

---
+++ Source/LifeDev/Game/Chaps/All/Inters/Rooms/Toys/Elephant00.h
@@ -0,0 +1,16 @@
+// Copyright (C) 2023-2025 Jeronimo Barraco-Marmol
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "LifeDev/Game/Interact/LInteract.h"
+
+#include "Elephant00.generated.h"
+
+UCLASS(Blueprintable)
+class LIFEDEV_API AElephant00 : public ALInteract {
+	GENERATED_BODY()
+
+public:
+	AElephant00();
+};
